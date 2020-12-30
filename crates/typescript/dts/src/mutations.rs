135c520639b6e5ee472fc68449c64ab49ba32217
강동윤 <kdy1997.dev@gmail.com>
2020-12-31 01:22:13 +0900

    Aplly function return type mutations

--- crates/typescript/dts/src/mutations.rs
+++ crates/typescript/dts/src/mutations.rs
@@ -1,5 +1,8 @@
+use rnode::VisitMut;
 use rnode::VisitMutWith;
+use stc_ts_ast_rnode::RFunction;
 use stc_ts_ast_rnode::RModule;
+use stc_ts_dts_mutations::FunctionMut;
 use stc_ts_dts_mutations::Mutations;
 
 pub fn apply_mutations(mutations: Mutations, m: &mut RModule) {
@@ -9,4 +12,16 @@ pub fn apply_mutations(mutations: Mutations, m: &mut RModule) {
 
 struct Operator {
     mutations: Mutations,
+}
+
+impl VisitMut<RFunction> for Operator {
+    fn visit_mut(&mut self, f: &mut RFunction) {
+        f.visit_mut_children_with(self);
+
+        if let Some(FunctionMut { ret_ty }) = self.mutations.for_fns.remove(&f.node_id) {
+            if let Some(ret_ty) = ret_ty {
+                f.return_type = Some(ret_ty.into())
+            }
+        }
+    }
 }
