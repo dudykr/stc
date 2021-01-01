5532b1a148a6589c2f9031bdc1f44822c276d4e1
강동윤 <kdy1997.dev@gmail.com>
2021-01-01 12:09:03 +0900

    More info on panic

--- crates/typescript/file_analyzer/src/mode/mod.rs
+++ crates/typescript/file_analyzer/src/mode/mod.rs
@@ -404,7 +404,10 @@ impl Mode for Group<'_> {
             }
         }
 
-        unreachable!("failed to get path by module id: {:?}", self.files)
+        unreachable!(
+            "failed to get path by module id({:?}):  {:?}",
+            id, self.files
+        )
     }
 
     fn subscope(&self) -> Storage {
