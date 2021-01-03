ccd9b488297d51887fdd448f011ebbc3c0a9881f
강동윤 <kdy1997.dev@gmail.com>
2021-01-03 12:23:37 +0900

    Rename

--- crates/typescript/file_analyzer/tests/fixture.rs
+++ crates/typescript/file_analyzer/tests/fixture.rs
@@ -29,7 +29,7 @@ use swc_ecma_parser::TsConfig;
 use swc_ecma_transforms::resolver::ts_resolver;
 use swc_ecma_visit::FoldWith;
 
-#[testing::fixture("no-errors/**/*.ts", exclude(".*\\.\\.d.\\.ts"))]
+#[testing::fixture("visualize/**/*.ts", exclude(".*\\.\\.d.\\.ts"))]
 fn visualize(file_name: PathBuf) {
     let fname = file_name.display().to_string();
     println!("{}", fname);
