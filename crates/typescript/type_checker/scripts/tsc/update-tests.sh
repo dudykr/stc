e9682b1e2402b4d90cd003d8a01af64cc7e21a86
강동윤 <kdy1997.dev@gmail.com>
2021-01-03 12:52:03 +0900

    conformance: Remove error spec files and update

--- crates/typescript/type_checker/scripts/tsc/update-tests.sh
+++ crates/typescript/type_checker/scripts/tsc/update-tests.sh
@@ -6,4 +6,5 @@ cp -R ~/projects/TypeScript/tests/cases/conformance/ ./tests/conformance/
 
 mkdir -p ~/projects/stc/typescript/checker/tests/reference/
 
-find ~/projects/TypeScript -name '*.errors.txt' -exec cp {} ~/projects/stc/typescript/checker/tests/reference/ \;
+# find ~/projects/TypeScript -name '*.errors.txt' -exec cp {} tests/reference/ \;
+find ~/projects/TypeScript -name '*.types' -exec cp {} tests/reference/ \;
