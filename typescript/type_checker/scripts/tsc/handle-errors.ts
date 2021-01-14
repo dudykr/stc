ac1a058b97b72a294e3268cba07b9929b6a95052
강동윤 <kdy1997.dev@gmail.com>
2021-01-15 01:32:18 +0900

    Fix scripts

--- typescript/type_checker/scripts/tsc/handle-errors.ts
+++ typescript/type_checker/scripts/tsc/handle-errors.ts
@@ -12,21 +12,56 @@ async function* walk(dir: string): any {
     }
 }
 
+interface ErrorRef {
+    line: number,
+    column: number,
+    code: string,
+}
+
+function extract(content: string): ErrorRef[] {
+    const errors: ErrorRef[] = [];
+    for (const str of content.split('\n')) {
+        if (str.startsWith('====')) {
+            break
+        }
+        const [, data] = str.split('(', 2);
+        if (!data) { continue }
+
+        const [line, column] = (data.split(')', 2)[0].split(',').map(v => parseInt(v)))
+
+        const code = data.split('error ', 2)[1]?.split(':')[0];
+
+        console.log(line, column, code);
+        errors.push({
+            line,
+            column,
+            code
+        })
+    }
+
+    return errors;
+}
 
 (async function () {
     for await (const p of walk('tests/conformance')) {
         if (!p.endsWith('.ts') && !p.endsWith('.tsx')) continue;
+        const dir = path.dirname(p);
         const fname = path.basename(p);
         const errorFilePath = path.join('tests', 'reference', `${fname.replace('.ts', '.errors.txt')}`)
         if (!fs.existsSync(errorFilePath)) {
             continue
         }
 
+        const errorJsonPath = path.join(dir, 'errors.json');
+
+        const content = await fs.promises.readFile(errorFilePath, 'utf-8');
+        const errors = extract(content)
+
         console.log('----- ----- ----- ----- -----')
         console.log(p)
 
         console.log('Error refs:', errorFilePath)
-
+        await fs.promises.writeFile(errorJsonPath, JSON.stringify(errors))
     }
 })()
 
