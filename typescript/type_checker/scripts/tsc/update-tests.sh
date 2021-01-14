15c48fe041c710b1bcec04fc1a46e33ad6cd85bc
강동윤 <kdy1997.dev@gmail.com>
2021-01-15 01:06:09 +0900

    WIP: Error reference parser

--- typescript/type_checker/scripts/tsc/update-tests.sh
+++ typescript/type_checker/scripts/tsc/update-tests.sh
@@ -8,3 +8,7 @@ mkdir -p ~/projects/stc/typescript/checker/tests/reference/
 
 find ~/projects/TypeScript -name '*.errors.txt' -exec cp {} tests/reference/ \;
 # find ~/projects/TypeScript -name '*.types' -exec cp {} tests/reference/ \;
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+bash $DIR/handle-errors.sh
