3c301949299f6dacf4786d1a7b752b53a6a597d4
강동윤 <kdy1997.dev@gmail.com>
2021-01-15 15:18:57 +0900

    Auto-update visualization test

--- typescript/type_checker/scripts/test.sh
+++ typescript/type_checker/scripts/test.sh
@@ -4,6 +4,6 @@ set -eux
 
 # We use visualizer to see actual types while working on conformance.
 # So existence of failing visualization test typically means that we added a new fixture.
-cargo test --test visualize
+UPDATE=1 cargo test --test visualize
 
 cargo test --test conformance
