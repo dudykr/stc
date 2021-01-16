e19b0398a562f3158cff36a21c37b8690a2d73be
강동윤 <kdy1997.dev@gmail.com>
2021-01-16 17:46:49 +0900

    Make success.sh print full path of successful tests

--- typescript/type_checker/scripts/success.sh
+++ typescript/type_checker/scripts/success.sh
@@ -6,4 +6,6 @@ cargo test --test conformance \
   | grep '.\.\. ok$' \
   | sed -e 's!test tsc::conformance::!!' \
   | sed -e 's! ... ok!!' \
+  | sed -e 's!::!/!' \
+  | sed -e 's!test !!' \
   > tests/success.txt
