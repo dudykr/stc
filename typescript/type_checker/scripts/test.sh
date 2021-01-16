2c0d80edc1da26e39122d8ff02a0ad03dc218de3
강동윤 <kdy1997.dev@gmail.com>
2021-01-16 15:10:53 +0900

    Improve test script

--- typescript/type_checker/scripts/test.sh
+++ typescript/type_checker/scripts/test.sh
@@ -3,8 +3,10 @@ set -eux
 
 ./scripts/sort.sh
 
+
 # We use visualizer to see actual types while working on conformance.
 # So existence of failing visualization test typically means that we added a new fixture.
+git restore -- tests/visualize
 UPDATE=1 cargo test --test visualize
 
 cargo test --test conformance
