ef6e72bb0695c9df9d960a6d69a5f9afb6ae687e
강동윤 <kdy1997.dev@gmail.com>
2021-01-24 00:26:53 +0900

    fixup

--- typescript/file_analyzer/src/analyzer/assign/type_el.rs
+++ typescript/file_analyzer/src/analyzer/assign/type_el.rs
@@ -1,11 +1,14 @@
+use super::AssignOpts;
 use crate::analyzer::Analyzer;
+use stc_ts_types::ClassMember;
+use stc_ts_types::TypeElement;
 
 impl Analyzer<'_, '_> {
     pub(super) fn assign_class_members_to_type_element(
         &mut self,
         opts: AssignOpts,
         el: &TypeElement,
-        rhs_members: &[stc_ts_types::ClassMember],
+        rhs_members: &[ClassMember],
     ) -> ValidationResult<()> {
         Ok(())
     }
