15e01c6f36d2adc60ef895012998b9082f55d02f
강동윤 <kdy1997.dev@gmail.com>
2021-01-24 00:37:45 +0900

    More work for assign_class_members_to_type_element

--- typescript/file_analyzer/src/analyzer/assign/type_el.rs
+++ typescript/file_analyzer/src/analyzer/assign/type_el.rs
@@ -1,5 +1,6 @@
 use super::AssignOpts;
 use crate::analyzer::Analyzer;
+use crate::ValidationResult;
 use stc_ts_types::ClassMember;
 use stc_ts_types::TypeElement;
 
@@ -10,6 +11,37 @@ impl Analyzer<'_, '_> {
         el: &TypeElement,
         rhs_members: &[ClassMember],
     ) -> ValidationResult<()> {
+        match el {
+            TypeElement::Call(_) => {}
+            TypeElement::Constructor(_) => {}
+            TypeElement::Property(lp) => {
+                for rhs_member in rhs_members {
+                    match rhs_member {
+                        ClassMember::Method(_) => {}
+                        ClassMember::Property(rp) => {
+                            // Check for property
+                            if self.assign(&lp.key.ty(), &rp.key.ty(), opts.span).is_ok() {
+                                if let Some(lt) = &lp.type_ann {
+                                    if let Some(rt) = &rp.value {
+                                        self.assign(&lt, &rt, opts.span)?;
+                                    }
+                                }
+                            }
+                        }
+                        _ => continue,
+                    }
+                }
+
+                if lp.optional {
+                    return Ok(());
+                }
+
+                // TODO: Report error.
+            }
+            TypeElement::Method(_) => {}
+            TypeElement::Index(_) => {}
+        }
+
         Ok(())
     }
 }
