1a49beba0a11f3ed19720a0e32c6503a91246d13
강동윤 <kdy1997.dev@gmail.com>
2021-02-01 20:41:29 +0900

    Move codes

--- typescript/file_analyzer/src/analyzer/assign/type_el.rs
+++ typescript/file_analyzer/src/analyzer/assign/type_el.rs
@@ -1,10 +1,489 @@
 use super::AssignOpts;
+use crate::analyzer::util::ResultExt;
 use crate::analyzer::Analyzer;
 use crate::ValidationResult;
+use stc_ts_ast_rnode::RTsKeywordType;
+use stc_ts_ast_rnode::RTsLit;
+use stc_ts_ast_rnode::RTsLitType;
+use stc_ts_errors::DebugExt;
+use stc_ts_errors::Error;
+use stc_ts_errors::Errors;
+use stc_ts_types::Array;
+use stc_ts_types::Class;
+use stc_ts_types::ClassInstance;
 use stc_ts_types::ClassMember;
+use stc_ts_types::Interface;
+use stc_ts_types::MethodSignature;
+use stc_ts_types::PropertySignature;
+use stc_ts_types::Tuple;
+use stc_ts_types::Type;
 use stc_ts_types::TypeElement;
+use stc_ts_types::TypeLit;
+use swc_atoms::js_word;
+use swc_common::Span;
+use swc_common::Spanned;
+use swc_common::TypeEq;
+use swc_ecma_ast::Accessibility;
+use swc_ecma_ast::TsKeywordTypeKind;
 
 impl Analyzer<'_, '_> {
+    /// This method is called when lhs of assignment is interface or type
+    /// literal.
+    ///
+    /// ```js
+    /// interface A {}
+    /// let a: A = foo;
+    /// let b: { key: string } = foo;
+    /// ```
+    pub(super) fn assign_to_type_elements(
+        &mut self,
+        opts: AssignOpts,
+        lhs_span: Span,
+        lhs: &[TypeElement],
+        rhs: &Type,
+    ) -> ValidationResult<()> {
+        let span = opts.span;
+        // debug_assert!(!span.is_dummy());
+
+        let mut errors = Errors::default();
+        let mut missing_fields = vec![];
+
+        let numeric_keyed_ty = lhs
+            .iter()
+            .filter_map(|e| match e {
+                TypeElement::Index(ref i)
+                    if i.params.len() == 1 && i.params[0].ty.is_kwd(TsKeywordTypeKind::TsNumberKeyword) =>
+                {
+                    Some(i.type_ann.as_ref())
+                }
+
+                _ => None,
+            })
+            .next();
+
+        if let Some(numeric_keyed_ty) = numeric_keyed_ty {
+            let any = box Type::any(span);
+            let numeric_keyed_ty = numeric_keyed_ty.unwrap_or(&any);
+
+            match *rhs.normalize() {
+                Type::Array(Array { ref elem_type, .. }) => {
+                    return self.assign_inner(numeric_keyed_ty, elem_type, opts)
+                }
+
+                Type::Tuple(Tuple { ref elems, .. }) => {
+                    let mut errors = Errors::default();
+                    for el in elems {
+                        self.assign_inner(
+                            numeric_keyed_ty,
+                            &el.ty,
+                            AssignOpts {
+                                span: if el.span().is_dummy() { span } else { el.span() },
+                                ..opts
+                            },
+                        )
+                        .store(&mut errors);
+                    }
+                    return if errors.is_empty() {
+                        Ok(())
+                    } else {
+                        Err(box Error::Errors {
+                            span,
+                            errors: errors.into(),
+                        })
+                    };
+                }
+
+                _ => {}
+            }
+        }
+
+        {
+            let mut unhandled_rhs = vec![];
+
+            macro_rules! handle_type_elements {
+                ($rhs:expr) => {{
+                    for r in $rhs {
+                        if !opts.allow_unknown_rhs {
+                            unhandled_rhs.push(r.span());
+                        }
+                    }
+
+                    // TODO: Index signature can eat multiple rhs.
+
+                    for (i, m) in lhs.into_iter().enumerate() {
+                        let res = self
+                            .assign_type_elements_to_type_element(opts, &mut missing_fields, m, $rhs)
+                            .with_context(|| format!("tried to assign to {}th element: {:?}", i, m.key()));
+
+                        let success = match res {
+                            Ok(()) => true,
+                            Err(box Error::Errors { ref errors, .. }) if errors.is_empty() => true,
+                            Err(err) => {
+                                errors.push(err);
+                                false
+                            }
+                        };
+                        if success && $rhs.len() > i {
+                            if let Some(pos) = unhandled_rhs.iter().position(|span| *span == $rhs[i].span()) {
+                                unhandled_rhs.remove(pos);
+                            } else {
+                                // panic!("it should be removable")
+                            }
+                        }
+                    }
+                }};
+            }
+
+            match rhs.normalize() {
+                Type::TypeLit(TypeLit {
+                    members: rhs_members, ..
+                }) => {
+                    handle_type_elements!(&*rhs_members);
+                }
+
+                Type::Interface(Interface { body, .. }) => {
+                    handle_type_elements!(&*body);
+                    // TODO: Check parent interface
+                }
+
+                Type::Tuple(..) if lhs.is_empty() => return Ok(()),
+
+                Type::Array(..) if lhs.is_empty() => return Ok(()),
+
+                Type::Array(..) => return Err(box Error::InvalidAssignmentOfArray { span }),
+
+                Type::Tuple(rhs) => {
+                    // Handle { 0: nubmer } = [1]
+                    let rhs_len = rhs.elems.len();
+
+                    // TODO: Check for literal properties
+
+                    // for el in lhs {
+                    //     match el {
+                    //         TypeElement::Property(l_el) => {
+                    //             match l
+                    //         }
+                    //         _ => {}
+                    //     }
+                    // }
+
+                    return Ok(());
+                }
+
+                Type::Class(rhs) => {
+                    // TODO: Check if constructor exists.
+                    if rhs.is_abstract {
+                        return Err(box Error::CannotAssignAbstractConstructorToNonAbstractConstructor { span });
+                    }
+                    //
+                    for el in lhs {
+                        self.assign_class_members_to_type_element(opts, el, &rhs.body)?;
+                    }
+
+                    return Ok(());
+                }
+
+                Type::Keyword(RTsKeywordType {
+                    kind: TsKeywordTypeKind::TsNumberKeyword,
+                    ..
+                })
+                | Type::Keyword(RTsKeywordType {
+                    kind: TsKeywordTypeKind::TsStringKeyword,
+                    ..
+                })
+                | Type::Lit(RTsLitType {
+                    lit: RTsLit::Number(..),
+                    ..
+                })
+                | Type::Lit(RTsLitType {
+                    lit: RTsLit::Str(..), ..
+                }) if lhs.is_empty() => return Ok(()),
+
+                _ => {
+                    return Err(box Error::Unimplemented {
+                        span,
+                        msg: format!("assign_to_type_elements - ??"),
+                    })
+                }
+            }
+
+            if !errors.is_empty() {
+                return Err(errors)?;
+            }
+
+            if !unhandled_rhs.is_empty() {
+                // The code below is invalid as c is not defined in type.
+                //
+                //      var c { [n: number]: { a: string; b: number; }; } = [{ a:
+                // '', b: 0, c: '' }];
+
+                return Err(box Error::Errors {
+                    span,
+                    errors: unhandled_rhs
+                        .into_iter()
+                        .map(|span| box Error::UnknownPropertyInObjectLiteralAssignment { span })
+                        .collect(),
+                });
+            }
+        }
+
+        'l: for m in lhs {
+            // Handle `toString()`
+            match m {
+                TypeElement::Method(ref m) => {
+                    if m.key == js_word!("toString") {
+                        continue;
+                    }
+                }
+                _ => {}
+            }
+
+            // Handle optional
+            match m {
+                TypeElement::Method(ref m) if m.optional => continue,
+                TypeElement::Property(ref m) if m.optional => continue,
+                _ => {}
+            }
+
+            match *rhs.normalize() {
+                // Check class itself
+                Type::Class(Class { ref body, .. }) => {
+                    match m {
+                        TypeElement::Call(_) => {
+                            unimplemented!("ssign: interface {{ () => ret; }} = class Foo {{}}",)
+                        }
+                        TypeElement::Constructor(_) => {
+                            // TODO: Check # of parameters
+                            for rm in body {
+                                match rm {
+                                    ClassMember::Constructor(..) => continue 'l,
+                                    _ => {}
+                                }
+                            }
+
+                            errors.push(box Error::ConstructorRequired {
+                                span,
+                                lhs: lhs_span,
+                                rhs: rhs.span(),
+                            });
+                        }
+                        TypeElement::Property(p) => {
+                            //
+
+                            for rm in body {
+                                match rm {
+                                    ClassMember::Constructor(..) => continue 'l,
+                                    _ => {}
+                                }
+                            }
+                        }
+                        TypeElement::Method(_) => {
+                            unimplemented!("assign: interface {{ method() => ret; }} = class Foo {{}}")
+                        }
+                        TypeElement::Index(_) => {
+                            unimplemented!("assign: interface {{ [key: string]: Type; }} = class Foo {{}}")
+                        }
+                    }
+
+                    // TODO: missing fields
+                }
+
+                // Check class members
+                Type::ClassInstance(ClassInstance {
+                    ty: box Type::Class(Class { ref body, .. }),
+                    ..
+                }) => {
+                    match m {
+                        TypeElement::Call(_) => {
+                            unimplemented!("assign: interface {{ () => ret; }} = new Foo()")
+                        }
+                        TypeElement::Constructor(_) => {
+                            unimplemented!("assign: interface {{ new () => ret; }} = new Foo()")
+                        }
+                        TypeElement::Property(ref lp) => {
+                            for rm in body {
+                                match rm {
+                                    ClassMember::Property(ref rp) => {
+                                        match rp.accessibility {
+                                            Some(Accessibility::Private) | Some(Accessibility::Protected) => {
+                                                errors.push(box Error::AccessibilityDiffers { span });
+                                            }
+                                            _ => {}
+                                        }
+
+                                        if lp.key.type_eq(&rp.key) {
+                                            continue 'l;
+                                        }
+                                    }
+                                    _ => {}
+                                }
+                            }
+
+                            unimplemented!("assign: interface {{ prop: string; }} = new Foo()")
+                        }
+                        TypeElement::Method(_) => {
+                            unimplemented!("assign: interface {{ method() => ret; }} = new Foo()")
+                        }
+                        TypeElement::Index(_) => {
+                            unimplemented!("assign: interface {{ [key: string]: Type; }} = new Foo()")
+                        }
+                    }
+                    // TOOD: missing fields
+                }
+
+                Type::Tuple(..)
+                | Type::Array(..)
+                | Type::Lit(..)
+                | Type::Keyword(RTsKeywordType {
+                    kind: TsKeywordTypeKind::TsUndefinedKeyword,
+                    ..
+                })
+                | Type::Keyword(RTsKeywordType {
+                    kind: TsKeywordTypeKind::TsVoidKeyword,
+                    ..
+                }) => return Ok(()),
+
+                _ => {}
+            }
+        }
+
+        if !missing_fields.is_empty() {
+            errors.push(box Error::MissingFields {
+                span,
+                fields: missing_fields,
+            });
+        }
+
+        if !errors.is_empty() {
+            return Err(box Error::Errors {
+                span,
+                errors: errors.into(),
+            });
+        }
+
+        Ok(())
+    }
+
+    /// This method assigns each property to corresponding property.
+    pub(super) fn assign_type_elements_to_type_element(
+        &mut self,
+        opts: AssignOpts,
+        missing_fields: &mut Vec<TypeElement>,
+        m: &TypeElement,
+        rhs_members: &[TypeElement],
+    ) -> ValidationResult<()> {
+        let span = opts.span;
+        // We need this to show error if not all of rhs_member is matched
+
+        if let Some(l_key) = m.key() {
+            for rm in rhs_members {
+                if let Some(r_key) = rm.key() {
+                    if l_key.type_eq(&*r_key) {
+                        match m {
+                            TypeElement::Property(ref el) => match rm {
+                                TypeElement::Property(ref r_el) => {
+                                    self.assign_inner(
+                                        el.type_ann.as_ref().unwrap_or(&Type::any(span)),
+                                        r_el.type_ann.as_ref().unwrap_or(&Type::any(span)),
+                                        opts,
+                                    )?;
+                                    return Ok(());
+                                }
+                                _ => {}
+                            },
+
+                            // `foo(a: string) is assignable to foo(a: any)`
+                            TypeElement::Method(ref lm) => match rm {
+                                TypeElement::Method(ref rm) => {
+                                    //
+
+                                    if lm.params.len() != rm.params.len() {
+                                        return Err(box Error::Unimplemented {
+                                            span,
+                                            msg: format!(
+                                                "lhs.method.params.len() = {}; rhs.method.params.len() = {};",
+                                                lm.params.len(),
+                                                rm.params.len()
+                                            ),
+                                        });
+                                    }
+
+                                    for (lp, rp) in lm.params.iter().zip(rm.params.iter()) {
+                                        self.assign_inner(&lp.ty, &rp.ty, opts)
+                                            .context("tried to assign a method parameter to a method parameter")?;
+                                    }
+
+                                    return Ok(());
+                                }
+
+                                TypeElement::Property(rp) => {
+                                    // Allow assigning property with callable type to methods.
+                                    if let Some(rp_ty) = &rp.type_ann {
+                                        if let Type::Function(rp_ty) = rp_ty.normalize() {
+                                            if lm.params.len() != rp_ty.params.len() {
+                                                return Err(box Error::Unimplemented {
+                                                    span,
+                                                    msg: format!(
+                                                        "lhs.method.params.len() = {}; rhs.property.params.len() = {};",
+                                                        lm.params.len(),
+                                                        rp_ty.params.len()
+                                                    ),
+                                                });
+                                            }
+
+                                            for (lp, rp) in lm.params.iter().zip(rp_ty.params.iter()) {
+                                                self.assign_inner(&lp.ty, &rp.ty, opts).context(
+                                                    "tried to assign a parameter of a property with callable type to \
+                                                     a method parameter",
+                                                )?;
+                                            }
+
+                                            return Ok(());
+                                        }
+                                    }
+                                }
+                                _ => {}
+                            },
+                            _ => {}
+                        }
+                    }
+                }
+            }
+
+            match m {
+                TypeElement::Property(PropertySignature { optional: true, .. })
+                | TypeElement::Method(MethodSignature { optional: true, .. }) => {}
+                _ => {
+                    // No property with `key` found.
+                    missing_fields.push(m.clone());
+                }
+            }
+        } else {
+            match m {
+                // TODO: Check type of the index.
+                TypeElement::Index(..) => {
+                    // TODO: Verify
+                }
+                TypeElement::Call(..) => {
+                    //
+                    for rm in rhs_members {
+                        match rm {
+                            // TODO: Check type of parameters
+                            // TODO: Check return type
+                            TypeElement::Call(..) => return Ok(()),
+                            _ => {}
+                        }
+                    }
+
+                    missing_fields.push(m.clone());
+                }
+                _ => unreachable!(),
+            }
+        }
+
+        Ok(())
+    }
+
     pub(super) fn assign_class_members_to_type_element(
         &mut self,
         opts: AssignOpts,
