175e0ff94e024c2baeae151d451143a5d564017f
강동윤 <kdy1997.dev@gmail.com>
2021-02-07 18:05:46 +0900

    WIP: Destructuring

--- typescript/file_analyzer/src/analyzer/scope/mod.rs
+++ typescript/file_analyzer/src/analyzer/scope/mod.rs
@@ -25,8 +25,6 @@ use slog::Logger;
 use smallvec::SmallVec;
 use stc_ts_ast_rnode::RArrayPat;
 use stc_ts_ast_rnode::RAssignPatProp;
-use stc_ts_ast_rnode::RExpr;
-use stc_ts_ast_rnode::RIdent;
 use stc_ts_ast_rnode::RKeyValuePatProp;
 use stc_ts_ast_rnode::RObjectPat;
 use stc_ts_ast_rnode::RObjectPatProp;
@@ -43,13 +41,10 @@ use stc_ts_types::name::Name;
 use stc_ts_types::Array;
 use stc_ts_types::Intersection;
 use stc_ts_types::Key;
-use stc_ts_types::TypeParamInstantiation;
 use stc_ts_types::{
     Conditional, FnParam, Id, IndexedAccessType, Mapped, ModuleId, Operator, QueryExpr, QueryType, StaticThis,
     TupleElement, TypeParam,
 };
-use stc_ts_utils::OptionExt;
-use stc_ts_utils::PatExt;
 use stc_utils::TryOpt;
 use std::mem::replace;
 use std::mem::take;
@@ -61,6 +56,7 @@ use swc_ecma_ast::*;
 
 mod this;
 mod type_param;
+mod vars;
 
 macro_rules! no_ref {
     ($t:expr) => {{
@@ -576,196 +572,6 @@ impl Analyzer<'_, '_> {
 
     pub(super) fn declare_vars_inner(&mut self, kind: VarDeclKind, pat: &RPat, export: bool) -> ValidationResult<()> {
         self.declare_vars_inner_with_ty(kind, pat, export, None)
-    }
-
-    /// Updates variable list.
-    ///
-    /// This method should be called for function parameters including error
-    /// variable from a catch clause.
-    fn declare_vars_inner_with_ty(
-        &mut self,
-        kind: VarDeclKind,
-        pat: &RPat,
-        export: bool,
-        ty: Option<Box<Type>>,
-    ) -> ValidationResult<()> {
-        let span = ty
-            .as_ref()
-            .map(|v| v.span())
-            .and_then(|span| if span.is_dummy() { None } else { Some(span) })
-            .unwrap_or_else(|| pat.span());
-        if !self.is_builtin {
-            assert_ne!(span, DUMMY_SP);
-        }
-
-        match &*pat {
-            RPat::Ident(i) => {
-                let name: Id = Id::from(i.clone());
-                if !self.is_builtin {
-                    debug_assert_ne!(span, DUMMY_SP);
-                }
-                let ty = match ty {
-                    None => try_opt!(i.type_ann.as_ref().map(|v| v.type_ann.validate_with(self))),
-                    Some(ty) => Some(ty),
-                };
-                self.declare_var(
-                    span,
-                    kind,
-                    name.clone(),
-                    ty.clone(),
-                    // initialized
-                    true,
-                    // allow_multiple
-                    kind == VarDeclKind::Var,
-                )?;
-                if export {
-                    self.storage
-                        .store_private_var(self.ctx.module_id, name.clone(), ty.unwrap_or(Type::any(i.span)));
-                    self.storage.export_var(span, self.ctx.module_id, name);
-                }
-                return Ok(());
-            }
-            RPat::Assign(ref p) => {
-                slog::debug!(
-                    self.logger,
-                    "({}) declare_vars: Assign({:?}), ty = {:?}",
-                    self.scope.depth(),
-                    p.left,
-                    ty
-                );
-                self.declare_vars_inner_with_ty(kind, &p.left, export, ty)?;
-
-                return Ok(());
-            }
-
-            RPat::Array(RArrayPat {
-                span,
-                ref elems,
-                ref type_ann,
-                ref optional,
-                node_id,
-                ..
-            }) => {
-                // TODO: Handle type annotation
-
-                if type_ann.is_none() {
-                    if let Some(ty) = ty {
-                        if let Some(m) = &mut self.mutations {
-                            m.for_pats.entry(*node_id).or_default().optional = Some(true);
-                            m.for_pats
-                                .entry(*node_id)
-                                .or_default()
-                                .ty
-                                .fill_with(|| ty.generalize_lit().generalize_tuple().into());
-                        }
-                    }
-                }
-
-                if type_ann.is_none() {
-                    let ctxt = self.ctx.module_id;
-                    if let Some(m) = &mut self.mutations {
-                        //
-                        m.for_pats.entry(*node_id).or_default().ty.fill_with(|| {
-                            box Type::Ref(Ref {
-                                span: *span,
-                                ctxt,
-                                type_name: RTsEntityName::Ident(RIdent::new("Iterable".into(), DUMMY_SP)),
-                                type_args: Some(box TypeParamInstantiation {
-                                    span: *span,
-                                    params: vec![box Type::Keyword(RTsKeywordType {
-                                        span: DUMMY_SP,
-                                        kind: TsKeywordTypeKind::TsAnyKeyword,
-                                    })],
-                                }),
-                            })
-                        });
-                    }
-                }
-
-                for elem in elems.iter() {
-                    match *elem {
-                        Some(ref elem) => {
-                            self.declare_vars_inner(kind, elem, export)?;
-                        }
-                        // Skip
-                        None => {}
-                    }
-                }
-
-                return Ok(());
-            }
-
-            RPat::Object(RObjectPat {
-                ref props,
-                ref type_ann,
-                node_id,
-                ..
-            }) => {
-                if type_ann.is_none() {
-                    if let Some(m) = &mut self.mutations {
-                        m.for_pats.entry(*node_id).or_default().ty = Some(box Type::TypeLit(TypeLit {
-                            span,
-                            // TODO: Fill it
-                            members: vec![],
-                        }));
-                    }
-                }
-                for prop in props {
-                    match prop {
-                        RObjectPatProp::Assign(RAssignPatProp {
-                            span, key, value: None, ..
-                        }) => {
-                            if self.ctx.in_declare {
-                                self.storage
-                                    .report(box Error::DestructuringAssignInAmbientContext { span: *span });
-                            }
-
-                            unimplemented!("pattern in object pattern")
-                        }
-                        RObjectPatProp::KeyValue(RKeyValuePatProp { .. }) => {
-                            unimplemented!("key-value pattern in object pattern")
-                        }
-                        RObjectPatProp::Assign(RAssignPatProp { .. }) => {
-                            unimplemented!("assign pattern in object pattern")
-                        }
-                        RObjectPatProp::Rest(RRestPat { .. }) => {
-                            unimplemented!("rest pattern in object pattern")
-                        }
-                    }
-                }
-
-                return Ok(());
-            }
-
-            RPat::Rest(RRestPat {
-                ref arg,
-                type_ann: ref ty,
-                node_id,
-                ..
-            }) => {
-                let mut arg = arg.clone();
-
-                self.declare_vars_inner(kind, &arg, export)?;
-
-                let new_ty = arg.get_mut_ty().take();
-                if ty.is_none() {
-                    if let Some(arg_node_id) = arg.node_id() {
-                        if let Some(m) = &mut self.mutations {
-                            let ty = m.for_pats.entry(arg_node_id).or_default().ty.take();
-                            if let Some(ty) = ty {
-                                m.for_pats.entry(*node_id).or_default().ty = Some(ty);
-                            }
-                        }
-                    }
-                }
-
-                return Ok(());
-            }
-
-            RPat::Invalid(..) | RPat::Expr(box RExpr::Invalid(..)) => Ok(()),
-
-            _ => unimplemented!("declare_vars for patterns other than ident: {:#?}", pat),
-        }
     }
 
     pub(super) fn resolve_typeof(&mut self, span: Span, name: &RTsEntityName) -> ValidationResult {
