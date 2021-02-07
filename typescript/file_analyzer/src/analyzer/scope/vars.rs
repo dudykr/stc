28ded60efd086128e55585007312830599a1b530
강동윤 <kdy1997.dev@gmail.com>
2021-02-07 18:13:54 +0900

    Move some code

--- typescript/file_analyzer/src/analyzer/scope/vars.rs
+++ typescript/file_analyzer/src/analyzer/scope/vars.rs
@@ -176,6 +176,11 @@ impl Analyzer<'_, '_> {
                 node_id,
                 ..
             }) => {
+                if self.ctx.in_declare {
+                    self.storage
+                        .report(box Error::DestructuringAssignInAmbientContext { span });
+                }
+
                 let ty = match ty {
                     None => try_opt!(type_ann.as_ref().map(|v| v.type_ann.validate_with(self))),
                     Some(ty) => Some(ty),
@@ -196,11 +201,6 @@ impl Analyzer<'_, '_> {
                         RObjectPatProp::Assign(RAssignPatProp {
                             span, key, value: None, ..
                         }) => {
-                            if self.ctx.in_declare {
-                                self.storage
-                                    .report(box Error::DestructuringAssignInAmbientContext { span: *span });
-                            }
-
                             unimplemented!("pattern in object pattern")
                         }
                         RObjectPatProp::KeyValue(RKeyValuePatProp { .. }) => {
