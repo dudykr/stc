8b285666be6138fa1be90909f80d00d4c571cc20
강동윤 <kdy1997.dev@gmail.com>
2021-02-13 16:27:09 +0900

    fixup

--- typescript/type_checker/tests/visualize/types/interface/generic/contextual-1-1.ts
+++
@@ -1,5 +0,0 @@
-declare function wrap<A, B>(f: (a: A) => B): (a: A) => B;
-
-declare function list<T>(a: T): T[];
-
-export const f02: <A>(x: A) => A[] = wrap(list);
--- typescript/type_checker/tests/visualize/types/interface/generic/contextual-1-2.ts
+++
@@ -1,3 +0,0 @@
-declare function wrap<A, B>(f: (a: A) => B): (a: A) => B;
-
-export const f03: <A>(x: A) => A[] = wrap(x => [x]);
