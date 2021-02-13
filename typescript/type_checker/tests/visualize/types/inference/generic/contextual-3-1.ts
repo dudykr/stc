1748b5352c9a9a89d3f5e6aa7eeee10e5bc15859
강동윤 <kdy1997.dev@gmail.com>
2021-02-13 17:32:13 +0900

    Split visualization tests

--- typescript/type_checker/tests/visualize/types/inference/generic/contextual-3-1.ts
+++ typescript/type_checker/tests/visualize/types/inference/generic/contextual-3-1.ts
@@ -8,6 +8,3 @@ declare function identity<T>(x: T): T;
 const arrayMap = <T, U>(f: (x: T) => U) => (a: T[]) => a.map(f);
 
 export const f20: (a: string[]) => number[] = arrayMap(x => x.length);
-export const f21: <A>(a: A[]) => A[][] = arrayMap(x => [x]);
-export const f22: <A>(a: A[]) => A[] = arrayMap(identity);
-export const f23: <A>(a: A[]) => Box<A>[] = arrayMap(value => ({ value }));
