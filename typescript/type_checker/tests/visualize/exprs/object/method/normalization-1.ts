7014ae795419c15459d72c95aae3380474687017
강동윤 <kdy1997.dev@gmail.com>
2021-02-15 14:07:25 +0900

    Add a visualization

--- typescript/type_checker/tests/visualize/exprs/object/method/normalization-1.ts
+++ typescript/type_checker/tests/visualize/exprs/object/method/normalization-1.ts
@@ -1,6 +1,6 @@
-var a2: { <T>(x: T): number; (x: string): string; };
-var b2: { <T>(x: T): number; (x: string): string; };
-var c2: { (x: number): number; <T>(x: T): any; };
+var a2: { (x: string): string; };
+var b2: { (x: string): string; };
+var c2: { (x: number): number; };
 
 var z2 = [a2, b2, c2];
 var r6 = z2[0];
