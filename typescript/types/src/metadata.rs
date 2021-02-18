48ce66e1004615434c9c2bc273b80ae213dcd2e8
강동윤 <kdy1997.dev@gmail.com>
2021-02-19 01:00:52 +0900

    Doc

--- typescript/types/src/metadata.rs
+++ typescript/types/src/metadata.rs
@@ -58,36 +58,6 @@ macro_rules! impl_traits {
     };
 }
 
-/// # Rules
-///
-/// ## specified
-///
-/// `specified` for `a` in the code below should be changed to `false`
-///
-/// ```ts
-/// declare var a: { a: string }
-///
-/// const b = [a, {b: 'foo'}]
-/// ```
-///
-/// because `b` is normalized and so that the the code above is validated.
-///
-/// But it should be `true` for the code below (copied from
-/// `objectLiteralNormalization.ts`).
-///
-/// ```ts
-/// declare function f<T>(...items: T[]): T;
-/// declare let data: { a: 1, b: "abc", c: true };
-///
-/// // Object literals are inferred as a single normalized union type
-/// let e1 = f({ a: 1, b: 2 }, { a: "abc" }, {});
-/// let e2 = f({}, { a: "abc" }, { a: 1, b: 2 });
-/// let e3 = f(data, { a: 2 }); // error
-/// let e4 = f({ a: 2 }, data); // error
-/// ```
-///
-/// because tsc selects type of `data` instead of a normalized type ltieral
-/// union if one of inferred type literal is `specifiead`.
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
 pub struct TypeLitMetadata {
     /// `true` if a spread element is used while initializing.
@@ -97,6 +67,35 @@ pub struct TypeLitMetadata {
 
     /// `true` if a type literal is declared by user or created from other
     /// user-declared types like [crate::Interface] or etc..
+    ///
+    /// # Note
+    ///
+    /// `specified` for `a` in the code below should be changed to `false`
+    ///
+    /// ```ts
+    /// declare var a: { a: string }
+    ///
+    /// const b = [a, {b: 'foo'}]
+    /// ```
+    ///
+    /// because `b` is normalized and so that the the code above is validated.
+    ///
+    /// But it should be `true` for the code below (copied from
+    /// `objectLiteralNormalization.ts`).
+    ///
+    /// ```ts
+    /// declare function f<T>(...items: T[]): T;
+    /// declare let data: { a: 1, b: "abc", c: true };
+    ///
+    /// // Object literals are inferred as a single normalized union type
+    /// let e1 = f({ a: 1, b: 2 }, { a: "abc" }, {});
+    /// let e2 = f({}, { a: "abc" }, { a: 1, b: 2 });
+    /// let e3 = f(data, { a: 2 }); // error
+    /// let e4 = f({ a: 2 }, data); // error
+    /// ```
+    ///
+    /// because tsc selects type of `data` instead of a normalized type ltieral
+    /// union if one of inferred type literal is `specifiead`.
     pub specified: bool,
 }
 
