4b7421b5006d8030bbac64039b73ccf108b7c08a
강동윤 <kdy1997.dev@gmail.com>
2021-02-22 00:54:51 +0900

    Add a visualization

---
+++ typescript/type_checker/tests/visualize/controlFlow/guard/tpl-1.ts
@@ -0,0 +1,12 @@
+declare const envVar: string | undefined;
+if (typeof envVar === `string`) {
+    envVar.slice(0)
+}
+
+declare const obj: { test: string } | {}
+if (`test` in obj) {
+    obj.test.slice(0)
+}
+
+
+export { }
