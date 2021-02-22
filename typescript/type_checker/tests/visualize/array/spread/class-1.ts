a470be7f504767710d1cf8c081c75b41f0698bcc
강동윤 <kdy1997.dev@gmail.com>
2021-02-22 22:54:46 +0900

    Add a visualization

---
+++ typescript/type_checker/tests/visualize/array/spread/class-1.ts
@@ -0,0 +1,15 @@
+export class SymbolIterator {
+    next() {
+        return {
+            value: Symbol(),
+            done: false
+        };
+    }
+
+    [Symbol.iterator]() {
+        return this;
+    }
+}
+
+export var array: symbol[];
+array.concat([...new SymbolIterator]);
