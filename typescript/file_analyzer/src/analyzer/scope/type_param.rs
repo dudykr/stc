a4e0528e90b7308d4e4f02fe9e26ac4607291a19
강동윤 <kdy1997.dev@gmail.com>
2021-02-24 20:19:23 +0900

    replace_invalid_type_params: Determine type parameters to retain correctly.

--- typescript/file_analyzer/src/analyzer/scope/type_param.rs
+++ typescript/file_analyzer/src/analyzer/scope/type_param.rs
@@ -4,7 +4,7 @@ use rnode::Visit;
 use rnode::VisitMut;
 use rnode::VisitMutWith;
 use rnode::VisitWith;
-use stc_ts_generics::type_param::finder::TypeParamUsageFinder;
+use stc_ts_generics::type_param::finder::TypeParamDeclFinder;
 use stc_ts_types::Id;
 use stc_ts_types::Type;
 use stc_ts_types::TypeLit;
@@ -94,17 +94,17 @@ struct TypeParamEscapeHandler<'a, 'b, 'c> {
 
 impl VisitMut<Type> for TypeParamEscapeHandler<'_, '_, '_> {
     fn visit_mut(&mut self, ty: &mut Type) {
-        {
-            let mut v = TypeParamUsageFinder::default();
+        let declared = {
+            let mut v = TypeParamDeclFinder::default();
             ty.visit_with(&mut v);
-            self.declared.extend(v.params.into_iter().map(|v| v.name))
-        }
+            v.params
+        };
 
         {
             // Fast path
             let mut v = TypeParamEscapeVisitor {
                 analyzer: self.analyzer,
-                declared: &self.declared,
+                declared: &declared,
                 should_work: false,
             };
             ty.visit_with(&mut v);
