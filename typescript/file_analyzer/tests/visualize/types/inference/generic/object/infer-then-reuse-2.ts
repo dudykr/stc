9936e13f0995e48d2e6fe2c3181000ab725e11b9
강동윤 <kdy1997.dev@gmail.com>
2021-02-24 17:15:17 +0900

    Split

--- typescript/file_analyzer/tests/visualize/types/inference/generic/object/infer-then-reuse-2.ts
+++ typescript/file_analyzer/tests/visualize/types/inference/generic/object/infer-then-reuse-2.ts
@@ -1,21 +1,3 @@
-interface Computed<T> {
-    read(): T;
-    write(value: T);
-}
-
-function foo<T>(x: Computed<T>) { }
-
-var s: string;
-
-// Calls below should infer string for T and then assign that type to the value parameter
-foo({
-    read: () => s,
-    write: value => s = value
-});
-foo({
-    write: value => s = value,
-    read: () => s
-});
 
 enum E1 { X }
 enum E2 { X }
