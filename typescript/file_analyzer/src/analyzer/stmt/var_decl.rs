72f493d31f4bd979116d0a0a9034094266ba5b8e
강동윤 <kdy1997.dev@gmail.com>
2021-02-26 14:09:18 +0900

    Revert "Instantiate class types in var decl."
    This reverts commit 0ec22f2383b3bcb40fc79f0e55b8c9daaafa42da.

--- typescript/file_analyzer/src/analyzer/stmt/var_decl.rs
+++ typescript/file_analyzer/src/analyzer/stmt/var_decl.rs
@@ -552,12 +552,8 @@ impl Analyzer<'_, '_> {
 
                         if !self.is_builtin {
                             // Report error if type is not found.
-                            if let Some(t) = ty {
-                                ty = Some(
-                                    self.instantiate_class(&t)
-                                        .report(&mut self.storage)
-                                        .unwrap_or_else(|| t),
-                                );
+                            if let Some(ty) = &ty {
+                                self.expand_top_ref(v.span, Cow::Borrowed(ty)).report(&mut self.storage);
                             }
                         }
 
