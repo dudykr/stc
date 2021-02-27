2237ce16a5742f4df4d07ef8c7bac0a1097ba0f6
강동윤 <kdy1997.dev@gmail.com>
2021-02-27 21:46:36 +0900

    Expand more mapped types.

--- typescript/file_analyzer/src/analyzer/types/mapped.rs
+++ typescript/file_analyzer/src/analyzer/types/mapped.rs
@@ -1,5 +1,7 @@
 use crate::analyzer::Analyzer;
 use crate::ValidationResult;
+use stc_ts_ast_rnode::RTsLit;
+use stc_ts_ast_rnode::RTsLitType;
 use stc_ts_errors::debug::dump_type_as_string;
 use stc_ts_errors::DebugExt;
 use stc_ts_types::Key;
@@ -77,10 +79,100 @@ impl Analyzer<'_, '_> {
             _ => {}
         }
 
+        match m.type_param.constraint.as_deref() {
+            Some(constraint) => {
+                if let Some(keys) = self.convert_type_to_keys(span, constraint)? {
+                    // TODO: Verify that m.ty does not contain key type.
+                    let members = keys
+                        .into_iter()
+                        .map(|key| PropertySignature {
+                            span: key.span(),
+                            key,
+                            type_ann: m.ty.clone(),
+                            readonly: false,
+                            optional: false,
+                            params: Default::default(),
+                            type_params: Default::default(),
+                        })
+                        .map(TypeElement::Property)
+                        .map(|mut el| {
+                            self.apply_mapped_flags(&mut el, m.optional, m.readonly);
+                            el
+                        })
+                        .collect();
+
+                    return Ok(Type::TypeLit(TypeLit {
+                        span: m.span,
+                        members,
+                        metadata: Default::default(),
+                    }));
+                }
+            }
+            None => {}
+        }
+
         unimplemented!(
             "expand_mapped: {}",
             dump_type_as_string(&self.cm, &Type::Mapped(m.clone()))
         )
+    }
+
+    /// Evaluate a type and convert it to keys.
+    ///
+    /// Used for types like `'foo' | 'bar'` or alias of them.
+    fn convert_type_to_keys(&mut self, span: Span, ty: &Type) -> ValidationResult<Option<Vec<Key>>> {
+        let ty = ty.normalize();
+
+        match ty {
+            Type::Ref(..) => {
+                let ty = self.expand_top_ref(span, Cow::Borrowed(ty))?;
+                return self.convert_type_to_keys(span, &ty);
+            }
+
+            Type::Alias(alias) => return self.convert_type_to_keys(span, &alias.ty),
+
+            Type::Lit(RTsLitType { lit, .. }) => match lit {
+                RTsLit::BigInt(v) => return Ok(Some(vec![Key::BigInt(v.clone())])),
+                RTsLit::Number(v) => return Ok(Some(vec![Key::Num(v.clone())])),
+                RTsLit::Str(v) => {
+                    return Ok(Some(vec![Key::Normal {
+                        span: v.span,
+                        sym: v.value.clone(),
+                    }]))
+                }
+                RTsLit::Tpl(t) if t.quasis.len() == 1 => {
+                    return Ok(Some(vec![Key::Normal {
+                        span: t.span,
+                        sym: match &t.quasis[0].cooked {
+                            Some(v) => v.value.clone(),
+                            _ => return Ok(None),
+                        },
+                    }]))
+                }
+                RTsLit::Bool(_) | RTsLit::Tpl(_) => return Ok(None),
+            },
+
+            Type::Union(u) => {
+                let mut keys = vec![];
+
+                for ty in &u.types {
+                    let elem_keys = self.convert_type_to_keys(span, ty)?;
+                    match elem_keys {
+                        Some(v) => keys.extend(v),
+                        None => return Ok(None),
+                    }
+                }
+
+                return Ok(Some(keys));
+            }
+
+            Type::TypeLit(..) | Type::Interface(..) | Type::Class(..) | Type::ClassDef(..) => return Ok(None),
+
+            _ => {
+                slog::error!(self.logger, "unimplemented: convert_type_to_keys: {:#?}", ty);
+                return Ok(None);
+            }
+        }
     }
 
     /// Evaluates `keyof` operator.
@@ -92,6 +184,9 @@ impl Analyzer<'_, '_> {
                 let ty = self.expand_top_ref(span, Cow::Borrowed(ty))?;
                 return self.get_keys(span, &ty);
             }
+
+            Type::Alias(alias) => return self.get_keys(span, &alias.ty),
+
             Type::TypeLit(ty) => {
                 let mut keys = vec![];
                 for m in &ty.members {
