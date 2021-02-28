5146a442c2f031fde5abf286e237c4814a1b6910
강동윤 <kdy1997.dev@gmail.com>
2021-02-28 18:02:39 +0900

    fixup

--- typescript/type_checker/tests/conformance.rs
+++ typescript/type_checker/tests/conformance.rs
@@ -314,7 +314,7 @@ fn parse_test(file_name: &Path) -> Vec<TestSpec> {
         Ok(targets
             .into_iter()
             .map(|(target, specified)| {
-                let libs = if specified {
+                let libs = if specified && libs == vec![Lib::Es5, Lib::Dom] {
                     match target {
                         JscTarget::Es3 | JscTarget::Es5 => vec![Lib::Es5],
                         JscTarget::Es2015 => Lib::load("es2015"),
