c3c9b7b93e75dcf37e214c4118c61127b3be1736
강동윤 <kdy1997.dev@gmail.com>
2021-03-04 22:11:06 +0900

    Add tests

---
+++ typescript/file_analyzer/tests/pass/exprs/object/this/2.ts
@@ -0,0 +1,25 @@
+
+
+let mutuallyRecursive = {
+    a: 100,
+    start() {
+        return this.passthrough(this.a);
+    },
+    passthrough(n: number) {
+        return this.sub1(n);
+    },
+    sub1(n: number): number {
+        if (n > 0) {
+            return this.passthrough(n - 1);
+        }
+        return n;
+    }
+}
+export var i: number = mutuallyRecursive.start();
+interface I {
+    a: number;
+    start(): number;
+    passthrough(n: number): number;
+    sub1(n: number): number;
+}
+export var impl: I = mutuallyRecursive;
