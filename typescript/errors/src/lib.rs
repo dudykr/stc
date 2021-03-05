3e5b2606570ac2f7735437e9e2d917fc9fe7fcef
강동윤 <kdy1997.dev@gmail.com>
2021-03-05 20:04:14 +0900

    More error codes

--- typescript/errors/src/lib.rs
+++ typescript/errors/src/lib.rs
@@ -895,6 +895,7 @@ impl Error {
             Error::DuplicateName { .. } => 2300,
 
             Error::NoSuchVar { .. } => 2304,
+            Error::NoSuchType { .. } => 2304,
             Error::NoSuchVarButThisHasSuchProperty { .. } => 2663,
 
             Error::CannotAssignAbstractConstructorToNonAbstractConstructor { .. } => 2322,
