15a43e0cd03e38de1d9f195d9c67dbdf37470847
강동윤 <kdy1997.dev@gmail.com>
2021-03-06 00:25:16 +0900

    WIP: Types for `for-of`/`for-in`.

--- typescript/file_analyzer/src/analyzer/stmt/loops.rs
+++ typescript/file_analyzer/src/analyzer/stmt/loops.rs
@@ -1,40 +1,53 @@
 use super::super::Analyzer;
+use crate::validator::ValidateWith;
 use crate::{
     analyzer::{expr::TypeOfMode, ScopeKind},
-    ty::{Array, Type},
-    validator,
-    validator::ValidateWith,
-    ValidationResult,
+    ty::Type,
+    validator, ValidationResult,
 };
 use rnode::VisitWith;
 use stc_ts_ast_rnode::RExpr;
 use stc_ts_ast_rnode::RForInStmt;
 use stc_ts_ast_rnode::RForOfStmt;
 use stc_ts_ast_rnode::RPat;
-use stc_ts_ast_rnode::RVarDecl;
+use stc_ts_ast_rnode::RTsKeywordType;
 use stc_ts_ast_rnode::RVarDeclOrPat;
+use stc_ts_errors::DebugExt;
 use stc_ts_errors::Error;
 use stc_ts_file_analyzer_macros::extra_validator;
-use swc_common::{Span, Spanned};
+use stc_ts_types::Id;
+use stc_ts_utils::find_ids_in_pat;
+use std::borrow::Cow;
+use swc_common::Span;
+use swc_common::Spanned;
+use swc_ecma_ast::TsKeywordTypeKind;
+
+#[derive(Clone, Copy)]
+enum ForHeadKind {
+    In,
+    Of,
+}
 
 impl Analyzer<'_, '_> {
     #[extra_validator]
-    fn check_lhs_of_for_loop(&mut self, e: &RVarDeclOrPat) {
+    fn check_lhs_of_for_loop(&mut self, e: &RVarDeclOrPat, elem_ty: &Type, kind: ForHeadKind) {
         match *e {
             RVarDeclOrPat::VarDecl(ref v) => {
                 // Store variables
                 v.visit_with(self);
             }
-            RVarDeclOrPat::Pat(ref pat) => match pat {
-                RPat::Expr(ref e) => {
-                    e.validate_with_args(self, (TypeOfMode::LValue, None, None))?;
+            RVarDeclOrPat::Pat(ref pat) => {
+                match pat {
+                    RPat::Expr(ref e) => {
+                        e.validate_with_args(self, (TypeOfMode::LValue, None, None))?;
+                    }
+                    RPat::Ident(ref i) => {
+                        // TODO: verify
+                        self.type_of_var(&i.id, TypeOfMode::LValue, None)?;
+                    }
+                    _ => {}
                 }
-                RPat::Ident(ref i) => {
-                    // TODO: verify
-                    self.type_of_var(&i.id, TypeOfMode::LValue, None)?;
-                }
-                _ => {}
-            },
+            }
         }
     }
 
@@ -43,55 +56,57 @@ impl Analyzer<'_, '_> {
         e.validate_with_default(self)
     }
 
-    fn validate_for_loop(&mut self, span: Span, lhs: &RVarDeclOrPat, rty: Type) {
-        match lhs {
-            RVarDeclOrPat::Pat(RPat::Expr(l)) => {
-                let lty = match l.validate_with_args(self, (TypeOfMode::LValue, None, None)) {
-                    Ok(ty) => ty,
-                    Err(..) => return,
+    #[extra_validator]
+    fn check_for_of_in_loop(&mut self, span: Span, left: &RVarDeclOrPat, rhs: &RExpr, kind: ForHeadKind) {
+        self.with_child(
+            ScopeKind::Flow,
+            Default::default(),
+            |child: &mut Analyzer| -> ValidationResult<()> {
+                // Error should not be `no such var` if it's used in rhs.
+                let created_vars: Vec<Id> = match left {
+                    RVarDeclOrPat::VarDecl(v) => find_ids_in_pat(&v.decls),
+                    RVarDeclOrPat::Pat(_) => {
+                        vec![]
+                    }
+                };
+                debug_assert_eq!(child.scope.declaring, Vec::<Id>::new());
+                child.scope.declaring.extend(created_vars);
+
+                let rty = rhs
+                    .validate_with_default(child)
+                    .context("tried to validate rhs of a for in/of loop")?;
+
+                let elem_ty = match kind {
+                    ForHeadKind::Of => child
+                        .get_iterator_element_type(rhs.span(), Cow::Owned(rty))
+                        .context("tried to get the element type of an iterator to calculate type for a for-of loop")?,
+                    ForHeadKind::In => {
+                        let s = Type::Keyword(RTsKeywordType {
+                            span: rhs.span(),
+                            kind: TsKeywordTypeKind::TsStringKeyword,
+                        });
+                        let n = Type::Keyword(RTsKeywordType {
+                            span: rhs.span(),
+                            kind: TsKeywordTypeKind::TsNumberKeyword,
+                        });
+                        Cow::Owned(Type::union(vec![s, n]))
+                    }
                 };
 
-                match self.assign(
-                    &Type::Array(Array {
-                        span,
-                        elem_type: box lty,
-                    }),
-                    &rty,
-                    lhs.span(),
-                ) {
-                    Ok(..) => {}
-                    Err(err) => self.storage.report(err),
-                }
-            }
-            _ => {}
-        }
-    }
+                child.scope.declaring.clear();
 
-    #[extra_validator]
-    fn check_for_of_in_loop(&mut self, span: Span, left: &RVarDeclOrPat, rhs: &RExpr) {
-        self.with_child(ScopeKind::Flow, Default::default(), |child| -> ValidationResult<()> {
-            child.check_lhs_of_for_loop(left);
-            let rty = if match left {
-                RVarDeclOrPat::VarDecl(RVarDecl { ref decls, .. }) => !decls.is_empty(),
-                _ => true,
-            } {
-                child.check_rhs_of_for_loop(rhs)?
-            } else {
-                return Ok(());
-            };
+                child.check_lhs_of_for_loop(left, &elem_ty, kind);
 
-            child.validate_for_loop(span, &left, rty);
-
-            Ok(())
-        })?;
+                Ok(())
+            },
+        )?;
     }
 }
 
 #[validator]
 impl Analyzer<'_, '_> {
     fn validate(&mut self, s: &RForInStmt) {
-        s.left.visit_with(self);
-        self.check_for_of_in_loop(s.span, &s.left, &s.right);
+        self.check_for_of_in_loop(s.span, &s.left, &s.right, ForHeadKind::In);
 
         s.body.visit_with(self);
 
@@ -102,9 +117,7 @@ impl Analyzer<'_, '_> {
 #[validator]
 impl Analyzer<'_, '_> {
     fn validate(&mut self, s: &RForOfStmt) {
-        s.left.visit_with(self);
-
-        self.check_for_of_in_loop(s.span, &s.left, &s.right);
+        self.check_for_of_in_loop(s.span, &s.left, &s.right, ForHeadKind::Of);
 
         s.body.visit_with(self);
 
