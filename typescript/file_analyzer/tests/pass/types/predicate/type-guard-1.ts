3cce5e58886530905f06f8302954523573811585
강동윤 <kdy1997.dev@gmail.com>
2021-03-05 17:40:35 +0900

    Update test refs

--- typescript/file_analyzer/tests/pass/types/predicate/type-guard-1.ts
+++ typescript/file_analyzer/tests/pass/types/predicate/type-guard-1.ts
@@ -10,7 +10,7 @@ export var a: A;
 
 // The parameter index and argument index for the type guard target is matching.
 // The type predicate type is assignable to the parameter type.
-declare function isC_multipleParams(p1, p2): p1 is C;
+declare function isC_multipleParams(p1: any, p2: any): p1 is C;
 if (isC_multipleParams(a, 0)) {
     a;
 }
