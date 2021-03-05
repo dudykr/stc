2fa5d8052458f2e59a3b7eef13d6451776e55692
강동윤 <kdy1997.dev@gmail.com>
2021-03-05 17:38:53 +0900

    Update test refs

--- typescript/file_analyzer/tests/pass/types/generic/call-chain-1.ts
+++ typescript/file_analyzer/tests/pass/types/generic/call-chain-1.ts
@@ -1,7 +1,5 @@
 declare function absorb<T>(): T;
 declare const a: { m?<T>(obj: { x: T }): T } | undefined;
-export const n1: number = a?.m?.({ x: 12 }); // should be an error (`undefined` is not assignable to `number`)
-export const n2: number = a?.m?.({ x: absorb() }); // likewise
 export const n3: number | undefined = a?.m?.({ x: 12 }); // should be ok
 export const n4: number | undefined = a?.m?.({ x: absorb() }); // likewise
 
