e86acb9f1d9e725e2e5efba8156adb643c94cca7
강동윤 <kdy1997.dev@gmail.com>
2021-03-06 12:51:11 +0900

    Add some tests

---
+++ typescript/file_analyzer/tests/pass/exprs/arrow/contextualTyping/paren/1.ts
@@ -0,0 +1,5 @@
+type FuncType = (x: <T>(p: T) => T) => typeof x;
+
+export var lambda1: FuncType = x => { x<number>(undefined); return x; };
+export var lambda2: FuncType = (x => { x<number>(undefined); return x; });
+
