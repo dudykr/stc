7d9b9d669138e4b05a0f48c5b00673da37fea6f6
강동윤 <kdy1997.dev@gmail.com>
2021-03-06 15:46:50 +0900

    Add more tests

---
+++ typescript/file_analyzer/tests/pass/vars/forOf/hoisting/3.ts
@@ -0,0 +1,12 @@
+
+var b = a
+a;
+b;
+for (var a of [1, b, 3]) {
+    a;
+    b;
+}
+
+b;
+
+export { }
---
+++ typescript/file_analyzer/tests/pass/vars/forOf/hoisting/5.ts
@@ -0,0 +1,19 @@
+
+var b = a
+a;
+b;
+
+
+var c: string | number = Math.random() > 0.5 ? a : 123;
+
+for (var a of [1, c, 3]) {
+    a;
+    b;
+}
+
+b;
+
+
+
+
+export { }
