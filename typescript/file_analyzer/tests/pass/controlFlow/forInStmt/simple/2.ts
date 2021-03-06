ae85df3d66ded4a5571bbac25095df23135f4039
강동윤 <kdy1997.dev@gmail.com>
2021-03-06 12:10:07 +0900

    Fix a test

--- typescript/file_analyzer/tests/pass/controlFlow/forInStmt/simple/2.ts
+++ typescript/file_analyzer/tests/pass/controlFlow/forInStmt/simple/2.ts
@@ -2,7 +2,7 @@
 
 // Repro from #12513
 
-function f2<T>(obj: { [P in keyof T]: T[P] }, k: keyof T) {
+export function f2<T>(obj: { [P in keyof T]: T[P] }, k: keyof T) {
     const b = k in obj;
     let k1: keyof T;
     for (k1 in obj) {
