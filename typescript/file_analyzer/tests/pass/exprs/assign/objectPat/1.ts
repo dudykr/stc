dfce57a5322d10e5bd9f07fb101aaef159341d37
강동윤 <kdy1997.dev@gmail.com>
2021-03-11 13:16:35 +0900

    Harden

--- typescript/file_analyzer/tests/pass/exprs/assign/objectPat/1.ts
+++ typescript/file_analyzer/tests/pass/exprs/assign/objectPat/1.ts
@@ -7,6 +7,16 @@ function f1<T extends { a: string, b: number }>(obj: T) {
     let { 'a': a3, ...r3 } = obj;
     let { ['a']: a4, ...r4 } = obj;
     let { [a]: a5, ...r5 } = obj;
+
+    r0;
+    a1;
+    r1;
+    a2;
+    b2;
+    b2;
+    r3;
+    r4;
+    r5;
 }
 
 export { }
