a08319ef53f39772dbbcf94d10fb346eba9d37e1
강동윤 <kdy1997.dev@gmail.com>
2021-03-28 16:25:15 +0900

    WIP

--- typescript/file_analyzer/src/analyzer/function/return_type.rs
+++ typescript/file_analyzer/src/analyzer/function/return_type.rs
@@ -1,7 +1,21 @@
 use crate::analyzer::Analyzer;
 use crate::ValidationResult;
+use rnode::VisitMut;
+use rnode::VisitMutWith;
 use stc_ts_types::Type;
 
 impl Analyzer<'_, '_> {
-    pub(super) fn expand_return_type_of_fn(&mut self, ret_ty: &mut Type) -> ValidationResult<()> {}
+    pub(super) fn expand_return_type_of_fn(&mut self, ret_ty: &mut Type) -> ValidationResult<()> {
+        Ok(())
+    }
+}
+
+struct FnReturnTypeHandler<'a, 'b, 'c> {
+    analyzer: &'a mut Analyzer<'b, 'c>,
+}
+
+impl VisitMut<Type> for FnReturnTypeHandler<'_, '_, '_> {
+    fn visit_mut(&mut self, ty: &mut Type) {
+        ty.visit_mut_children_with(self);
+    }
 }
