08cf9739771dbb9ba82956aa37b1f36ca0d47219
강동윤 <kdy1997.dev@gmail.com>
2021-04-24 14:18:05 +0900

    type_element_to_class_member

--- typescript/file_analyzer/src/analyzer/decl_merging.rs
+++ typescript/file_analyzer/src/analyzer/decl_merging.rs
@@ -1,9 +1,44 @@
 use super::Analyzer;
 use crate::ValidationResult;
+use stc_ts_types::ClassMember;
+use stc_ts_types::ClassProperty;
 use stc_ts_types::Id;
+use stc_ts_types::Method;
 use stc_ts_types::Type;
+use stc_ts_types::TypeElement;
 
 impl Analyzer<'_, '_> {
+    fn type_element_to_class_member(&mut self, el: &TypeElement) -> ValidationResult<Option<ClassMember>> {
+        match el {
+            TypeElement::Call(_) => Ok(None),
+            TypeElement::Constructor(c) => Ok(Some(ClassMember::Constructor(c.clone()))),
+            TypeElement::Property(p) => Ok(Some(ClassMember::Property(ClassProperty {
+                span: p.span,
+                key: p.key.clone(),
+                value: p.type_ann.clone(),
+                is_static: false,
+                accessibility: p.accessibility,
+                is_abstract: false,
+                is_optional: p.optional,
+                readonly: p.readonly,
+                definite: false,
+            }))),
+            TypeElement::Method(m) => Ok(Some(ClassMember::Method(Method {
+                span: m.span,
+                accessibility: m.accessibility,
+                key: m.key.clone(),
+                is_static: false,
+                is_abstract: false,
+                is_optional: m.optional,
+                type_params: m.type_params.clone(),
+                params: m.params.clone(),
+                ret_ty: m.ret_ty.clone().unwrap_or_else(|| box Type::any(m.span)),
+                kind: swc_ecma_ast::MethodKind::Method,
+            }))),
+            TypeElement::Index(i) => Ok(Some(ClassMember::IndexSignature(i.clone()))),
+        }
+    }
+
     fn merge_from_to(&mut self, a: Type, b: Type) -> ValidationResult<Option<Type>> {
         debug_assert!(a.is_clone_cheap());
         debug_assert!(b.is_clone_cheap());
