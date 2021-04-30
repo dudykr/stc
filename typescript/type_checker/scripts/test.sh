87f106d7eb82871e70e1dc7bde488e7c94261643
강동윤 <kdy1997.dev@gmail.com>
2021-04-30 14:52:46 +0900

    test.sh: Logging

--- typescript/type_checker/scripts/test.sh
+++ typescript/type_checker/scripts/test.sh
@@ -4,6 +4,7 @@ set -eux
 ./scripts/sort.sh
 
 export RUST_BACKTRACE=1
+export RUST_LOG=debug
 
 # # We use visualizer to see actual types while working on conformance.
 # # So existence of failing visualization test typically means that we added a new fixture.
