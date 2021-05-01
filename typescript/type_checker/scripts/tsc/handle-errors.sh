f4fef7ef49df55f0d21daafc04bb2b177f2d2805
강동윤 <kdy1997.dev@gmail.com>
2021-05-01 11:57:15 +0900

    Ignore some tests

--- typescript/type_checker/scripts/tsc/handle-errors.sh
+++ typescript/type_checker/scripts/tsc/handle-errors.sh
@@ -4,3 +4,9 @@ set -eu
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ts-node $DIR/handle-errors.ts
+
+# Ignore tests with multiple result.
+find ./tests/conformance -name "*\(*" -print \
+    | sed 's!./tests/conformance/!!' \
+    | sed 's!(.*!!' \
+    >> tests/conformance.ignored.txt
