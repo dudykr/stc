90debf6a2977c36aeaf9c2eeffe054982b6ef95c
강동윤 <kdy1997.dev@gmail.com>
2021-05-05 21:41:00 +0900

    fixup

--- typescript/file_analyzer/tests/pass/vars/duplicate.ts
+++ typescript/file_analyzer/tests/pass/vars/duplicate.ts
@@ -7,11 +7,3 @@ function other<T>(x: T) {
     var r6 = foo((a: T) => a, (b: T) => b); // T => T
     var r6b = foo((a) => a, (b) => b); // {} => {}
 }
-
-function other2<T extends Date>(x: T) {
-    var r7 = foo((a: T) => a, (b: T) => b); // T => T
-    var r7b = foo((a) => a, (b) => b); // {} => {}
-    var r8 = r7(null);
-    // BUG 835518
-    //var r9 = r7(new Date());
-}
