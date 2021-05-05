2ac125ebef40099b57c34cc064379befc768bd90
강동윤 <kdy1997.dev@gmail.com>
2021-05-05 23:27:50 +0900

    Vis

--- typescript/file_analyzer/tests/pass/vars/duplicate.ts
+++
@@ -1,9 +0,0 @@
-function foo<T>(a: (x: T) => T, b: (x: T) => T) {
-    var r: (x: T) => T;
-    return r;
-}
-
-function other<T>(x: T) {
-    var r6 = foo((a: T) => a, (b: T) => b); // T => T
-    var r6b = foo((a) => a, (b) => b); // {} => {}
-}
