878c81b41271101bc9fca7577b2bb1f130655a65
강동윤 <kdy1997.dev@gmail.com>
2021-05-05 18:16:05 +0900

    sort.sh: Support tsc.wip.txt

--- typescript/type_checker/scripts/sort.sh
+++ typescript/type_checker/scripts/sort.sh
@@ -5,3 +5,6 @@ mv tests/conformance_sorted.txt tests/conformance.pass.txt
 
 cat tests/compiler.pass.txt | awk NF | sort | uniq | awk '{$1=$1};1' | uniq | sort > tests/compiler_sorted.txt
 mv tests/compiler_sorted.txt tests/compiler.pass.txt
+
+cat tests/tsc.wip.txt | awk NF | sort | uniq | awk '{$1=$1};1' | uniq | sort > tests/tmp.txt
+mv tests/tmp.txt tests/tsc.wip.txt
