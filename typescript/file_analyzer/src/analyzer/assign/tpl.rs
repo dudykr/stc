5326dda5709a1a9534a20eecc5ffbf4503506b61
강동윤 <kdy1997.dev@gmail.com>
2021-05-14 19:25:28 +0900

    WIP: assign to tpl

--- typescript/file_analyzer/src/analyzer/assign/tpl.rs
+++ typescript/file_analyzer/src/analyzer/assign/tpl.rs
@@ -1,11 +1,27 @@
 use super::AssignOpts;
 use crate::analyzer::Analyzer;
 use crate::ValidationResult;
+use stc_ts_ast_rnode::RTsLit;
+use stc_ts_ast_rnode::RTsLitType;
+use stc_ts_errors::Error;
 use stc_ts_types::TplType;
 use stc_ts_types::Type;
 
 impl Analyzer<'_, '_> {
     pub(crate) fn assign_to_tpl(&mut self, l: &TplType, r: &Type, opts: AssignOpts) -> ValidationResult<()> {
-        Ok(())
+        let span = opts.span;
+        let r = r.normalize();
+
+        match r {
+            Type::Lit(RTsLitType {
+                lit: RTsLit::Str(r), ..
+            }) => {
+                //
+
+                Ok(())
+            }
+
+            _ => Err(Error::SimpleAssignFailed { span }),
+        }
     }
 }
