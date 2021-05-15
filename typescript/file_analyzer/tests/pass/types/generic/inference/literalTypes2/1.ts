09408a16b57e1e429a439d1dabfba4ba7d1860b3
강동윤 <kdy1997.dev@gmail.com>
2021-05-15 12:42:26 +0900

    Add a vis

---
+++ typescript/file_analyzer/tests/pass/types/generic/inference/literalTypes2/1.ts
@@ -0,0 +1,13 @@
+
+function append<T>(a: T[], x: T): T[] {
+    let result = a.slice();
+    result.push(x);
+    return result;
+}
+
+type Bit = 0 | 1;
+
+let aa = makeArray<Bit>(0);
+aa = append(aa, 1);
+
+export { }
