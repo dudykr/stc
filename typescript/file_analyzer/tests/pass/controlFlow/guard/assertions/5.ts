77b4f462ab454d948bcc1d15e5f83c36f3bb467d
강동윤 <kdy1997.dev@gmail.com>
2021-05-22 18:29:37 +0900

    Split a vis

---
+++ typescript/file_analyzer/tests/pass/controlFlow/guard/assertions/5.ts
@@ -0,0 +1,39 @@
+// @strictNullChecks: true
+
+// Repro from #8513
+
+let cond: boolean;
+
+export type Optional<a> = Some<a> | None;
+
+export interface None { readonly none: string; }
+export interface Some<a> { readonly some: a; }
+
+export const none: None = { none: '' };
+
+export function isSome<a>(value: Optional<a>): value is Some<a> {
+    return 'some' in value;
+}
+
+function someFrom<a>(some: a) {
+    return { some };
+}
+
+export function fn<r>(makeSome: () => r): void {
+    let result: Optional<r> = none;
+    result;  // None
+    while (cond) {
+        result;  // Some<r> | None
+        result = someFrom(isSome(result) ? result.some : makeSome());
+        result;  // Some<r>
+    }
+}
+
+function f2() {
+    let x: string | number | undefined = undefined;
+    x;  // undefined
+    if (typeof x === "string") {
+        x;  // string (guard as assertion)
+    }
+    x;  // string | undefined
+}
---
+++ typescript/file_analyzer/tests/pass/controlFlow/guard/assertions/9.ts
@@ -0,0 +1,47 @@
+// @strictNullChecks: true
+
+// Repro from #8513
+
+let cond: boolean;
+
+export type Optional<a> = Some<a> | None;
+
+export interface None { readonly none: string; }
+export interface Some<a> { readonly some: a; }
+
+export const none: None = { none: '' };
+
+export function isSome<a>(value: Optional<a>): value is Some<a> {
+    return 'some' in value;
+}
+
+function someFrom<a>(some: a) {
+    return { some };
+}
+
+export function fn<r>(makeSome: () => r): void {
+    let result: Optional<r> = none;
+    result;  // None
+    while (cond) {
+        result;  // Some<r> | None
+        result = someFrom(isSome(result) ? result.some : makeSome());
+        result;  // Some<r>
+    }
+}
+
+function f6() {
+    let x: string | undefined | null;
+    x!.slice();
+    x = "";
+    x!.slice();
+    x = undefined;
+    x!.slice();
+    x = null;
+    x!.slice();
+    x = <undefined | null>undefined;
+    x!.slice();
+    x = <string | undefined>"";
+    x!.slice();
+    x = <string | null>"";
+    x!.slice();
+}
