43ce8de506ad9ab102506ee06507b34a535819a7
강동윤 <kdy1997.dev@gmail.com>
2021-05-23 23:05:09 +0900

    Visbility

--- typescript/file_analyzer/src/tests.rs
+++ typescript/file_analyzer/src/tests.rs
@@ -3,6 +3,6 @@ use once_cell::sync::Lazy;
 use std::sync::Arc;
 use swc_common::Globals;
 
-pub static GLOBALS: Lazy<Arc<Globals>> = Lazy::new(Default::default);
+pub(crate) static GLOBALS: Lazy<Arc<Globals>> = Lazy::new(Default::default);
 
-pub static MARKS: Lazy<Marks> = Lazy::new(|| Marks::new(&*GLOBALS));
+pub(crate) static MARKS: Lazy<Marks> = Lazy::new(|| Marks::new(&*GLOBALS));
