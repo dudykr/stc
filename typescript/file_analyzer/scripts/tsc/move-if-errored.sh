296b5cbad36c0c1e80997bfaee2fb5ec3ed587f1
강동윤 <kdy1997.dev@gmail.com>
2021-05-23 19:07:11 +0900

    Add a script to check if a pass test is correct

---
+++ typescript/file_analyzer/scripts/tsc/move-if-errored.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -eu
+
+
+TEST_FILES=$(find ./tests/pass -type f -name "*.ts")
+
+for F in $TEST_FILES
+do
+    if tsc --strict --noEmit $F ; then
+        echo "$F is a valid pass test"
+    else
+        ./scripts/allow-error.sh $F
+    fi
+done
+
+
