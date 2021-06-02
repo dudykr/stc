611139526beec8115d3fe83def873d261808f8e8
강동윤 <kdy1997.dev@gmail.com>
2021-06-02 18:47:16 +0900

    Fix inference of array and type literals

--- typescript/file_analyzer/src/analyzer/generic/type_form.rs
+++ typescript/file_analyzer/src/analyzer/generic/type_form.rs
@@ -9,12 +9,18 @@ use stc_ts_types::Type;
 pub(super) enum TypeForm {
     Ref(Name),
     Other,
+    Array(Box<TypeForm>),
+    TypeLit { prop_count: usize },
 }
 
 impl From<&Type> for TypeForm {
     fn from(ty: &Type) -> Self {
         match ty.normalize() {
             Type::Ref(r) => Self::Ref(r.type_name.clone().into()),
+            Type::Array(ty) => Self::Array(box Self::from(&*ty.elem_type)),
+            Type::TypeLit(ty) => Self::TypeLit {
+                prop_count: ty.members.len(),
+            },
             _ => Self::Other,
         }
     }
