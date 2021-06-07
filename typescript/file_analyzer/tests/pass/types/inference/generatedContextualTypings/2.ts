8a1ff8610946a5554776337f576fb9a557ad2052
강동윤 <kdy1997.dev@gmail.com>
2021-06-07 18:53:52 +0900

    Fix visualization tests

--- typescript/file_analyzer/tests/pass/types/inference/generatedContextualTypings/2.ts
+++ typescript/file_analyzer/tests/pass/types/inference/generatedContextualTypings/2.ts
@@ -1,2 +1,10 @@
+class Base { private p; }
+class Derived1 extends Base { private m; }
+class Derived2 extends Base { private n; }
+
+var b = new Base(), d1 = new Derived1(), d2 = new Derived2();
+interface Genric<T> { func(n: T[]); }
+
+
 export class x95 { constructor(parm: (s: Base[]) => any = n => { var n: Base[]; return null; }) { } }
 export class x96 { constructor(parm: Genric<Base> = { func: n => { return [d1, d2]; } }) { } }
--- typescript/file_analyzer/tests/pass/types/inference/generatedContextualTypings/4.ts
+++ typescript/file_analyzer/tests/pass/types/inference/generatedContextualTypings/4.ts
@@ -1,2 +1,9 @@
+class Base { private p; }
+class Derived1 extends Base { private m; }
+class Derived2 extends Base { private n; }
+
+var b = new Base(), d1 = new Derived1(), d2 = new Derived2();
+interface Genric<T> { func(n: T[]); }
+
 export function x131(parm: (s: Base[]) => any = n => { var n: Base[]; return null; }) { }
 export function x132(parm: Genric<Base> = { func: n => { return [d1, d2]; } }) { }
