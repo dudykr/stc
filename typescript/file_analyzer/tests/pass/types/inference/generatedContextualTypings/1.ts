a12c354e276dddcb6b7bcc0767ff9fba612a9b26
강동윤 <kdy1997.dev@gmail.com>
2021-06-07 18:38:54 +0900

    Add a vis

---
+++ typescript/file_analyzer/tests/pass/types/inference/generatedContextualTypings/1.ts
@@ -0,0 +1,3 @@
+export class x87 { constructor(parm: () => Base[] = function named() { return [d1, d2] }) { } }
+export class x90 { constructor(parm: { (): Base[]; } = function named() { return [d1, d2] }) { } }
+
