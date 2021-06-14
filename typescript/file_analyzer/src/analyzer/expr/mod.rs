7c6b139d1f68e26829cb3a3ebac51d194aa40dff
강동윤 <kdy1997.dev@gmail.com>
2021-06-14 13:52:17 +0900

    Don't mix computed keys with normal keys

--- typescript/file_analyzer/src/analyzer/expr/mod.rs
+++ typescript/file_analyzer/src/analyzer/expr/mod.rs
@@ -605,17 +605,6 @@ impl Analyzer<'_, '_> {
 
     pub(crate) fn validate_key(&mut self, prop: &RExpr, computed: bool) -> ValidationResult<Key> {
         if computed {
-            match prop {
-                RExpr::Lit(RLit::Str(s)) => {
-                    return Ok(Key::Normal {
-                        span: s.span,
-                        sym: s.value.clone(),
-                    })
-                }
-                RExpr::Lit(RLit::Num(n)) => return Ok(Key::Num(n.clone())),
-                _ => {}
-            }
-
             prop.validate_with_default(self)
                 .and_then(|ty| self.expand_top_ref(ty.span(), Cow::Owned(ty)).map(Cow::into_owned))
                 .and_then(|ty| self.expand_enum(ty))
@@ -1029,6 +1018,16 @@ impl Analyzer<'_, '_> {
                         type_mode,
                         id_ctx,
                     ) {
+                        return Ok(ty);
+                    }
+                }
+
+                Type::Lit(RTsLitType {
+                    lit: RTsLit::Number(n), ..
+                }) => {
+                    // As some types has rules about computed propeties, we use the result only if
+                    // it sucesses.
+                    if let Ok(ty) = self.access_property(span, obj, &Key::Num(n.clone()), type_mode, id_ctx) {
                         return Ok(ty);
                     }
                 }
