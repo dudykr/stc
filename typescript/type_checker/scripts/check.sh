6b80b9a5441525646ebabb0c4761c1729d22fda4
강동윤 <kdy1997.dev@gmail.com>
2021-06-17 21:47:42 +0900

    check.sh: Use visualization tests

--- typescript/type_checker/scripts/check.sh
+++ typescript/type_checker/scripts/check.sh
@@ -9,6 +9,9 @@ set -eu
 
 export CARGO_TERM_COLOR=always
 
+# We prevent regression using faster checks
+GOLDEN_ONLY=1 cargo test -q -p stc_ts_file_analyzer --test visualize
+
 TEST='' cargo test --test tsc \
   | tee /dev/stderr \
   | grep 'ts .\.\. ok$' \
