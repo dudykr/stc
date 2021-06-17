d78970ca609a6652be986b87c14dbe82bff49156
강동윤 <kdy1997.dev@gmail.com>
2021-06-18 00:13:20 +0900

    test.sh: Use -q for visualization tests

--- typescript/type_checker/scripts/test.sh
+++ typescript/type_checker/scripts/test.sh
@@ -7,7 +7,7 @@ export RUST_BACKTRACE=1
 export RUST_LOG=debug
 
 # We prevent regression using faster checks
-GOLDEN_ONLY=1 cargo test -p stc_ts_file_analyzer --test visualize
+GOLDEN_ONLY=1 cargo test -q -p stc_ts_file_analyzer --test visualize
 
 WIP_STATS=1 cargo test --color always -q --test tsc
 
