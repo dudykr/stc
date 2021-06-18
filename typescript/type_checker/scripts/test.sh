4d11e3032923eeac38c5eb17a90c6f376774c3a1
강동윤 <kdy1997.dev@gmail.com>
2021-06-18 13:19:26 +0900

    test.sh: Add an error handler

--- typescript/type_checker/scripts/test.sh
+++ typescript/type_checker/scripts/test.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 set -eux
 
+err_handler () {
+   if command -v osascript &> /dev/null
+  then
+      osascript -e 'display notification "Test failed!"'
+  fi
+   exit
+}
+
+trap err_handler ERR
+
 ./scripts/sort.sh
 
 export RUST_BACKTRACE=1
