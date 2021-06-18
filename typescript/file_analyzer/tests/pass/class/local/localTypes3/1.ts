78a8abb1762f288950b3ec5df66f581da49b6962
강동윤 <kdy1997.dev@gmail.com>
2021-06-18 16:47:19 +0900

    Add a vis

---
+++ typescript/file_analyzer/tests/pass/class/local/localTypes3/1.ts
@@ -0,0 +1,12 @@
+function f<X>(x: X) {
+    class C<Y> {
+        public x = x;
+        constructor(public y: Y) { }
+    }
+    return C;
+}
+let C = f(10);
+let v = new C("hello");
+
+
+export { }
