9c08aaf9673913520c275c0f174c1e670e922d10
강동윤 <kdy1997.dev@gmail.com>
2021-06-19 23:11:25 +0900

    Some work for index signatures

--- typescript/file_analyzer/src/analyzer/types/index_signature.rs
+++ typescript/file_analyzer/src/analyzer/types/index_signature.rs
@@ -1,3 +1,32 @@
-use crate::analyzer::Analyzer;
+use crate::{analyzer::Analyzer, ValidationResult};
+use stc_ts_errors::{debug::dump_type_as_string, DebugExt};
+use stc_ts_types::{ClassDef, IndexSignature, Type};
+use std::borrow::Cow;
+use swc_common::Span;
 
-impl Analyzer<'_, '_> {}
+impl Analyzer<'_, '_> {
+    pub(crate) fn get_index_signature(&mut self, span: Span, ty: &Type) -> ValidationResult<Option<IndexSignature>> {
+        (|| -> ValidationResult<_> {
+            let ty = self.normalize(Some(span), Cow::Borrowed(ty), Default::default())?;
+
+            match &*ty {
+                Type::ClassDef(cls) => return self.get_index_signature_from_class(span, &cls),
+                _ => Ok(None),
+            }
+        })()
+        .with_context(|| {
+            format!(
+                "tried to get index signature of '{}'",
+                dump_type_as_string(&self.cm, &ty)
+            )
+        })
+    }
+
+    pub(crate) fn get_index_signature_from_class(
+        &mut self,
+        span: Span,
+        class: &ClassDef,
+    ) -> ValidationResult<Option<IndexSignature>> {
+        Ok(None)
+    }
+}
