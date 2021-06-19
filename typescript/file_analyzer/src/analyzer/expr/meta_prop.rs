bdc93c5171d371a196da3dc39f4572b85cfaf0c0
강동윤 <kdy1997.dev@gmail.com>
2021-06-19 13:28:54 +0900

    new.target

--- typescript/file_analyzer/src/analyzer/expr/meta_prop.rs
+++ typescript/file_analyzer/src/analyzer/expr/meta_prop.rs
@@ -1,11 +1,23 @@
 use crate::{analyzer::Analyzer, ValidationResult};
-use stc_ts_ast_rnode::RMetaPropExpr;
+use stc_ts_ast_rnode::{RIdent, RMetaPropExpr};
 use stc_ts_file_analyzer_macros::validator;
+use stc_ts_types::Type;
+use swc_atoms::js_word;
 
 #[validator]
 impl Analyzer<'_, '_> {
     fn validate(&mut self, e: &RMetaPropExpr) -> ValidationResult {
         match (&e.meta, &e.prop) {
+            (
+                RIdent {
+                    sym: js_word!("new"), ..
+                },
+                RIdent {
+                    sym: js_word!("target"),
+                    ..
+                },
+            ) => return Ok(Type::any(e.meta.span)),
+
             _ => {
                 todo!("Unsupported meta property {:?}", e)
             }
