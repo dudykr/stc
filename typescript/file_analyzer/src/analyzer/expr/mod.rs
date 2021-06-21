3401643ada65465f4d9451934ad912cc6e5f88e5
강동윤 <kdy1997.dev@gmail.com>
2021-06-21 11:25:21 +0900

    Fix abstract method check

--- typescript/file_analyzer/src/analyzer/expr/mod.rs
+++ typescript/file_analyzer/src/analyzer/expr/mod.rs
@@ -1534,12 +1534,12 @@ impl Analyzer<'_, '_> {
                                 return Ok(Type::any(span));
                             }
 
-                            if mtd.is_abstract {
-                                self.storage.report(Error::CannotAccessAbstractMemeber { span });
-                                return Ok(Type::any(span));
-                            }
-
                             if self.key_matches(span, &mtd.key, prop, false) {
+                                if mtd.is_abstract {
+                                    self.storage.report(Error::CannotAccessAbstractMemeber { span });
+                                    return Ok(Type::any(span));
+                                }
+
                                 return Ok(Type::Function(stc_ts_types::Function {
                                     span: mtd.span,
                                     type_params: mtd.type_params.clone(),
