36b630823f4afb1b213bcf1d2c980ebf5e3701f0
강동윤 <kdy1997.dev@gmail.com>
2021-06-24 14:43:03 +0900

    Add visulaizations

---
+++ typescript/file_analyzer/tests/pass/types/predicate/controlFlowOptionalChain/2.ts
@@ -0,0 +1,19 @@
+declare const o2: { f(x: any): x is number; } | undefined;
+if (o2?.f(x)) {
+    x; // number
+    o2.f; // (x: any) => x is number
+    o2?.f;
+    o2?.f(x);
+}
+else {
+    x;
+    o2;
+    o2?.f;
+    o2.f;
+}
+x;
+o2;
+o2?.f;
+o2.f;
+
+export { }
