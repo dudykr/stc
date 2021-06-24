b3b1e639d0e795921625379fd36a7ff27a47cd00
강동윤 <kdy1997.dev@gmail.com>
2021-06-24 15:41:01 +0900

    Disable logs of swc_common

--- typescript/type_checker/scripts/test.sh
+++ typescript/type_checker/scripts/test.sh
@@ -11,7 +11,7 @@ trap err_handler ERR
 ./scripts/sort.sh
 
 export RUST_BACKTRACE=1
-export RUST_LOG=debug
+export RUST_LOG=debug,swc_common=off
 
 # We prevent regression using faster checks
 GOLDEN_ONLY=1 cargo test -q -p stc_ts_file_analyzer --test visualize
