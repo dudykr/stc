242a16f45cd7ab6a21793ea82064403ac648be23
강동윤 <kdy1997.dev@gmail.com>
2021-06-27 15:10:00 +0900

    Add ExpandGenericOpts

--- typescript/file_analyzer/generics/src/lib.rs
+++ typescript/file_analyzer/generics/src/lib.rs
@@ -1,4 +1,14 @@
 #![feature(specialization)]
 #![allow(incomplete_features)]
 
+use stc_ts_types::Key;
+
 pub mod type_param;
+
+#[derive(Debug, Clone, Copy, Default)]
+pub struct ExpandGenericOpts<'a> {
+    /// If this field is empty, all properites will be expanded.
+    ///
+    /// [Key::Computed] is not allowed.
+    pub props: &'a [Key],
+}
