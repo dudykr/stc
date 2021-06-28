1787b582f9d8c5de281c40bf859653f340c41438
강동윤 <kdy1997.dev@gmail.com>
2021-06-28 13:44:12 +0900

    DONT_PRINT_MATCHED

--- typescript/type_checker/scripts/check.sh
+++ typescript/type_checker/scripts/check.sh
@@ -19,7 +19,7 @@ export CARGO_TERM_COLOR=always
 # We prevent regression using faster checks
 GOLDEN_ONLY=1 cargo test -q -p stc_ts_file_analyzer --test visualize
 
-RUST_LOG=0 TEST='' cargo test --test tsc \
+RUST_LOG=off TEST='' DONT_PRINT_MATCHED=1 cargo test --test tsc \
   | tee /dev/stderr \
   | grep 'ts .\.\. ok$' \
   | sed -e 's!test conformance::!!' \
