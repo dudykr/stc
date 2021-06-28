d0803e7d85dde34a105bc3ee2ebe318896842feb
강동윤 <kdy1997.dev@gmail.com>
2021-06-28 19:59:41 +0900

    lspower

--- typescript/lang-server/src/main.rs
+++ typescript/lang-server/src/main.rs
@@ -1,1 +1,30 @@
-pub fn main() {}
+use lspower::{jsonrpc::Result, lsp::*, Client, LanguageServer, LspService, Server};
+
+#[derive(Debug)]
+struct Backend {
+    client: Client,
+}
+
+#[lspower::async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult::default())
+    }
+
+    async fn initialized(&self, _: InitializedParams) {
+        self.client.log_message(MessageType::Info, "server initialized!").await;
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+
+    let (service, messages) = LspService::new(|client| Backend { client });
+    Server::new(stdin, stdout).interleave(messages).serve(service).await;
+}
