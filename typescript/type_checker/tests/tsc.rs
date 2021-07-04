4a3d9612ee458799c0dfe223153c544b4898fb63
강동윤 <kdy1997.dev@gmail.com>
2021-07-04 15:36:24 +0900

    Trace time used for clone

--- typescript/type_checker/tests/tsc.rs
+++ typescript/type_checker/tests/tsc.rs
@@ -503,6 +503,7 @@ fn do_test(file_name: &Path) -> Result<(), StdErr> {
     let _tracing = tracing::subscriber::set_default(
         tracing_subscriber::FmtSubscriber::builder()
             .without_time()
+            .with_max_level(tracing::Level::TRACE)
             .with_ansi(true)
             .with_test_writer()
             .finish(),
