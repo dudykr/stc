094e951a656c395b3102d3a0758920fbd6d83c29
강동윤 <kdy1997.dev@gmail.com>
2021-07-06 23:30:00 +0900

    fixup

--- typescript/file_analyzer/tests/builtin.rs
+++ typescript/file_analyzer/tests/builtin.rs
@@ -55,6 +55,44 @@ pub fn builtin() {
                 assert_eq!(keyed_item_count, 10);
             }
 
+            Ok(())
+        })
+    })
+    .unwrap();
+}
+
+#[test]
+pub fn intl() {
+    testing::run_test2(false, |_, _| {
+        let globals = Arc::new(Globals::default());
+
+        GLOBALS.set(&globals, || {
+            let log = logger();
+            let shared = StableEnv::new(slog::Logger::root(slog::Discard, slog::o!()), globals.clone());
+            let mut libs = vec![];
+            for s in &[
+                "es2020.full",
+                "es2019.full",
+                "es2018.full",
+                "es2017.full",
+                "es2016.full",
+                "es2015.full",
+                "es5.full",
+            ] {
+                libs.extend(Lib::load(&s));
+            }
+            libs.sort();
+            libs.dedup();
+            let data = BuiltIn::from_ts_libs(&shared, &libs);
+
+            let env = Env::new(
+                shared,
+                Default::default(),
+                swc_ecma_ast::EsVersion::Es2020,
+                ModuleConfig::None,
+                Arc::new(data),
+            );
+
             {
                 let intl = env
                     .get_global_type(DUMMY_SP, &"Intl".into())
