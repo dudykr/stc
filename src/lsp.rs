a1bc9e9f5a69755c74c29a1c69a1a40e442a4690
강동윤 <kdy1997.dev@gmail.com>
2021-07-08 00:33:23 +0900

    feat(ts/lsp): Groundwork for a language server (#61)

---
+++ src/lsp.rs
@@ -0,0 +1,21 @@
+use lspower::{LspService, Server};
+use stc_ts_lang_server::LspBackend;
+use structopt::StructOpt;
+use tracing::{info, instrument};
+
+#[derive(Debug, StructOpt)]
+#[structopt(rename_all = "camel-case")]
+pub struct LspCommand {}
+
+impl LspCommand {
+    #[instrument(skip(self))]
+    pub async fn run(self) {
+        info!("Starting server");
+
+        let stdin = tokio::io::stdin();
+        let stdout = tokio::io::stdout();
+
+        let (service, messages) = LspService::new(|client| LspBackend { client });
+        Server::new(stdin, stdout).interleave(messages).serve(service).await;
+    }
+}
