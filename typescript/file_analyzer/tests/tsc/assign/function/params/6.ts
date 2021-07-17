738a8d001383df8499d5fe9c5d6203340afb8577
강동윤 <kdy1997.dev@gmail.com>
2021-07-17 17:10:55 +0900

    Harden tests

--- typescript/file_analyzer/tests/tsc/assign/function/params/6.ts
+++ typescript/file_analyzer/tests/tsc/assign/function/params/6.ts
@@ -4,6 +4,6 @@ class Derived extends Base { bar: string; }
 var a6: <T extends Base>(x: (arg: T) => Derived) => T;
 var b6: <T extends Base, U extends Derived>(x: (arg: T) => U) => T;
 b6 = a6; // ok
-
+a6 = b6
 
 export { }
--- typescript/file_analyzer/tests/tsc/assign/function/params/8.ts
+++ typescript/file_analyzer/tests/tsc/assign/function/params/8.ts
@@ -4,6 +4,7 @@ class Derived extends Base { bar: string; }
 var a6: <T extends Base>(arg: T) => Derived
 var b6: <T extends Base, U extends Derived>(arg: T) => U
 b6 = a6; // ok
+a6 = b6;
 
 
 export { }
