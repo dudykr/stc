7df191325f570e55aa1e980bef1101dcbd96de43
강동윤 <kdy1997.dev@gmail.com>
2021-07-17 17:10:18 +0900

    Add a test

---
+++ typescript/file_analyzer/tests/tsc/assign/function/params/8.ts
@@ -0,0 +1,9 @@
+class Base { foo: string; }
+class Derived extends Base { bar: string; }
+
+var a6: <T extends Base>(arg: T) => Derived
+var b6: <T extends Base, U extends Derived>(arg: T) => U
+b6 = a6; // ok
+
+
+export { }
