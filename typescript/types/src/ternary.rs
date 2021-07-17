68d63b3b84f3fef1910e6c56a0111573bceaad23
강동윤 <kdy1997.dev@gmail.com>
2021-07-18 00:53:51 +0900

    fixup

--- typescript/types/src/ternary.rs
+++ typescript/types/src/ternary.rs
@@ -1,6 +1,6 @@
 /// Ported from `Ternary` of `tsc`.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub enum TscTerany {
+pub enum TscTernary {
     False,
     Unknown,
     Maybe,
