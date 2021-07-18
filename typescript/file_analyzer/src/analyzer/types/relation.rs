e3be79af86219a08e33e624a4f740ff5694b62c3
강동윤 <kdy1997.dev@gmail.com>
2021-07-18 16:08:11 +0900

    More work for relations

--- typescript/file_analyzer/src/analyzer/types/relation.rs
+++ typescript/file_analyzer/src/analyzer/types/relation.rs
@@ -1,7 +1,10 @@
+use std::borrow::Cow;
+
 use crate::{analyzer::Analyzer, ValidationResult};
 use fxhash::FxHashSet;
-use stc_ts_types::{ternary::TscTernary, Operator, Type};
+use stc_ts_types::{ternary::TscTernary, Operator, Type, TypeParam};
 use swc_atoms::JsWord;
+use swc_common::{Span, TypeEq};
 use swc_ecma_ast::TsTypeOperatorOp;
 use tracing::instrument;
 
@@ -9,6 +12,7 @@ use tracing::instrument;
 pub enum RelationCheckMode {
     Identity,
     Subtype,
+    Comparable,
     Assignability,
 }
 
@@ -21,7 +25,7 @@ impl Default for RelationCheckMode {
 #[derive(Debug, Clone, Copy, Default)]
 pub struct TypeRelatedToOpts {
     pub mode: RelationCheckMode,
-    pub report_errros: bool,
+    pub report_errors: bool,
     pub intesection_state: TscIntersectionState,
 }
 
@@ -32,9 +36,10 @@ pub struct TscIntersectionState {
 
 impl Analyzer<'_, '_> {
     /// Ported from `structuredTypeRelatedTo` of `tsc`.
-    #[instrument(skip(self, source, target, opts))]
+    #[instrument(skip(self, span, source, target, opts))]
     pub(crate) fn structured_type_related_to(
         &mut self,
+        span: Span,
         source: &Type,
         target: &Type,
         opts: TypeRelatedToOpts,
@@ -67,10 +72,11 @@ impl Analyzer<'_, '_> {
                         }),
                     ) => {
                         return self.is_related_to(
+                            span,
                             source,
                             target,
                             TypeRelatedToOpts {
-                                report_errros: false,
+                                report_errors: false,
                                 ..opts
                             },
                         )
@@ -82,19 +88,21 @@ impl Analyzer<'_, '_> {
                 match (source.normalize(), target.normalize()) {
                     (Type::IndexedAccessType(source), Type::IndexedAccessType(target)) => {
                         result = self.is_related_to(
+                            span,
                             &source.obj_type,
                             &target.obj_type,
                             TypeRelatedToOpts {
-                                report_errros: false,
+                                report_errors: false,
                                 ..opts
                             },
                         )?;
                         if result.as_bool() {
                             result &= self.is_related_to(
+                                span,
                                 &source.obj_type,
                                 &target.obj_type,
                                 TypeRelatedToOpts {
-                                    report_errros: false,
+                                    report_errors: false,
                                     ..opts
                                 },
                             )?;
@@ -110,39 +118,43 @@ impl Analyzer<'_, '_> {
                 match (source.normalize(), target.normalize()) {
                     (Type::Conditional(source), Type::Conditional(target)) => {
                         result = self.is_related_to(
+                            span,
                             &source.check_type,
                             &target.check_type,
                             TypeRelatedToOpts {
-                                report_errros: false,
+                                report_errors: false,
                                 ..opts
                             }?,
                         );
                         if result.as_bool() {
                             result &= self.is_related_to(
+                                span,
                                 &source.extends_type,
                                 &target.extends_type,
                                 TypeRelatedToOpts {
-                                    report_errros: false,
+                                    report_errors: false,
                                     ..opts
                                 },
                             )?;
 
                             if result.as_bool() {
                                 result &= self.is_related_to(
+                                    span,
                                     &source.true_type,
                                     &target.true_type,
                                     TypeRelatedToOpts {
-                                        report_errros: false,
+                                        report_errors: false,
                                         ..opts
                                     },
                                 )?;
 
                                 if result.as_bool() {
                                     result &= self.is_related_to(
+                                        span,
                                         &source.false_type,
                                         &target.false_type,
                                         TypeRelatedToOpts {
-                                            report_errros: false,
+                                            report_errors: false,
                                             ..opts
                                         },
                                     )?;
@@ -172,13 +184,100 @@ impl Analyzer<'_, '_> {
     }
 
     /// Ported from `isRelatedTo` of `tsc`.
-    fn is_related_to(&mut self, source: &Type, target: &Type, opts: TypeRelatedToOpts) -> ValidationResult<TscTernary> {
-        unimplemented!()
+    #[instrument(skip(self, span, source, target, opts))]
+    fn is_related_to(
+        &mut self,
+        span: Span,
+        source: &Type,
+        target: &Type,
+        opts: TypeRelatedToOpts,
+    ) -> ValidationResult<TscTernary> {
+        let source = self.normalize(Some(span), Cow::Borrowed(source), Default::default())?;
+        let target = self.normalize(Some(span), Cow::Borrowed(target), Default::default())?;
+
+        match (&*source, &*target) {
+            (Type::TypeLit(..), Type::Lit(..) | Type::Keyword(..)) => {
+                if self.is_simple_type_related_to(source, target, opts)? {
+                    return Ok(TscTernary::True);
+                }
+
+                // TODO: reportErrorResults(originalSource, originalTarget,
+                // Ternary.False, !!(getObjectFlags(originalSource) &
+                // ObjectFlags.JsxAttributes));
+
+                return Ok(TscTernary::False);
+            }
+            _ => {}
+        }
+
+        if source.type_eq(target) {
+            return Ok(TscTernary::True);
+        }
+
+        match opts.mode {
+            RelationCheckMode::Identity => return self.is_identical_to(span, source, target),
+            _ => {}
+        }
+
+        // We fastpath comparing a type parameter to exactly its constraint, as this is
+        // _super_ common, and otherwise, for type parameters in large unions,
+        // causes us to need to compare the union to itself, as we break down
+        // the _target_ union first, _then_ get the source constraint - so for every
+        // member of the target, we attempt to find a match in the source. This avoids
+        // that in cases where the target is exactly the constraint.
+        match &*source {
+            Type::Param(TypeParam {
+                constraint: Some(constraint),
+                ..
+            }) => {
+                if constraint.type_eq(target) {
+                    return Ok(TscTernary::True);
+                }
+            }
+            _ => {}
+        }
+
+        match opts.mode {
+            RelationCheckMode::Comparable => {
+                if !target.is_never() {
+                    if self.is_simple_type_related_to(
+                        span,
+                        &target,
+                        &source,
+                        TypeRelatedToOpts {
+                            report_errors: false,
+                            ..opts
+                        },
+                    )? || self.is_simple_type_related_to(span, &source, &target, opts)?
+                    {
+                        return Ok(TscTernary::True);
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+
+    /// Ported from `isIdenticalTo` of `tsc`.
+    #[instrument(skip(self, span, source, target))]
+    fn is_identical_to(&mut self, span: Span, source: &Type, target: &Type) -> ValidationResult<TscTernary> {}
+
+    /// Ported from `isSimpleTypeRelatedTo` of `tsc`.
+    #[instrument(skip(self, span, source, target, opts))]
+    fn is_simple_type_related_to(
+        &mut self,
+        span: Span,
+        source: &Type,
+        target: &Type,
+        opts: TypeRelatedToOpts,
+    ) -> ValidationResult<bool> {
     }
 
     /// Ported from `propertiesRelatedTo` of `tsc`.
+    #[instrument(skip(self, span, source, target, exclude_properties, opts))]
     fn properties_related_to(
         &mut self,
+        span: Span,
         source: &Type,
         target: &Type,
         exclude_properties: FxHashSet<JsWord>,
