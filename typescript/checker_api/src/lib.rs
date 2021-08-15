ce27281776aa8dae0c74327016d770e4bcd089a9
강동윤 <kdy1997.dev@gmail.com>
2021-08-15 15:35:21 +0900

    auto impl

--- typescript/checker_api/src/lib.rs
+++ typescript/checker_api/src/lib.rs
@@ -1,14 +1,15 @@
-use swc_common::{sync::Lrc, SourceFile, SourceMap};
+use auto_impl::auto_impl;
+use swc_common::{errors::Handler, sync::Lrc, SourceFile, SourceMap};
 
 pub mod cache;
 
 /// For efficient cacing, this uses one [swc_common::SourceMap] per file.
-
 ///
 ///
 /// Note that [swc_common::Globals] is shared.
+#[auto_impl(Arc, Box)]
 pub trait TypeChecker {
-    fn check(&self, cm: Lrc<SourceMap>, fm: Lrc<SourceFile>) -> FileResult;
+    fn check(&self, cm: Lrc<SourceMap>, handler: Lrc<Handler>, fm: Lrc<SourceFile>) -> FileResult;
 }
 
 #[derive(Clone)]
