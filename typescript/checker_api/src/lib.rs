0061e3f8ed8340241e112c11327e8fe1f439a64b
강동윤 <kdy1997.dev@gmail.com>
2021-08-15 15:24:30 +0900

    Some work for checker_api

--- typescript/checker_api/src/lib.rs
+++ typescript/checker_api/src/lib.rs
@@ -1,7 +1,18 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use swc_common::{sync::Lrc, SourceFile, SourceMap};
+
+pub mod cache;
+
+/// For efficient cacing, this uses one [swc_common::SourceMap] per file.
+
+///
+///
+/// Note that [swc_common::Globals] is shared.
+pub trait TypeChecker {
+    fn check(&self, cm: Lrc<SourceMap>, fm: Lrc<SourceFile>) -> FileResult;
+}
+
+#[derive(Clone)]
+pub struct FileResult {
+    pub cm: Lrc<SourceMap>,
+    pub fm: Lrc<SourceFile>,
 }
