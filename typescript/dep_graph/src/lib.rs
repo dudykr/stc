dc743d653b5094b8428d04fc4719995c0e17ed40
강동윤 <kdy1997.dev@gmail.com>
2021-08-20 01:15:42 +0900

    Use chunk api

--- typescript/dep_graph/src/lib.rs
+++ typescript/dep_graph/src/lib.rs
@@ -1,7 +1,14 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use anyhow::Error;
+use stc_utils::path::intern::FileId;
+use std::sync::Arc;
+use swc_ecma_ast::Module;
+
+#[derive(Debug, Clone)]
+pub enum Chunk {
+    Cycle(Vec<Arc<Module>>),
+    Single(Arc<Module>),
+}
+
+pub trait ModuleLoader {
+    fn load(&self, path: FileId) -> Result<Chunk, Error>;
 }
