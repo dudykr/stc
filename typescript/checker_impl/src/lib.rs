9ade00f6586980603842a8c8fa57469f83ae8dfa
강동윤 <kdy1997.dev@gmail.com>
2021-08-21 00:14:37 +0900

    Checker genric

--- typescript/checker_impl/src/lib.rs
+++ typescript/checker_impl/src/lib.rs
@@ -1,14 +1,23 @@
 use async_trait::async_trait;
 use stc_ts_checker_api::{cache::Cached, dedup::Deduplicated, FileData, TypeChecker};
+use stc_ts_dep_graph::Load;
 use stc_utils::path::intern::FileId;
 use std::sync::Arc;
 
-pub type FastTypeChecker = Cached<Deduplicated<Arc<Checker>>>;
+pub type FastTypeChecker<L> = Cached<Deduplicated<Arc<Checker<L>>>>;
 
 #[derive(Debug)]
-pub struct Checker {}
+pub struct Checker<L>
+where
+    L: Load,
+{
+    loader: L,
+}
 
 #[async_trait]
-impl TypeChecker for Checker {
+impl<L> TypeChecker for Checker<L>
+where
+    L: Load,
+{
     async fn check(&self, name: &FileId, src: &str) -> FileData {}
 }
