b2782fec2c052eacf52672ce2c9f35bc70e69d67
강동윤 <kdy1997.dev@gmail.com>
2021-08-20 22:04:46 +0900

    Add `LoadedModule`

--- typescript/dep_graph/src/lib.rs
+++ typescript/dep_graph/src/lib.rs
@@ -1,12 +1,27 @@
 use anyhow::Error;
+use derivative::Derivative;
 use stc_utils::path::intern::FileId;
 use std::sync::Arc;
+use swc_common::{sync::Lrc, SourceFile, SourceMap};
 use swc_ecma_ast::Module;
 
 #[derive(Debug, Clone)]
 pub enum Chunk {
-    Cycle(Vec<Arc<Module>>),
-    Single(Arc<Module>),
+    Cycle(Vec<LoadedModule>),
+    Single(Arc<LoadedModule>),
+}
+
+/// Cheap to clone. [SourceMap] and [SourceFile] are stored inline to make cache
+/// efficient.
+#[derive(Derivative)]
+#[derivative(Debug)]
+#[derive(Clone)]
+pub struct LoadedModule {
+    #[derivative(Debug = "ignore")]
+    pub cm: Lrc<SourceMap>,
+    #[derivative(Debug = "ignore")]
+    pub fm: Lrc<SourceFile>,
+    pub module: Arc<Module>,
 }
 
 pub trait ModuleLoader {
