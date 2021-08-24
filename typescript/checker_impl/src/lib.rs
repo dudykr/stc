53b78a5ee225f984b50e4464c5f05c2547b97ee3
강동윤 <kdy1997.dev@gmail.com>
2021-08-25 00:54:29 +0900

    More work

--- typescript/checker_impl/src/lib.rs
+++ typescript/checker_impl/src/lib.rs
@@ -4,23 +4,35 @@ use async_trait::async_trait;
 use stc_ts_checker_api::{cache::Cached, dedup::Deduplicated, FileData, TypeChecker};
 use stc_ts_dep_graph::{Chunk, Load};
 use stc_ts_dts::{apply_mutations, cleanup_module_for_dts};
+use stc_ts_errors::debug::debugger::Debugger;
 use stc_ts_file_analyzer::{analyzer::Analyzer, env::Env};
 use stc_ts_storage::Single;
 use stc_ts_types::module_id;
 use stc_utils::path::intern::FileId;
+use stc_visit::VisitWith;
 use std::sync::Arc;
 use tokio::task::spawn_blocking;
 
 pub type FastTypeChecker<L> = Cached<Deduplicated<Arc<Checker<L>>>>;
 
-#[derive(Debug)]
 pub struct Checker<L>
 where
     L: Load,
 {
     env: Env,
 
+    debugger: Option<Debugger>,
+
     loader: L,
+}
+
+impl<L> Checker<L>
+where
+    L: Load,
+{
+    pub fn new(loader: L, env: Env, debugger: Option<Debugger>) -> Self {
+        Checker { env, debugger, loader }
+    }
 }
 
 #[async_trait]
@@ -46,7 +58,7 @@ where
                     let mut mutations;
                     {
                         let mut a = Analyzer::root(
-                            self.logger.clone(),
+                            slog::Logger::root(slog::Discard, slog::o!()),
                             self.env.clone(),
                             parsed.cm.clone(),
                             box &mut storage,
@@ -54,7 +66,7 @@ where
                             self.debugger.clone(),
                         );
 
-                        parsed.module.visit_with(&mut a);
+                        m.visit_with(&mut a);
                         mutations = a.mutations.unwrap();
                     }
 
