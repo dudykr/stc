92345c14fb339340ee3e8c0af543e86e69b0a929
강동윤 <kdy1997.dev@gmail.com>
2021-08-25 18:14:03 +0900

    Doc

--- typescript/file_analyzer/src/analyzer/types/index_signature.rs
+++ typescript/file_analyzer/src/analyzer/types/index_signature.rs
@@ -6,9 +6,12 @@ use std::borrow::Cow;
 use swc_common::Span;
 
 impl Analyzer<'_, '_> {
+    /// Get [IndexSignature] from `ty`, if there's one.
     pub(crate) fn get_index_signature(&mut self, span: Span, ty: &Type) -> ValidationResult<Option<IndexSignature>> {
         (|| -> ValidationResult<_> {
             let ty = self.normalize(Some(span), Cow::Borrowed(ty), Default::default())?;
+
+            // TODO: Support type literals and interfaces.
 
             match &*ty {
                 Type::ClassDef(cls) => return self.get_index_signature_from_class(span, &cls),
