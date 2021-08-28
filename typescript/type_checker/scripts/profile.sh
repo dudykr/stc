d82bc9cee357a73ba72fa8eac994752f29076049
강동윤 <kdy1997.dev@gmail.com>
2021-08-29 00:09:01 +0900

    SIGILL

--- typescript/type_checker/scripts/profile.sh
+++ typescript/type_checker/scripts/profile.sh
@@ -4,4 +4,4 @@ set -eux
 
 docker run -d -p6831:6831/udp -p6832:6832/udp -p16686:16686 jaegertracing/all-in-one:latest || true
 
-TEST='' cargo test --release --test tsc -- -Z unstable-options --report-time=colored
+cargo test --release --test tsc -- -Z unstable-options --report-time=colored
