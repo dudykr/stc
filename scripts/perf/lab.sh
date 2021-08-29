7ba7b50ff064c045f58f04abc86bdd8e302ccf20
강동윤 <kdy1997.dev@gmail.com>
2021-08-29 16:34:56 +0900

    circular import has a bug

--- scripts/perf/lab.sh
+++ scripts/perf/lab.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 set -eu
 
-cargo install --path .
+cargo install --offline --path .
 
 export LD_LIBRARY_PATH="$(rustc --print sysroot)/lib"
 
-(cd stc-lab/rxjs && stc check src/index.ts)
+# The type checker is buggy.
+#
+# (cd stc-lab/rxjs && stc check src/index.ts)
+
+(cd stc-lab/ant-design-mobile && stc check src/index.ts)
+
