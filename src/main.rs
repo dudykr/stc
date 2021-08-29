ede7c01edda90d11f2d7c73b99164071efa40d31
강동윤 <kdy1997.dev@gmail.com>
2021-08-29 15:34:03 +0900

    WIP: Real world benchmark

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
+use std::{env, sync::Arc};
+
 use crate::{lsp::LspCommand, tsc::TscCommand};
 use anyhow::Error;
+use stc_ts_builtin_types::Lib;
+use stc_ts_file_analyzer::{
+    env::{Env, ModuleConfig},
+    Rule,
+};
+use stc_ts_module_loader::resolver::node::NodeResolver;
+use stc_ts_type_checker::Checker;
 use structopt::StructOpt;
+use swc_common::{
+    errors::{ColorConfig, Handler},
+    SourceMap,
+};
+use swc_ecma_ast::EsVersion;
+use swc_ecma_parser::TsConfig;
 
 mod lsp;
 mod tsc;
@@ -30,8 +45,30 @@ async fn main() -> Result<(), Error> {
 
     let command = Command::from_args();
 
+    let cm = Arc::new(SourceMap::default());
+    let handler = Arc::new(Handler::with_tty_emitter(
+        ColorConfig::Always,
+        true,
+        false,
+        Some(cm.clone()),
+    ));
+
     match command {
-        Command::Tsc(_) => todo!("tsc mode"),
+        Command::Tsc(c) => {
+            let mut checker = Checker::new(
+                cm.clone(),
+                handler.clone(),
+                Env::simple(
+                    Rule { ..Default::default() },
+                    EsVersion::latest(),
+                    ModuleConfig::None,
+                    &Lib::load(&env::var("STC_LIBS").unwrap_or("es5".into())),
+                ),
+                TsConfig { ..Default::default() },
+                None,
+                Arc::new(NodeResolver),
+            );
+        }
         Command::Lsp(c) => c.run().await,
     }
 
