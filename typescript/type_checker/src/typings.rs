628662b81f8f1395efa09dc805172d84bb2dd9e8
강동윤 <kdy1997.dev@gmail.com>
2021-08-29 20:41:55 +0900

    More work for typing packages

--- typescript/type_checker/src/typings.rs
+++ typescript/type_checker/src/typings.rs
@@ -1,12 +1,49 @@
 use crate::Checker;
-use std::{path::PathBuf, sync::Arc};
+use rayon::prelude::*;
+use std::{
+    env,
+    fs::read_dir,
+    path::{Path, PathBuf},
+};
 
 impl Checker {
-    fn load_typing_dir(&self, dir: &Arc<PathBuf>) {}
+    fn try_loading_typing_of_one_package(&self, dir: &Path) {}
+
+    fn load_typings_from_dir(&self, dir: &Path, types: Option<&[String]>) {
+        let types_dir = dir.join("node_modules").join("@types");
+
+        if !types_dir.is_dir() {
+            return Default::default();
+        }
+
+        let dirs = types
+            .map(|s| s.iter().map(|s| PathBuf::from(s.clone())).collect())
+            .or_else(|| {
+                let pkgs = read_dir(&types_dir).ok()?;
+
+                let f = pkgs
+                    .into_iter()
+                    .filter_map(Result::ok)
+                    .map(|e| e.path())
+                    .collect::<Vec<_>>();
+
+                Some(f)
+            });
+
+        if let Some(dirs) = dirs {
+            dirs.into_par_iter().for_each(|dir| {
+                self.try_loading_typing_of_one_package(&types_dir.join(dir));
+            });
+        }
+    }
 
     /// Load typings from node_modules.
     ///
     /// - https://www.typescriptlang.org/tsconfig#typeRoots
     /// - https://www.typescriptlang.org/tsconfig#types
-    pub fn load_typings(&self, type_roots: &[PathBuf], types: &[String]) {}
+    pub fn load_typings(&self, type_roots: Option<&[PathBuf]>, types: Option<&[String]>) {
+        let dir =
+            env::current_dir().expect("failed to get current directory which is required to load typing packages");
+        self.load_typings_from_dir(&dir, types)
+    }
 }
