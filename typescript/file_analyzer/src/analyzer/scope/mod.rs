b2e5c79bd1d703f268ba9181efea25083a699df0
강동윤 <kdy1997.dev@gmail.com>
2021-09-02 10:28:59 +0900

    Instrument

--- typescript/file_analyzer/src/analyzer/scope/mod.rs
+++ typescript/file_analyzer/src/analyzer/scope/mod.rs
@@ -1575,7 +1575,7 @@ impl Analyzer<'_, '_> {
     /// Check if `ty` stores infer type in it. **Note**: This mehods only checks
     /// for [Mark], and this method should be used with
     /// [crate::util::contains_infer_type] in most case.
-    #[instrument(skip(self, ty))]
+    #[instrument(name = "Analyzer::contains_infer_type", skip(self, ty))]
     pub(crate) fn contains_infer_type<T>(&self, ty: &T) -> bool
     where
         T: VisitWith<MarkFinder>,
