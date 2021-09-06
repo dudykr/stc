2dc9c763d99a1e77874e313d9299c27cd026a0f9
강동윤 <kdy1997.dev@gmail.com>
2021-09-06 23:31:00 +0900

    Remove some assertions

--- typescript/file_analyzer/src/analyzer/expr/bin.rs
+++ typescript/file_analyzer/src/analyzer/expr/bin.rs
@@ -246,10 +246,8 @@ impl Analyzer<'_, '_> {
 
         if !self.is_builtin {
             debug_assert!(!lt.span().is_dummy());
-            debug_assert!(!lt.normalize().span().is_dummy());
 
             debug_assert!(!rt.span().is_dummy());
-            debug_assert!(!rt.normalize().span().is_dummy());
         }
 
         // Handle control-flow based typing
