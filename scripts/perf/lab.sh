8720e7082bdcb482604f41ee32cdb793f296f5a6
강동윤 <kdy1997.dev@gmail.com>
2021-09-11 20:16:57 +0900

    lab.sh: Instruments

--- scripts/perf/lab.sh
+++ scripts/perf/lab.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 
-cargo install --offline --path .
+# cargo install --offline --path .
 
 export RUST_LOG=debug,swc_common=off
 export LD_LIBRARY_PATH="$(rustc --print sysroot)/lib"
@@ -19,4 +19,4 @@ export LD_LIBRARY_PATH="$(rustc --print sysroot)/lib"
 # (cd stc-lab/jest/packages/jest-cli && stc check src/index.ts)
 
 
-cargo instruments -t time --open check ../stc-lab/jest/packages/jest-cli/src/index.ts
+cargo instruments --release -t time --open check ../stc-lab/jest/packages/jest-cli/src/index.ts
