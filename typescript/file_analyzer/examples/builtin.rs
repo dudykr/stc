d0d849bcb2b3202ad42e3c35a58278ff2f7dea91
강동윤 <kdy1997.dev@gmail.com>
2021-09-14 15:04:59 +0900

    Let's profile builtins

---
+++ typescript/file_analyzer/examples/builtin.rs
@@ -0,0 +1,31 @@
+#![feature(box_syntax)]
+#![feature(test)]
+
+use stc_ts_builtin_types::Lib;
+use stc_ts_file_analyzer::env::{Env, ModuleConfig};
+use std::hint::black_box;
+use swc_ecma_ast::EsVersion;
+
+fn main() {
+    testing::run_test2(false, |_cm, _handler| {
+        let mut libs = vec![];
+        for s in &[
+            "es2020.full",
+            "es2019.full",
+            "es2018.full",
+            "es2017.full",
+            "es2016.full",
+            "es2015.full",
+            "es5.full",
+        ] {
+            libs.extend(Lib::load(&s));
+        }
+        libs.sort();
+        libs.dedup();
+        let env = Env::simple(Default::default(), EsVersion::latest(), ModuleConfig::None, &libs);
+
+        black_box(env);
+        Ok(())
+    })
+    .unwrap()
+}
