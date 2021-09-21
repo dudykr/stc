989b090dc07d8059f46d282eecd27f40e74fffc4
Donny <kdy1997.dev@gmail.com>
2021-09-21 21:05:16 +0900

    Add a field

--- typescript/type_cache/src/lib.rs
+++ typescript/type_cache/src/lib.rs
@@ -1,8 +1,12 @@
-use stc_ts_types::{Id, Type};
+use stc_ts_ast_rnode::RTsEntityName;
+use stc_ts_types::{Id, Type, TypeParamInstantiation};
 use stc_utils::cache::CacheMap;
 
 #[derive(Debug, Default)]
 pub struct TypeCache {
+    pub expand_cache: CacheMap<(RTsEntityName, Option<TypeParamInstantiation>), Type>,
+
+    /// Key should be [Type::Arc] of [Type::TypeLit].
     pub keyof_type_lit: CacheMap<Type, Type>,
 }
 
