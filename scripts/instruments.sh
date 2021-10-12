9fefd136c270a1eba5edf77566542c0859216ca1
Donny <kdy1997.dev@gmail.com>
2021-10-12 20:42:10 +0900

    instrument

--- scripts/instruments.sh
+++ scripts/instruments.sh
@@ -3,7 +3,7 @@ set -eu
 
 # cargo install --offline --path .
 
-export RUST_LOG=debug,swc_common=off
+# export RUST_LOG=debug,swc_common=off
 # export RUST_BACKTRACE=1
 
 # The type checker is buggy.
@@ -15,4 +15,6 @@ export RUST_LOG=debug,swc_common=off
 # (cd stc-lab/ant-design-mobile && stc check src/index.ts)
 
 
-(cd stc-lab/jest/packages/jest-cli && stc check src/index.ts)
+cargo instruments --release -t time --features tracing/release_max_level_off -- check ~/projects/stc-lab/jest/packages/jest-cli/src/index.ts
+cargo instruments --release -t time --features tracing/release_max_level_off -- check $@
+# 
