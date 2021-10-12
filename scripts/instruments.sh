d334ed92163bff5fa2497e17d8501a4ca7093eb1
Donny <kdy1997.dev@gmail.com>
2021-10-12 20:52:15 +0900

    Comment out

--- scripts/instruments.sh
+++ scripts/instruments.sh
@@ -16,5 +16,5 @@ set -eu
 
 
 cargo instruments --release -t time --features tracing/release_max_level_off -- check ~/projects/stc-lab/jest/packages/jest-cli/src/index.ts
-cargo instruments --release -t time --features tracing/release_max_level_off -- check $@
+# cargo instruments --release -t time --features tracing/release_max_level_off -- check $@
 # 
