37c5c8656a94a7181cc627dd85acaccf1537beaa
Donny <kdy1997.dev@gmail.com>
2021-11-15 20:00:17 +0900

    Remove Freeze::freezed

--- utils/src/cache/mod.rs
+++ utils/src/cache/mod.rs
@@ -7,12 +7,6 @@ pub trait Freeze: Sized + Clone {
     fn is_clone_cheap(&self) -> bool;
 
     fn make_clone_cheap(&mut self);
-
-    #[inline]
-    fn freezed(mut self) -> Self {
-        self.make_clone_cheap();
-        self
-    }
 }
 
 impl<T> Freeze for Option<T>
