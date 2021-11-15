ba4a5a1ab50f0bdf841e9ba25cad7291c73fd162
Donny <kdy1997.dev@gmail.com>
2021-11-15 23:14:46 +0900

    More fixes

--- typescript/analyzer/type_ops/src/tuple_to_array/mod.rs
+++ typescript/analyzer/type_ops/src/tuple_to_array/mod.rs
@@ -1,5 +1,6 @@
 pub use self::metadata::prevent_tuple_to_array;
 use rnode::{Fold, FoldWith};
+use stc_arc_cow::BoxedArcCow;
 use stc_ts_types::{Array, ArrayMetadata, Type};
 use swc_common::TypeEq;
 
@@ -10,24 +11,23 @@ pub struct TupleToArray;
 impl Fold<Type> for TupleToArray {
     fn fold(&mut self, mut ty: Type) -> Type {
         // TODO(kdy1): PERF
-        ty.normalize_mut();
         let ty = ty.fold_children_with(self);
 
         match ty {
             Type::Tuple(tuple) => {
                 let span = tuple.span;
 
-                let mut types: Vec<Type> = vec![];
+                let mut types: Vec<BoxedArcCow<Type>> = vec![];
 
                 for element in tuple.elems {
                     if types.iter().any(|item| item.type_eq(&element.ty)) {
                         continue;
                     }
 
-                    types.push(*element.ty);
+                    types.push(element.ty);
                 }
 
-                let elem_type = box Type::union(types);
+                let elem_type = Type::union(types);
                 return Type::Array(Array {
                     span,
                     elem_type,
