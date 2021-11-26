e279ed858ea385695dd49bd7d5b902e2bbcac9fc
Donny <kdy1997.dev@gmail.com>
2021-11-26 20:10:16 +0900

    Reduce clone

--- typescript/analyzer/file_analyzer/src/env.rs
+++ typescript/analyzer/file_analyzer/src/env.rs
@@ -188,7 +188,9 @@ pub trait BuiltInGen: Sized {
                                 let body = i
                                     .clone()
                                     .validate_with(&mut analyzer)
-                                    .expect("builtin: failed to parse interface body");
+                                    .expect("builtin: failed to parse interface body")
+                                    .foldable()
+                                    .expect_interface();
 
                                 match types.entry(i.id.sym.clone()) {
                                     Entry::Occupied(mut e) => match &mut *e.get_mut() {
