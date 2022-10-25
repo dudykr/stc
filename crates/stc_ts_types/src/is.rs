9ada858a43f6c69530776d50a60c33bf3b527e13
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-25 21:27:59 +0900

    WIP

--- crates/stc_ts_types/src/is.rs
+++ crates/stc_ts_types/src/is.rs
@@ -1,1 +1,30 @@
 //! Smarter version of `derive(Is)`
+
+use crate::*;
+
+macro_rules! impl_is {
+    ($variant:ident,$type_name:ident, $is_name:ident,$as_name:ident,$as_mut_name:ident,$opt_name:ident,$expect_name:ident) => {
+        impl Type {
+            pub fn $is_name(&self) -> bool {
+                matches!(self.normalize(), Type::$variant(_))
+            }
+
+            pub fn $as_name(&self) -> Option<&$type_name> {
+                match self.normalize() {
+                    Type::$variant(ty) => Some(ty),
+                    _ => None,
+                }
+            }
+        }
+    };
+}
+
+impl_is!(
+    Array,
+    Array,
+    is_array,
+    as_array,
+    as_array_mut,
+    array,
+    expect_array
+);
