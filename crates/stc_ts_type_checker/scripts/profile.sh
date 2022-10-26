28d9e156c73a4cc2a9333c68300fffda89e5e475
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-26 22:40:29 +0900

    fixup

--- crates/stc_ts_type_checker/scripts/profile.sh
+++ crates/stc_ts_type_checker/scripts/profile.sh
@@ -4,4 +4,4 @@ set -eux
 
 export RUST_LOG=off
 
-TEST='' cargo profile instruments -t time test --test tsc --release --features tracing/release_max_level_off > /dev/null
+TEST='' cargo profile instruments -t time --test tsc --release --features tracing/release_max_level_off > /dev/null
