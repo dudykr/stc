ec42c1e2941f1bb0027d11cf52d2adc867b0d1e6
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-26 21:10:56 +0900

    45

--- crates/stc_ts_types/src/id.rs
+++ crates/stc_ts_types/src/id.rs
@@ -6,6 +6,7 @@ use std::{
 use rnode::NodeId;
 use serde::{Deserialize, Serialize};
 use stc_ts_ast_rnode::{RIdent, RTsEntityName};
+use stc_ts_ast_rnode::{RIdent, RModuleExportName, RTsEntityName};
 use stc_visit::Visit;
 use swc_atoms::JsWord;
 use swc_common::{EqIgnoreSpan, SyntaxContext, TypeEq, DUMMY_SP};
@@ -137,5 +138,14 @@ impl Debug for Id {
 impl PartialEq<str> for Id {
     fn eq(&self, other: &str) -> bool {
         self.sym == *other
+    }
+}
+
+impl From<RModuleExportName> for Id {
+    fn from(v: RModuleExportName) -> Self {
+        match v {
+            RModuleExportName::Ident(i) => i.into(),
+            RModuleExportName::Str(s) => Id::word(s.value),
+        }
     }
 }
