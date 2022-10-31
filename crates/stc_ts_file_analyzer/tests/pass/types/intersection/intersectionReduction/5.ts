cf93a7bc481e0f5496a9b427453e7a9af17523bd
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-10-31 22:45:24 +0900

    Add tests

---
+++ crates/stc_ts_file_analyzer/tests/pass/types/intersection/intersectionReduction/5.ts
@@ -0,0 +1,12 @@
+//@strict: false
+
+const enum Tag1 { }
+const enum Tag2 { }
+
+declare let t1: string & Tag1 | undefined;
+declare let t2: string & Tag2 | undefined;
+
+t1 = t2;
+t2 = t1;
+
+export { }
