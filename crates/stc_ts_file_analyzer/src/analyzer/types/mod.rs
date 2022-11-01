e9f0a753ffbad36efd89a0ffd374c79281f2fd26
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-01 16:36:56 +0900

    option

--- crates/stc_ts_file_analyzer/src/analyzer/types/mod.rs
+++ crates/stc_ts_file_analyzer/src/analyzer/types/mod.rs
@@ -682,7 +682,7 @@ impl Analyzer<'_, '_> {
             return never!();
         }
 
-        if types.len() == 2 {
+        if !self.rule().always_strict && types.len() == 2 {
             let (a, b) = (&types[0], &types[1]);
 
             if (a.is_str_lit() && b.is_str_lit() || (a.is_num_lit() && b.is_num_lit()) || (a.is_bool_lit() && b.is_bool_lit()))
