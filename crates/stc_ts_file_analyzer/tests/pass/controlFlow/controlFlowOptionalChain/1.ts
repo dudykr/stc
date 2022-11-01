ffc35741fd0bb75ac17a7de0256894c8f9884508
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-01 21:26:45 +0900

    Add a test

---
+++ crates/stc_ts_file_analyzer/tests/pass/controlFlow/controlFlowOptionalChain/1.ts
@@ -0,0 +1,15 @@
+// @strict: true
+// @allowUnreachableCode: false
+
+// assignments in shortcutting chain
+declare const o: undefined | {
+    [key: string]: any;
+    [key: number]: any;
+    (...args: any[]): any;
+};
+
+let d: number;
+o?.x(d = 1);
+d.toString();
+
+export { }
