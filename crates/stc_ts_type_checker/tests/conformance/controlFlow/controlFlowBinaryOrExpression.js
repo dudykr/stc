3b3cb3d98c8f8f4aa1b6cb936c489b70449754c0
Yongwook Choi <flow@hrmm.xyz>
2022-11-06 17:01:28 +0900

    Include .d.ts, .js extension for TypeScript tests

---
+++ crates/stc_ts_type_checker/tests/conformance/controlFlow/controlFlowBinaryOrExpression.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+var x;
+var cond;
+(x = "") || (x = 0);
+x; // string | number
+x = "";
+cond || (x = 0);
+x; // string | number
+var sourceObj = undefined;
+if (isNodeList(sourceObj)) {
+    sourceObj.length;
+}
+if (isHTMLCollection(sourceObj)) {
+    sourceObj.length;
+}
