b3d32cf8d2c34b2c873369aa48a9091186b28adb
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-09 20:58:52 +0900

    Remove wrong check

--- crates/stc_ts_file_analyzer/src/analyzer/assign/type_el.rs
+++ crates/stc_ts_file_analyzer/src/analyzer/assign/type_el.rs
@@ -136,7 +136,7 @@ impl Analyzer<'_, '_> {
                                     used_keys.push(p.key.clone());
                                 }
                                 TypeElement::Property(PropertySignature { optional: true, .. })
-                                | TypeElement::Method(MethodSignature { optional: true, .. }) => continue,
+                                | TypeElement::Method(MethodSignature { optional: true, .. }) => {}
                                 _ => {}
                             }
 
