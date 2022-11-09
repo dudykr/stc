7ad9570cb7463cb07f1c513b4f20d779c5df244b
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-09 14:22:19 +0900

    super

--- crates/stc_ts_file_analyzer/src/analyzer/expr/mod.rs
+++ crates/stc_ts_file_analyzer/src/analyzer/expr/mod.rs
@@ -301,6 +301,10 @@ impl Analyzer<'_, '_> {
                     return self.type_of_member_expr(expr, mode);
                 }
 
+                RExpr::SuperProp(ref expr) => {
+                    return self.type_of_super_prop_expr(expr, mode);
+                }
+
                 RExpr::MetaProp(e) => return e.validate_with(self),
 
                 RExpr::Invalid(ref i) => return Ok(Type::any(i.span(), Default::default())),
