6a76e858863ba005a2dff6b71f6aeb4a1ba1da7c
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-10 13:16:11 +0900

    Option

--- crates/stc_ts_file_analyzer/src/analyzer/assign/type_el.rs
+++ crates/stc_ts_file_analyzer/src/analyzer/assign/type_el.rs
@@ -226,7 +226,17 @@ impl Analyzer<'_, '_> {
                         .map(Type::TypeLit)
                     {
                         return self
-                            .assign_to_type_elements(data, lhs_span, lhs, &rty, lhs_metadata, opts)
+                            .assign_to_type_elements(
+                                data,
+                                lhs_span,
+                                lhs,
+                                &rty,
+                                lhs_metadata,
+                                AssignOpts {
+                                    report_object_instead_of_fields: true,
+                                    ..opts
+                                },
+                            )
                             .context("tried to assign to type elements by converting rhs to a type literal");
                     }
 
