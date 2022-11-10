8cdb693cb9d34ce1b7703305e05d3f50801dc19e
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-10 18:26:25 +0900

    base.sh

---
+++ crates/stc_ts_type_checker/scripts/base.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# Usage: `./scripts/base.sh test_name_to_run``
+#
+set -eu
+set -o pipefail
+
+export CARGO_TERM_COLOR=always
+export RUST_BACKTRACE=1
+export RUST_MIN_STACK=$((16 * 1024 * 1024))
+
+# We prevent regression using faster checks
+touch ../stc_ts_file_analyzer/tests/base.rs
+UPDATE=1 cargo test -p stc_ts_file_analyzer --color always --lib --test base -- -Zunstable-options --report-time $@
