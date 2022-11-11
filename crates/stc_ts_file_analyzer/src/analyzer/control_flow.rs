263853b9dc1064dfda8cc2acc95b2e2e4b40762f
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-11 22:17:24 +0900

    Trivial refactor

--- crates/stc_ts_file_analyzer/src/analyzer/control_flow.rs
+++ crates/stc_ts_file_analyzer/src/analyzer/control_flow.rs
@@ -28,7 +28,7 @@ use tracing::info;
 use crate::{
     analyzer::{
         assign::AssignOpts,
-        expr::{AccessPropertyOpts, IdCtx, TypeOfMode},
+        expr::{optional_chaining::is_obj_opt_chaining, AccessPropertyOpts, IdCtx, TypeOfMode},
         scope::{ScopeKind, VarInfo},
         util::ResultExt,
         Analyzer, Ctx,
@@ -1028,7 +1028,6 @@ impl Analyzer<'_, '_> {
 
                                 RPat::Expr(expr) => {
                                     // { ...obj?.a["b"] }
-                                    use crate::analyzer::expr::optional_chaining::is_obj_opt_chaining;
                                     if is_obj_opt_chaining(&expr) {
                                         return Err(Error::InvalidRestPatternInOptionalChain { span: r.span });
                                     }
