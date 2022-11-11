108e6b732946e50fceee02b9af985ffe72eb4880
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-11 20:15:57 +0900

    Reduce

--- crates/stc_ts_file_analyzer/tests/visualize/types/unknown/unknownType1/1.ts
+++ crates/stc_ts_file_analyzer/tests/visualize/types/unknown/unknownType1/1.ts
@@ -1,12 +1,7 @@
 export function f10(x: unknown) {
-    x == 5;
     x !== 10;
     x >= 0;  // Error
     x.foo;  // Error
     x[10];  // Error
     x();  // Error
-    x + 1;  // Error
-    x * 2;  // Error
-    -x;  // Error
-    +x;  // Error
 }
