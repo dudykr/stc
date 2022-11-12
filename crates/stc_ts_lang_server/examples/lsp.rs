1b4a548ad564808f6ed3a4dcec1c495ed1315a75
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-12 12:34:18 +0900

    lsp

--- crates/stc_ts_lang_server/examples/lsp.rs
+++ crates/stc_ts_lang_server/examples/lsp.rs
@@ -1,3 +1,6 @@
+//! A command used for testing. This starts a language server using same code as
+//! the release build.
+
 use clap::Parser;
 use stc_ts_lang_server::LspCommand;
 
@@ -7,6 +10,9 @@ pub struct App {
     pub cmd: LspCommand,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let app: App = Parser::parse();
+
+    app.cmd.run().await.unwrap();
 }
