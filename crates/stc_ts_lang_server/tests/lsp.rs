4fa413d5924ea9035ca028c83c2a71ec608fba28
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-12 12:39:18 +0900

    test build

--- crates/stc_ts_lang_server/tests/lsp.rs
+++ crates/stc_ts_lang_server/tests/lsp.rs
@@ -1,1 +1,29 @@
+use std::{
+    path::PathBuf,
+    process::{Command, Stdio},
+};
 
+use once_cell::sync::Lazy;
+
+/// Builds the example lsp command, and returns to the path to it.
+fn exec_path() -> PathBuf {
+    static BIN_PATH: Lazy<PathBuf> = Lazy::new(|| {
+        let mut c = Command::new("cargo");
+        c.arg("build").arg("--example").arg("lsp");
+        c.stderr(Stdio::inherit());
+        let output = c.output().expect("cargo build --example lsp failed");
+
+        assert!(output.status.success());
+
+        let output = String::from_utf8(output.stdout).expect("cargo build --example lsp produced invalid utf8");
+
+        output.into()
+    });
+
+    BIN_PATH.to_path_buf()
+}
+
+#[test]
+fn test_build() {
+    assert!(exec_path().exists());
+}
