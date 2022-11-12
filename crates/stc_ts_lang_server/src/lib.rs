0ec21805a3d7be4ff01764dc93a476646fff15e6
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-12 12:32:57 +0900

    run()

--- crates/stc_ts_lang_server/src/lib.rs
+++ crates/stc_ts_lang_server/src/lib.rs
@@ -1,17 +1,33 @@
+use anyhow::Result;
 use clap::Args;
-use lspower::{async_trait, jsonrpc, lsp};
+use tower_lsp::{async_trait, jsonrpc, lsp_types::*, Client, LanguageServer, LspService, Server};
 
 #[derive(Debug, Args)]
 pub struct LspCommand {}
 
-pub struct StcLangServer {}
+impl LspCommand {
+    pub async fn run(self) -> Result<()> {
+        let stdin = tokio::io::stdin();
+        let stdout = tokio::io::stdout();
+
+        let (service, socket) = LspService::new(|client| StcLangServer { client });
+        Server::new(stdin, stdout, socket).serve(service).await;
+
+        Ok(())
+    }
+}
+
+pub struct StcLangServer {
+    #[allow(unused)]
+    client: Client,
+}
 
 #[async_trait]
-impl lspower::LanguageServer for StcLangServer {
-    async fn initialize(&self, params: lsp::InitializeParams) -> jsonrpc::Result<lsp::InitializeResult> {
-        Ok(lsp::InitializeResult {
-            capabilities: lsp::ServerCapabilities { ..Default::default() },
-            server_info: Some(lsp::ServerInfo {
+impl LanguageServer for StcLangServer {
+    async fn initialize(&self, _params: InitializeParams) -> jsonrpc::Result<InitializeResult> {
+        Ok(InitializeResult {
+            capabilities: ServerCapabilities { ..Default::default() },
+            server_info: Some(ServerInfo {
                 name: "stc-ts-lsp".to_string(),
                 version: Some(env!("CARGO_PKG_VERSION").to_string()),
             }),
