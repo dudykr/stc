0037aa3cc5f567632224e0c00fea2385eb86ef35
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-13 16:52:03 +0900

    Reduce

--- crates/stc_ts_file_analyzer/tests/pass/types/tpl/stringMappingOverPatternLiterals/1.ts
+++ crates/stc_ts_file_analyzer/tests/pass/types/tpl/stringMappingOverPatternLiterals/1.ts
@@ -1,19 +1,11 @@
 // non-template
-type A = "aA";
-type B = Uppercase<A>;
-type C = Lowercase<A>;
 
 // templated
 type ATemplate = `aA${string}`;
 type BTemplate = Uppercase<ATemplate>;
-type CTemplate = Lowercase<ATemplate>;
 
-var a: ATemplate
 var b: BTemplate
-var c: CTemplate
 
-a = 'aAFoo'
 b = 'AAFOO'
-c = 'aafoo'
 
 export { }
