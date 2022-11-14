6bb8b226a66a46c7152c4f517a38e490d1b26226
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-14 13:13:09 +0900

    chore(ci): Disable logging to make CI faster (#291)

--- crates/stc_ts_type_checker/scripts/base.sh
+++ crates/stc_ts_type_checker/scripts/base.sh
@@ -11,4 +11,4 @@ export RUST_MIN_STACK=$((16 * 1024 * 1024))
 
 # We prevent regression using faster checks
 touch ../stc_ts_file_analyzer/tests/base.rs
-UPDATE=1 cargo test -p stc_ts_file_analyzer --color always --lib --test base -- -Zunstable-options --report-time $@
+UPDATE=1 cargo test -p stc_ts_file_analyzer $@ --color always --lib --test base -- -Zunstable-options --report-time
