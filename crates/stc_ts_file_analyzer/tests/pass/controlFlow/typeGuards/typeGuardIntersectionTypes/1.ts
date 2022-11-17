a58fdd98746fe4b3266cd5623cd96de1d8ae8aed
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-17 20:07:14 +0900

    Reduce

--- crates/stc_ts_file_analyzer/tests/pass/controlFlow/typeGuards/typeGuardIntersectionTypes/1.ts
+++ crates/stc_ts_file_analyzer/tests/pass/controlFlow/typeGuards/typeGuardIntersectionTypes/1.ts
@@ -1,63 +1,13 @@
 // @strictNullChecks: true
 
-interface X {
-    x: string;
-}
-
-interface Y {
-    y: string;
-}
-
-interface Z {
-    z: string;
-}
-
-declare function isX(obj: any): obj is X;
-declare function isY(obj: any): obj is Y;
-declare function isZ(obj: any): obj is Z;
-
-function f1(obj: Object) {
-    if (isX(obj) || isY(obj) || isZ(obj)) {
-        obj;
-    }
-    if (isX(obj) && isY(obj) && isZ(obj)) {
-        obj;
-    }
-}
-
-// Repro from #8911
-
-// two interfaces
-interface A {
-  a: string;
-}
-
-interface B {
-  b: string;
-}
-
-// a type guard for B
-function isB(toTest: any): toTest is B {
-  return toTest && toTest.b;
-}
-
-// a function that turns an A into an A & B
-function union(a: A): A & B | null {
-  if (isB(a)) {
-    return a;
-  } else {
-    return null;
-  }
-}
-
 // Repro from #9016
 
 declare function log(s: string): void;
 
 // Supported beast features
-interface Beast     { wings?: boolean; legs?: number }
-interface Legged    { legs: number; }
-interface Winged    { wings: boolean; }
+interface Beast { wings?: boolean; legs?: number }
+interface Legged { legs: number; }
+interface Winged { wings: boolean; }
 
 // Beast feature detection via user-defined type guards
 function hasLegs(x: Beast): x is Legged { return x && typeof x.legs === 'number'; }
@@ -96,18 +46,5 @@ function identifyBeast(beast: Beast) {
         else {
             log(`snake - no legs, no wings`)
         }
-    }
-}
-
-function beastFoo(beast: Object) {
-    if (hasWings(beast) && hasLegs(beast)) {
-        beast;  // Winged & Legged
-    }
-    else {
-        beast;
-    }
-
-    if (hasLegs(beast) && hasWings(beast)) {
-        beast;  // Legged & Winged
     }
 }
