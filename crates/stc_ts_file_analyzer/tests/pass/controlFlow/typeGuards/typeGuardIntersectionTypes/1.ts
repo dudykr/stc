42823622100e595ef57b26faf0df1e78923fc6ad
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-17 20:11:45 +0900

    Reduce

--- crates/stc_ts_file_analyzer/tests/pass/controlFlow/typeGuards/typeGuardIntersectionTypes/1.ts
+++ crates/stc_ts_file_analyzer/tests/pass/controlFlow/typeGuards/typeGuardIntersectionTypes/1.ts
@@ -10,8 +10,8 @@ interface Legged { legs: number; }
 interface Winged { wings: boolean; }
 
 // Beast feature detection via user-defined type guards
-function hasLegs(x: Beast): x is Legged { return x && typeof x.legs === 'number'; }
-function hasWings(x: Beast): x is Winged { return x && !!x.wings; }
+declare function hasLegs(x: Beast): x is Legged;
+declare function hasWings(x: Beast): x is Winged;
 
 // Function to identify a given beast by detecting its features
 function identifyBeast(beast: Beast) {
