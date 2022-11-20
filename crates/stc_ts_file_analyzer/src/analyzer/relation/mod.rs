fad2d2cc75ceaf894335f0a0567e8ac3726a2122
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-20 23:34:52 +0900

    Not sure

--- crates/stc_ts_file_analyzer/src/analyzer/relation/mod.rs
+++ crates/stc_ts_file_analyzer/src/analyzer/relation/mod.rs
@@ -1,3 +1,18 @@
+use stc_ts_types::Type;
+use stc_ts_utils::Ternary;
+use swc_common::Span;
+
 use crate::analyzer::Analyzer;
 
-impl Analyzer<'_, '_> {}
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct IsRelatedOpts {
+    /// If this is [Some], errors are reported with this span. Otherwise, errors
+    /// are ignored.
+    pub span: Option<Span>,
+}
+
+impl Analyzer<'_, '_> {
+    pub fn check_type_related_to(&mut self, src: &Type, target: &Type) -> bool {}
+
+    fn is_related(&mut self, src: &Type, target: &Type) -> Ternary {}
+}
