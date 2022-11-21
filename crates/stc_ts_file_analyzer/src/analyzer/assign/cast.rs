13184c7fd26cf062831d390e73855b64d3df2d99
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-21 22:36:24 +0900

    `can_be_casted_to_number_in_rhs`

--- crates/stc_ts_file_analyzer/src/analyzer/assign/cast.rs
+++ crates/stc_ts_file_analyzer/src/analyzer/assign/cast.rs
@@ -4,13 +4,24 @@ use stc_ts_types::Type;
 use swc_common::Span;
 use swc_ecma_ast::TsKeywordTypeKind;
 
-use crate::analyzer::Analyzer;
+use crate::analyzer::{types::NormalizeTypeOpts, Analyzer};
 
 impl Analyzer<'_, '_> {
     /// Returns true if the type can be casted to number if it's in the rvalue
     /// position.
     pub(crate) fn can_be_casted_to_number_in_rhs(&mut self, span: Span, ty: &Type) -> bool {
-        let ty = ty.normalize();
+        let ty = match self.normalize(
+            Some(span),
+            Cow::Borrowed(ty),
+            NormalizeTypeOpts {
+                preserve_global_this: true,
+                preserve_union: true,
+                ..Default::default()
+            },
+        ) {
+            Ok(v) => v.into_owned(),
+            _ => return false,
+        };
 
         if ty.is_num() {
             return true;
@@ -22,14 +33,7 @@ impl Analyzer<'_, '_> {
             return true;
         }
 
-        match ty {
-            Type::Ref(..) => {
-                if let Ok(expanded) = self.expand_top_ref(span, Cow::Borrowed(ty), Default::default()) {
-                    return self.can_be_casted_to_number_in_rhs(span, &expanded);
-                }
-
-                false
-            }
+        match ty.normalize() {
             Type::EnumVariant(e) => {
                 // TODO(kdy1): Check if value is string
                 true
