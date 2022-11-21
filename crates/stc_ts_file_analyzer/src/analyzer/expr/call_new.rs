473cb13cfc022c4f6781a4a28439950eca9899f2
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-21 22:42:27 +0900

    `call_property`

--- crates/stc_ts_file_analyzer/src/analyzer/expr/call_new.rs
+++ crates/stc_ts_file_analyzer/src/analyzer/expr/call_new.rs
@@ -826,7 +826,9 @@ impl Analyzer<'_, '_> {
                 .context("tried to access property to call it")?;
 
             let callee_before_expanding = dump_type_as_string(&callee);
-            let callee = self.expand_top_ref(span, Cow::Owned(callee), Default::default())?.into_owned();
+            let callee = self
+                .normalize(Some(span), Cow::Owned(callee), NormalizeTypeOpts { ..Default::default() })?
+                .into_owned();
 
             if let Type::ClassDef(cls) = callee.normalize() {
                 if cls.is_abstract {
