e66e4ff35b759504a33ee234c943594c1c719878
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-25 22:46:03 +0900

    WIP

--- crates/stc_ts_lang_server/src/lib.rs
+++ crates/stc_ts_lang_server/src/lib.rs
@@ -1,8 +1,16 @@
 use std::{path::PathBuf, sync::Arc};
 
 use clap::Args;
+use stc_ts_builtin_types::Lib;
+use stc_ts_env::{Env, ModuleConfig, Rule};
+use stc_ts_file_analyzer::env::EnvFactory;
 use stc_ts_type_checker::Checker;
 use stc_utils::AHashMap;
+use swc_common::{
+    errors::{ColorConfig, Handler},
+    SourceMap,
+};
+use swc_ecma_ast::EsVersion;
 use tokio::sync::Mutex;
 use tower_lsp::{
     async_trait,
@@ -48,6 +56,8 @@ struct Data {
 /// A directory with `package.json` is treated as a package.
 struct TsProject {
     checker: Checker,
+
+    open_cnt: usize,
 }
 
 #[async_trait]
@@ -73,7 +83,39 @@ impl LanguageServer for StcLangServer {
         Ok(())
     }
 
-    async fn did_open(&self, params: DidOpenTextDocumentParams) {}
+    async fn did_open(&self, params: DidOpenTextDocumentParams) {
+        let uri = params.text_document.uri;
+        let path = uri.to_file_path().unwrap();
+        let parent = path.parent().unwrap();
+        let mut data = self.data.lock().await;
+        let project = data.projects.entry(parent.to_path_buf()).or_insert_with(|| {
+            let cm = Arc::new(SourceMap::default());
+            let handler = Arc::new(Handler::with_tty_emitter(ColorConfig::Never, true, false, Some(cm.clone())));
+
+            // TODO: Parse tsconfig.json
+            let env = Env::simple(
+                Rule { ..Default::default() },
+                EsVersion::latest(),
+                ModuleConfig::EsNext,
+                &Lib::load("es2020"),
+            );
+
+            let checker = Checker::new(
+                cm,
+                handler,
+                env,
+                swc_ecma_parser::TsConfig {
+                    // TODO
+                    ..Default::default()
+                },
+                None,
+                resolver,
+            );
+            TsProject { checker, open_cnt: 0 }
+        });
+
+        project.open_cnt += 1;
+    }
 
     async fn hover(&self, _params: HoverParams) -> jsonrpc::Result<Option<Hover>> {
         Ok(Some(Hover {
