0a4b80993bbb4f6500a31eff6d2efbefe49de242
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-25 11:24:09 +0900

    test

--- crates/stc_ts_lang_server/tests/lsp.rs
+++ crates/stc_ts_lang_server/tests/lsp.rs
@@ -1,9 +1,13 @@
 use std::{
+    fs::read_to_string,
     path::{Path, PathBuf},
     process::{Command, Stdio},
 };
 
 use once_cell::sync::Lazy;
+use serde::de::DeserializeOwned;
+use serde_json::{json, Value};
+use stc_ts_testing::lsp::LspClient;
 
 /// Builds the example lsp command, and returns to the path to it.
 fn exec_path() -> PathBuf {
@@ -26,7 +30,45 @@ fn exec_path() -> PathBuf {
     BIN_PATH.to_path_buf()
 }
 
+fn load_fixture(path: &str) -> Value {
+    load_fixture_as(path)
+}
+
+fn load_fixture_as<T>(path: &str) -> T
+where
+    T: DeserializeOwned,
+{
+    let fixture_str = load_fixture_str(path);
+    serde_json::from_str::<T>(&fixture_str).unwrap()
+}
+
+fn load_fixture_str(path: &str) -> String {
+    let fixtures_path = Path::new("tests").join("fixture");
+    let path = fixtures_path.join(path);
+    read_to_string(path).unwrap()
+}
+
+fn init(init_path: &str) -> LspClient {
+    let mut client = LspClient::new(&exec_path(), true).expect("failed to create a lsp client");
+
+    client.write_request::<_, _, Value>("initialize", load_fixture(init_path)).unwrap();
+    client.write_notification("initialized", json!({})).unwrap();
+    client
+}
+
+fn shutdown(client: &mut LspClient) {
+    client.write_request::<_, _, Value>("shutdown", json!(null)).unwrap();
+    client.write_notification("exit", json!(null)).unwrap();
+}
+
 #[test]
 fn test_build() {
     assert!(exec_path().exists());
+}
+
+#[test]
+fn test_init() {
+    let mut client = init("initialize_params.json");
+
+    shutdown(&mut client);
 }
