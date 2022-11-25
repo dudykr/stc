20766d1f881e90cdc254714e7daadb959600c68e
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-25 15:18:47 +0900

    Copy hover test

--- crates/stc_ts_lang_server/tests/lsp.rs
+++ crates/stc_ts_lang_server/tests/lsp.rs
@@ -5,9 +5,11 @@ use std::{
 };
 
 use once_cell::sync::Lazy;
-use serde::de::DeserializeOwned;
+use serde::{de::DeserializeOwned, Serialize};
 use serde_json::{json, Value};
 use stc_ts_testing::lsp::LspClient;
+use stc_utils::AHashSet;
+use tower_lsp::lsp_types::{Diagnostic, PublishDiagnosticsParams};
 
 /// Builds the example lsp command, and returns to the path to it.
 fn exec_path() -> PathBuf {
@@ -56,9 +58,85 @@ fn init(init_path: &str) -> LspClient {
     client
 }
 
+fn did_open<V>(client: &mut LspClient, params: V) -> Vec<PublishDiagnosticsParams>
+where
+    V: Serialize,
+{
+    client.write_notification("textDocument/didOpen", params).unwrap();
+
+    handle_configuration_request(
+        client,
+        json!([{
+          "enable": true,
+          "codeLens": {
+            "test": true
+          }
+        }]),
+    );
+    read_diagnostics(client).0
+}
+
+fn handle_configuration_request(client: &mut LspClient, result: Value) {
+    let (id, method, _) = client.read_request::<Value>().unwrap();
+    assert_eq!(method, "workspace/configuration");
+    client.write_response(id, result).unwrap();
+}
+
+fn read_diagnostics(client: &mut LspClient) -> CollectedDiagnostics {
+    // diagnostics come in batches of three unless they're cancelled
+    let mut diagnostics = vec![];
+    for _ in 0..3 {
+        let (method, response) = client.read_notification::<PublishDiagnosticsParams>().unwrap();
+        assert_eq!(method, "textDocument/publishDiagnostics");
+        diagnostics.push(response.unwrap());
+    }
+    CollectedDiagnostics(diagnostics)
+}
+
 fn shutdown(client: &mut LspClient) {
     client.write_request::<_, _, Value>("shutdown", json!(null)).unwrap();
     client.write_notification("exit", json!(null)).unwrap();
+}
+
+#[derive(Debug, Clone)]
+struct CollectedDiagnostics(Vec<PublishDiagnosticsParams>);
+
+impl CollectedDiagnostics {
+    /// Gets the diagnostics that the editor will see after all the publishes.
+    pub fn viewed(&self) -> Vec<Diagnostic> {
+        self.viewed_messages().into_iter().flat_map(|m| m.diagnostics).collect()
+    }
+
+    /// Gets the messages that the editor will see after all the publishes.
+    pub fn viewed_messages(&self) -> Vec<PublishDiagnosticsParams> {
+        // go over the publishes in reverse order in order to get
+        // the final messages that will be shown in the editor
+        let mut messages = Vec::new();
+        let mut had_specifier = AHashSet::default();
+        for message in self.0.iter().rev() {
+            if had_specifier.insert(message.uri.clone()) {
+                messages.insert(0, message.clone());
+            }
+        }
+        messages
+    }
+
+    pub fn with_source(&self, source: &str) -> PublishDiagnosticsParams {
+        self.viewed_messages()
+            .iter()
+            .find(|p| p.diagnostics.iter().any(|d| d.source == Some(source.to_string())))
+            .map(ToOwned::to_owned)
+            .unwrap()
+    }
+
+    // pub fn with_file_and_source(&self, specifier: &str, source: &str) ->
+    // PublishDiagnosticsParams {     let specifier =
+    // ModuleSpecifier::parse(specifier).unwrap();     self.viewed_messages()
+    //         .iter()
+    //         .find(|p| p.uri == specifier && p.diagnostics.iter().any(|d| d.source
+    // == Some(source.to_string())))         .map(ToOwned::to_owned)
+    //         .unwrap()
+    // }
 }
 
 #[test]
@@ -70,5 +148,60 @@ fn test_build() {
 fn test_init() {
     let mut client = init("initialize_params.json");
 
+    shutdown(&mut client);
+}
+
+#[test]
+fn test_hover() {
+    let mut client = init("initialize_params.json");
+    did_open(
+        &mut client,
+        json!({
+          "textDocument": {
+            "uri": "file:///a/file.ts",
+            "languageId": "typescript",
+            "version": 1,
+            "text": "console.log(Deno.args);\n"
+          }
+        }),
+    );
+    let (maybe_res, maybe_err) = client
+        .write_request(
+            "textDocument/hover",
+            json!({
+              "textDocument": {
+                "uri": "file:///a/file.ts"
+              },
+              "position": {
+                "line": 0,
+                "character": 19
+              }
+            }),
+        )
+        .unwrap();
+    assert!(maybe_err.is_none());
+    assert_eq!(
+        maybe_res,
+        Some(json!({
+          "contents": [
+            {
+              "language": "typescript",
+              "value": "const Deno.args: string[]"
+            },
+            "Returns the script arguments to the program.\n\nGive the following command line invocation of Deno:\n\n```sh\ndeno run --allow-read https://deno.land/std/examples/cat.ts /etc/passwd\n```\n\nThen `Deno.args` will contain:\n\n```\n[ \"/etc/passwd\" ]\n```\n\nIf you are looking for a structured way to parse arguments, there is the\n[`std/flags`](https://deno.land/std/flags) module as part of the Deno\nstandard library.",
+            "\n\n*@category* - Runtime Environment",
+          ],
+          "range": {
+            "start": {
+              "line": 0,
+              "character": 17
+            },
+            "end": {
+              "line": 0,
+              "character": 21
+            }
+          }
+        }))
+    );
     shutdown(&mut client);
 }
