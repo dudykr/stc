e1c4c4414f3b23b340d5bc69af9c344ba27354a9
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-25 11:01:32 +0900

    More work for lsp client

--- crates/stc_ts_testing/src/lsp.rs
+++ crates/stc_ts_testing/src/lsp.rs
@@ -1,22 +1,33 @@
 use std::{path::Path, process::Stdio};
 
 use anyhow::{Context, Result};
-use tokio::process::Child;
+use tokio::{
+    io::{BufReader, BufWriter},
+    process::{Child, ChildStdin, ChildStdout},
+};
 
 pub struct LspClient {
     child: Child,
+
+    reader: BufReader<ChildStdout>,
+    writer: BufWriter<ChildStdin>,
 }
 
 impl LspClient {
     pub async fn new(exec_path: &Path) -> Result<Self> {
-        let child = tokio::process::Command::new(exec_path)
+        let mut child = tokio::process::Command::new(exec_path)
             .stdin(Stdio::piped())
             .stdout(Stdio::piped())
-            .stderr(Stdio::piped())
             .kill_on_drop(true)
             .spawn()
             .context("failed to spawn lsp executable")?;
 
-        Ok(LspClient { child })
+        let stdout = child.stdout.take().unwrap();
+        let reader = BufReader::new(stdout);
+
+        let stdin = child.stdin.take().unwrap();
+        let writer = BufWriter::new(stdin);
+
+        Ok(LspClient { child, reader, writer })
     }
 }
