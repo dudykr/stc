99f096705e4aba6c1d27887e5bb898a7f127676b
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-27 16:53:40 +0900

    Reduce

--- crates/stc_ts_file_analyzer/tests/visualize/controlFlow/controlFlowTypeofObject/1.ts
+++ crates/stc_ts_file_analyzer/tests/visualize/controlFlow/controlFlowTypeofObject/1.ts
@@ -1,16 +1,6 @@
-// @strict: true
-// @declaration: true
+//@strict: true
 
 declare function obj(x: object): void;
-
-function f1(x: unknown) {
-    if (!x) {
-        return;
-    }
-    if (typeof x === 'object') {
-        obj(x);
-    }
-}
 
 function f2(x: unknown) {
     if (x === null) {
@@ -21,51 +11,4 @@ function f2(x: unknown) {
     }
 }
 
-function f3(x: unknown) {
-    if (x == null) {
-        return;
-    }
-    if (typeof x === 'object') {
-        obj(x);
-    }
-}
-
-function f4(x: unknown) {
-    if (x == undefined) {
-        return;
-    }
-    if (typeof x === 'object') {
-        obj(x);
-    }
-}
-
-function f5(x: unknown) {
-    if (!!true) {
-        if (!x) {
-            return;
-        }
-    }
-    else {
-        if (x === null) {
-            return;
-        }
-    }
-    if (typeof x === 'object') {
-        obj(x);
-    }
-}
-
-function f6(x: unknown) {
-    if (x === null) {
-        x;
-    }
-    else {
-        x;
-        if (typeof x === 'object') {
-            obj(x);
-        }
-    }
-    if (typeof x === 'object') {
-        obj(x);  // Error
-    }
-}
+export { }
