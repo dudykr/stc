2e177436b17f23cd9c8dfcf4d662e3f8ca156b1e
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-30 17:44:08 +0900

    Remove

--- crates/stc_ts_file_analyzer/tests/pass/exprs/call/overloadResolution/1.ts
+++
@@ -1,9 +0,0 @@
-
-// Non - generic overloads where contextual typing of function arguments has errors
-function fn5(f: (n: string) => void): string;
-function fn5(f: (n: number) => void): number;
-function fn5() { return undefined; }
-var n = fn5((name) => name.toFixed());
-
-
-export { }
