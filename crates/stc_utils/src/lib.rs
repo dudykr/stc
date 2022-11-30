cd54e0d6f572d81684f0a3ba58073467c47b8da3
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-11-30 17:42:30 +0900

    dedup

--- crates/stc_utils/src/lib.rs
+++ crates/stc_utils/src/lib.rs
@@ -6,6 +6,7 @@ extern crate swc_node_base;
 use std::{
     collections::{HashMap, HashSet},
     env,
+    mem::take,
 };
 
 use once_cell::sync::Lazy;
@@ -50,3 +51,31 @@ pub trait TryOpt<T>: Sized + Into<Option<T>> {
 }
 
 impl<T> TryOpt<T> for Option<T> {}
+
+pub fn dedup<T>(v: &mut Vec<T>)
+where
+    T: Eq,
+{
+    let mut remove_list = vec![];
+
+    for (i, i1) in v.iter().enumerate() {
+        for (j, j1) in v.iter().enumerate() {
+            if i < j && i1 == j1 {
+                remove_list.push(j);
+            }
+        }
+    }
+
+    // Fast path. We don't face real duplicates in most cases.
+    if remove_list.is_empty() {
+        return;
+    }
+
+    let new = take(v)
+        .into_iter()
+        .enumerate()
+        .filter_map(|(idx, value)| if remove_list.contains(&idx) { None } else { Some(value) })
+        .collect::<Vec<_>>();
+
+    *v = new;
+}
