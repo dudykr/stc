ee7131f830ee8a1aea09cf1f244aea4424a2ea51
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-19 19:12:57 +0900

    test(ts/analyzer): Ignore wrong test references (#414)
    **Related issue:**
    
     - Closes https://github.com/dudykr/stc/issues/408.

--- crates/stc_ts_file_analyzer/tests/pass/types/generic/call-chain-1.ts
+++ crates/stc_ts_file_analyzer/tests/pass/types/generic/call-chain-1.ts
@@ -4,5 +4,5 @@ export const n3: number | undefined = a?.m?.({ x: 12 }); // should be ok
 export const n4: number | undefined = a?.m?.({ x: absorb() }); // likewise
 
 // Also a test showing `!` vs `?` for good measure
-export let t1 = a?.m?.({ x: 12 });
+export let t1: 12 | undefined = a?.m?.({ x: 12 });
 t1 = a!.m!({ x: 12 });
