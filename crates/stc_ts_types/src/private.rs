c1fdfbd02009ff3096fc7c27716aad0cf7572d1b
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-21 14:03:56 +0900

    Generic

--- crates/stc_ts_types/src/private.rs
+++ crates/stc_ts_types/src/private.rs
@@ -1,21 +1,34 @@
 use rnode::Visit;
 use serde::{Deserialize, Serialize};
 use swc_common::{EqIgnoreSpan, TypeEq};
+use tracing::info;
 
 /// A type used to make types private, so we can track all type creation.
-#[derive(Debug, Default, Clone, Copy, PartialEq, Visit, Serialize, Deserialize)]
-pub(crate) struct Private {}
+#[derive(Debug, Clone, Copy, PartialEq, Visit, Serialize, Deserialize)]
+pub(crate) struct Private<const N: &'static str> {
+    _priv: (),
+}
 
-impl EqIgnoreSpan for Private {
+impl<const N: &'static str> EqIgnoreSpan for Private<N> {
     #[inline(always)]
     fn eq_ignore_span(&self, _: &Self) -> bool {
         true
     }
 }
 
-impl TypeEq for Private {
+impl<const N: &'static str> TypeEq for Private<N> {
     #[inline(always)]
     fn type_eq(&self, _: &Self) -> bool {
         true
+    }
+}
+
+impl<const N: &'static str> Default for Private<N> {
+    #[inline(always)]
+    fn default() -> Self {
+        #[cfg(debug_assertions)]
+        info!("Creating `{}`", N);
+
+        Self { _priv: () }
     }
 }
