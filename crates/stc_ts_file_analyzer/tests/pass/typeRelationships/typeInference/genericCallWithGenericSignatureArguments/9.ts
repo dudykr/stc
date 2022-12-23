0e67a98f43edb46ca606b803542f92d8cbb1460c
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-23 13:06:08 +0900

    Add

---
+++ crates/stc_ts_file_analyzer/tests/pass/typeRelationships/typeInference/genericCallWithGenericSignatureArguments/9.ts
@@ -0,0 +1,6 @@
+// When a function expression is inferentially typed (section 4.9.3) and a type assigned to a parameter in that expression references type parameters for which inferences are being made, 
+// the corresponding inferred type arguments to become fixed and no further candidate inferences are made for them.
+
+declare function foo<T>(x: T, a: (x: T) => T, b: (x: T) => T);
+var r4 = foo(1, (x) => '', (x) => ''); // any => any
+export { }
