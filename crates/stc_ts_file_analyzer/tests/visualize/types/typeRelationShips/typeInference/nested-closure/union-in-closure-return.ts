ab73dbc1728dbf83b32d307425465ae4a8f951db
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-25 19:42:55 +0900

    Fix input

--- crates/stc_ts_file_analyzer/tests/visualize/types/typeRelationShips/typeInference/nested-closure/union-in-closure-return.ts
+++ crates/stc_ts_file_analyzer/tests/visualize/types/typeRelationShips/typeInference/nested-closure/union-in-closure-return.ts
@@ -3,4 +3,4 @@
 export declare function foo<T>(a: (t: T) => (t: T) => T): T;
 
 
-foo((t) => (t) => null as any as string | number) // string | number
+foo((t) => (t) => null as any as string | number) // unknown
