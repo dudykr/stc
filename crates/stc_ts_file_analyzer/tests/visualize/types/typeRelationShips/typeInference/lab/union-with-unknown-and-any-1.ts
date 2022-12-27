93069300ba81d9f5da49fff772a35400f8b0611a
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-27 10:07:38 +0900

    split

--- crates/stc_ts_file_analyzer/tests/visualize/types/typeRelationShips/typeInference/lab/union-with-unknown-and-any-1.ts
+++ crates/stc_ts_file_analyzer/tests/visualize/types/typeRelationShips/typeInference/lab/union-with-unknown-and-any-1.ts
@@ -3,6 +3,5 @@
 declare function foo<T>(x: T, a: T, b: T): T;
 
 foo(null as any, null as any as string | number | boolean, null as any as unknown) // unknown
-foo(null as any as unknown, null as any as string | number | boolean, null as any) // any
 
 export { }
