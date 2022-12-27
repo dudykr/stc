1eec417a40852b9fa7388e3f0541ff524bf91182
sunrabbit123 <qudwls185@naver.com>
2022-12-27 17:24:33 +0900

    resolve numericStringLiteralTypes

--- crates/stc_ts_file_analyzer/src/analyzer/types/mod.rs
+++ crates/stc_ts_file_analyzer/src/analyzer/types/mod.rs
@@ -553,7 +553,18 @@ impl Analyzer<'_, '_> {
                             if let Type::IndexedAccessType(prop_ty) = prop_ty.normalize() {
                                 match prop_ty.index_type.normalize() {
                                     // if you want remove this, first you should overflow narrow any
-                                    Type::Param(..) => {}
+                                    Type::Param(..)
+                                    | Type::Keyword(KeywordType {
+                                        kind:
+                                            TsKeywordTypeKind::TsNumberKeyword
+                                            | TsKeywordTypeKind::TsStringKeyword
+                                            | TsKeywordTypeKind::TsSymbolKeyword,
+                                        ..
+                                    })
+                                    | Type::Lit(LitType {
+                                        lit: RTsLit::Str(..) | RTsLit::Number(..),
+                                        ..
+                                    }) => {}
                                     _ => {
                                         panic!("{:?}", prop_ty);
                                     }
