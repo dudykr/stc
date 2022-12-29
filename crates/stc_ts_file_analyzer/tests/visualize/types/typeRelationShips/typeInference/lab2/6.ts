3373655756a86cf3834e7c1656c9fe11e159d446
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-29 17:51:48 +0900

    Fix wrong test

--- crates/stc_ts_file_analyzer/tests/visualize/types/typeRelationShips/typeInference/lab2/6.ts
+++ crates/stc_ts_file_analyzer/tests/visualize/types/typeRelationShips/typeInference/lab2/6.ts
@@ -2,6 +2,6 @@
 
 declare function f2<T>(a: T, b: (a: T) => T): T
 
-f2('a', (t: boolean) => null as any as string | number) // string | number
+f2('a', (t: boolean) => null as any as string | number) // boolean
 
 export { }
