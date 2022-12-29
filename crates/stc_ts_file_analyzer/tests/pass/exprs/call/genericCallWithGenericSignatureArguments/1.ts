d0551536cdab8f6e808aab4b8bcd85bb137bfebd
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-29 17:22:21 +0900

    Fix test input

--- crates/stc_ts_file_analyzer/tests/pass/exprs/call/genericCallWithGenericSignatureArguments/1.ts
+++ crates/stc_ts_file_analyzer/tests/pass/exprs/call/genericCallWithGenericSignatureArguments/1.ts
@@ -7,6 +7,6 @@ function foo<T>(a: (x: T) => T, b: (x: T) => T) {
 }
 
 //var r1 = foo((x: number) => 1, (x: string) => ''); // error
-var r1b = foo((x) => 1, (x) => ''); // {} => {}
+var r1b = foo((x) => 1, (x) => ''); // unknown => unknown
 
 export { }
