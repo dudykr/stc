b167cb18c90159ee26e592e05b466bd3f8cb54c4
Donny/강동윤 <kdy1997.dev@gmail.com>
2022-12-29 20:50:30 +0900

    Add tests

---
+++ crates/stc_ts_file_analyzer/tests/visualize/types/typeRelationShips/typeInference/closure-1/builtin-object-in-closure-param-2.ts
@@ -0,0 +1,5 @@
+
+
+export declare function foo2<T>(x: T, a: (t: T) => T, b: (t: T) => T): T;
+
+foo2(null, (t: Object) => t, (t: string) => t) // string
---
+++ crates/stc_ts_file_analyzer/tests/visualize/types/typeRelationShips/typeInference/closure-1/builtin-object-in-closure-param-3.ts
@@ -0,0 +1,5 @@
+
+
+export declare function foo2<T>(x: T, a: (t: T) => T, b: (t: T) => T): T;
+
+foo2('a', (t: Object) => t, (t: string) => t) // Object
