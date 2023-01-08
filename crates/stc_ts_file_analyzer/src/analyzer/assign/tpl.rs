c0abd16f40e1572df150e10a6cbfa2acccdbe158
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-08 21:33:24 +0900

    More wokr

--- crates/stc_ts_file_analyzer/src/analyzer/assign/tpl.rs
+++ crates/stc_ts_file_analyzer/src/analyzer/assign/tpl.rs
@@ -28,113 +28,22 @@ impl Analyzer<'_, '_> {
         let span = opts.span;
         let r_ty = r_ty.normalize();
 
-        match r_ty {
-            Type::Tpl(r) => {
-                let _ctx = ctx!(format!(
-                    "lhs = {}\nrhs = {}",
-                    dump_type_as_string(&Type::Tpl(l.clone())),
-                    dump_type_as_string(r_ty)
-                ));
+        let types = self.infer_types_from_tpl_lit_type(span, r_ty, l)?;
 
-                {
-                    // First
-                    let (l, r) = (&l.quasis.first().unwrap().value, &r.quasis.first().unwrap().value);
-                    if !r.starts_with(&**l) {
-                        return Err(ErrorKind::SimpleAssignFailed { span, cause: None }.context("starts_with"));
-                    }
-                }
+        let types = match types {
+            Some(types) => types,
+            None => return Err(ErrorKind::SimpleAssignFailed { span, cause: None }.context("tried to infer")),
+        };
 
-                {
-                    // Last
-                    let (l, r) = (&l.quasis.last().unwrap().value, &r.quasis.last().unwrap().value);
-                    if !r.ends_with(&**l) {
-                        return Err(ErrorKind::SimpleAssignFailed { span, cause: None }.context("ends_with"));
-                    }
-                }
-
-                // TODO(kdy1): We should iterator over two types, and check if each element is
-                // assignable.
-
-                let mut li = 0;
-                let mut ri = 0;
-
-                while li < l.quasis.len() + l.types.len() && ri < r.quasis.len() + r.types.len() {
-                    // 0: quasi, 1: type
-
-                    if li % 2 == 0 {
-                        // LHS is literal
-                        if ri % 2 == 0 {
-                            // RHS is literal
-                            if l.quasis[li / 2].value != r.quasis[ri / 2].value {
-                                // TODO: Restore this after implementing correct
-                                // logic
-
-                                // return Err(
-                                //     Error::SimpleAssignFailed { span, cause:
-                                // None }.context("failed to assign a literal to
-                                // literal") );
-                            }
-                        } else {
-                            // TODO: Restore this after implementing correct
-                            // logic
-
-                            // RHS is type
-
-                            // return Err(Error::SimpleAssignFailed { span,
-                            // cause: None }.context("cannot assign expression
-                            // to literal"));
-                        }
-                    } else {
-                        // LHS is type
-
-                        // We should eat as much text as possible.
-
-                        if ri % 2 == 0 {
-                            // RHS is literal
-                        } else {
-                            // RHS is type
-
-                            // TODO: Restore this after implementing correct
-                            // logic
-
-                            // let l = &l.types[li / 2];
-                            // let r = &r.types[ri / 2];
-
-                            // self.assign_inner(data, l, r, opts)
-                            //     .context("tried to assign a type to a type
-                            // for a template literal")?;
-                        }
-                    }
-
-                    // Bump
-                    li += 1;
-                    ri += 1;
-                }
-
-                Ok(())
+        for (i, ty) in types.iter().enumerate() {
+            if !self.is_valid_type_for_tpl_lit_placeholder(ty, &l.types[i])? {
+                return Err(ErrorKind::SimpleAssignFailed { span, cause: None }.context("verified types"));
             }
-            Type::Lit(LitType { lit: RTsLit::Str(r), .. }) => {
-                let mut start = 0;
-                let mut positions = vec![];
-
-                for item in &l.quasis {
-                    let q = &item.value;
-                    if r.value.len() < start {
-                        return Err(ErrorKind::SimpleAssignFailed { span, cause: None }.into());
-                    }
-                    match r.value[start..].find(&**q) {
-                        Some(pos) => {
-                            positions.push(pos);
-                            start += pos + 1;
-                        }
-                        None => return Err(ErrorKind::SimpleAssignFailed { span, cause: None }.into()),
-                    }
-                }
-
-                Ok(())
-            }
-
-            _ => Err(ErrorKind::SimpleAssignFailed { span, cause: None }.into()),
         }
+
+        Ok(())
     }
+
+    /// Ported from `isValidTypeForTemplateLiteralPlaceholder` of `tsc`
+    pub(crate) fn is_valid_type_for_tpl_lit_placeholder(&mut self, source: &Type, target: &Type) -> VResult<bool> {}
 }
