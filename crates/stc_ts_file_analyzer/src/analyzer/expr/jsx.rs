3011880842e5ec6f387ff5c9ad89db61cd5dd7f2
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-10 20:50:20 +0900

    More work

--- crates/stc_ts_file_analyzer/src/analyzer/expr/jsx.rs
+++ crates/stc_ts_file_analyzer/src/analyzer/expr/jsx.rs
@@ -1,6 +1,7 @@
 use stc_ts_ast_rnode::{RJSXElement, RJSXElementChild, RJSXElementName, RJSXFragment, RJSXMemberExpr, RJSXNamespacedName, RJSXObject};
+use stc_ts_errors::ErrorKind;
 use stc_ts_file_analyzer_macros::validator;
-use stc_ts_types::{Id, Type, TypeParamInstantiation};
+use stc_ts_types::{CommonTypeMetadata, Id, KeywordTypeMetadata, Type, TypeParamInstantiation};
 
 use super::TypeOfMode;
 use crate::{analyzer::Analyzer, validator::ValidateWith, VResult};
@@ -63,7 +64,24 @@ impl Analyzer<'_, '_> {
                 if ident.sym.starts_with(|c: char| c.is_ascii_uppercase()) {
                     ident.validate_with_default(self)
                 } else {
-                    self.get_jsx_namespace()
+                    if let Some(jsx) = self.get_jsx_namespace() {
+                    } else {
+                        if !self.ctx.in_declare && self.rule().no_implicit_any {
+                            self.storage
+                                .report(ErrorKind::ImplicitAny { span: ident.span }.context("jsx namespace not found"))
+                        }
+
+                        Ok(Type::any(
+                            ident.span,
+                            KeywordTypeMetadata {
+                                common: CommonTypeMetadata {
+                                    implicit: true,
+                                    ..Default::default()
+                                },
+                                ..Default::default()
+                            },
+                        ))
+                    }
                 }
             }
             RJSXElementName::JSXMemberExpr(e) => e.validate_with(self),
