857b6edd9730416690c923f6af7029a42bc2d5ad
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-12 21:38:46 +0900

    More work

--- crates/stc_ts_file_analyzer/src/analyzer/relation/mod.rs
+++ crates/stc_ts_file_analyzer/src/analyzer/relation/mod.rs
@@ -11,40 +11,37 @@ pub(crate) enum Relation {
     StrictSubtype,
 }
 
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
-pub(crate) struct IsRelatedOpts {
-    pub kind: Relation,
-}
-
 #[derive(Debug, Default)]
 struct IsRelatedData {}
 
 impl Analyzer<'_, '_> {
     /// Ported from `isTypeRelatedTo` of `tsc`.
-    pub(crate) fn is_type_related_to(&mut self, source: &Type, target: &Type, opts: IsRelatedOpts) -> bool {
+    pub(crate) fn is_type_related_to(&mut self, source: &Type, target: &Type, relation: Relation) -> bool {
         let mut data = IsRelatedData::default();
-        self.is_type_related_to_inner(&mut data, source, target, opts)
+        self.is_type_related_to_inner(&mut data, source, target, relation)
     }
 
-    fn is_type_related_to_inner(&mut self, data: &mut IsRelatedData, source: &Type, target: &Type, opts: IsRelatedOpts) -> bool {
-        let relation = opts.kind;
-
+    fn is_type_related_to_inner(&mut self, data: &mut IsRelatedData, source: &Type, target: &Type, relation: Relation) -> bool {
         if source.type_eq(target) {
             return true;
         }
 
-        if relation == Relation::Comparable && !target.is_never() && self.is_simple_type_related_to(target, source, opts)
-            || self.is_simple_type_related_to(source, target, opts)
-        {
-            return true;
+        if relation != Relation::Identity {
+            if relation == Relation::Comparable && !target.is_never() && self.is_simple_type_related_to(target, source, opts)
+                || self.is_simple_type_related_to(source, target, relation)
+            {
+                return true;
+            }
         }
 
-        match opts.kind {
-            Relation::Identity => false,
-            Relation::Comparable => false,
-            Relation::StrictSubtype => true,
+        if source.is_structured_or_instantiable() || target.is_structured_or_instantiable() {
+            return self.check_type_related_to(source, target, relation);
         }
+
+        false
     }
 
-    fn is_simple_type_related_to(&mut self, source: &Type, target: &Type, opts: IsRelatedOpts) -> bool {}
+    fn is_simple_type_related_to(&mut self, source: &Type, target: &Type, relation: Relation) -> bool {}
+
+    fn check_type_related_to(&mut self, source: &Type, target: &Type, relation: Relation) -> bool {}
 }
