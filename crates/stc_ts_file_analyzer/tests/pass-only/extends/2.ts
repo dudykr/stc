3bae4ee151e46f3c35abd90c455761d4e89b012d
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-12 17:52:20 +0900

    Add tests for `extends`

---
+++ crates/stc_ts_file_analyzer/tests/pass-only/extends/2.ts
@@ -0,0 +1,16 @@
+
+
+type A = {
+    f(): void;
+}
+
+type B = {
+    f(x?: string): void;
+    g(): void;
+}
+
+
+type T1 = Extract<A | B, A>
+
+export let _11: T1 = null as any as A | B;
+export let _12: A | B = null as any as T1;
---
+++ crates/stc_ts_file_analyzer/tests/pass-only/extends/3.ts
@@ -0,0 +1,16 @@
+
+
+type A = {
+    f(): void;
+}
+
+type B = {
+    f(x?: string): void;
+    g(): void;
+}
+
+
+type T2 = Extract<A | B, B>
+
+export let _21: T2 = null as any as B;
+export let _22: B = null as any as T2;
