d4e222d1f92cd14ccea183e56e4b25f33480a23d
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-13 22:14:18 +0900

    Remove unused: `pass`

--- crates/stc_ts_type_checker/tests/pass/call/.signature/index.ts
+++
@@ -1,8 +0,0 @@
-interface Foo {
-  (n: number): number;
-  (): string;
-}
-
-let a = {} as Foo;
-let b: number = a(1);
-let c: string = a();
--- crates/stc_ts_type_checker/tests/pass/copied/.union-type-with-index-signature/index.ts
+++
@@ -1,4 +0,0 @@
-type Two = { foo: { bar: true }, baz: true } | { [s: string]: string };
-declare var u: Two
-u.foo = 'bye'
-u.baz = 'hi'
