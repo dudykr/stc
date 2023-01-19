14aa7783770217b01911635dfdf015c3f3fe7a95
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-19 20:09:45 +0900

    types-pkg

--- crates/stc_ts_type_checker/scripts/instrument/types-pkg.sh
+++ crates/stc_ts_type_checker/scripts/instrument/types-pkg.sh
@@ -7,7 +7,7 @@ export RUST_LOG=off
 export MIMALLOC_SHOW_STATS=1
 export CARGO_MANIFEST_DIR="$(pwd)"
 export RUST_MIN_STACK=$((16 * 1024 * 1024))
-
+export CARGO_PROFILE_RELEASE_DEBUG=true
 export STC_SKIP_EXEC=1
 
 cargo profile instruments -t time --features swc_common/concurrent --features tracing/max_level_off --release --test types_pkg $@
