9d00737b7349de74979be7f4998b8dc6512673e4
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-19 18:01:41 +0900

    fix: Fix evaluation of instantiation expressions (#519)

--- crates/stc_ts_file_analyzer/src/analyzer/expr/misc.rs
+++ crates/stc_ts_file_analyzer/src/analyzer/expr/misc.rs
@@ -41,12 +41,12 @@ impl Analyzer<'_, '_> {
         type_args: Option<&TypeParamInstantiation>,
         type_ann: Option<&Type>,
     ) -> VResult<Type> {
+        let type_args = e.type_args.validate_with(self)?;
+
         let ty = e
             .expr
-            .validate_with_args(self, (mode, type_args, type_ann))
+            .validate_with_args(self, (mode, Some(&type_args), type_ann))
             .context("tried to verify expr of ts instantiation expression")?;
-
-        let type_args = e.type_args.validate_with(self)?;
 
         let new = self.expand_generics_with_type_args(e.span, ty, &type_args)?;
 
