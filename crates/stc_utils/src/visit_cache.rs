99f936b052f07d92fca16e015191c54ed74974b5
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-19 22:09:33 +0900

    WIP

--- crates/stc_utils/src/visit_cache.rs
+++ crates/stc_utils/src/visit_cache.rs
@@ -1,1 +1,34 @@
+use std::thread::LocalKey;
 
+use rustc_hash::FxHashMap;
+
+/// A cache for `Visit`.
+///
+/// This prefers the first call.
+///
+///
+/// # Implementation details
+///
+/// This is inspired by `scoped_tls` and `tracing::subscriber::set_default()`.
+#[derive(Debug)]
+pub struct VisitCache<T>
+where
+    T: 'static,
+{
+    data: &'static LocalKey<FxHashMap<*const (), T>>,
+}
+
+impl<T> VisitCache<T>
+where
+    T: 'static,
+{
+    pub fn current(&'static self) -> Option<Self> {}
+}
+
+/// Cleans up the cache on drop.
+pub struct VisitCacheGuard<'a, T>
+where
+    T: 'static,
+{
+    data: &'a VisitCache<T>,
+}
