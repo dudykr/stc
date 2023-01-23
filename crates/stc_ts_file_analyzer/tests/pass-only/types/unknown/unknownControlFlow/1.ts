b9704bbd2a11c1c37b54c491bf090c1bbd965b7b
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-23 19:20:28 +0900

    Add a test

---
+++ crates/stc_ts_file_analyzer/tests/pass-only/types/unknown/unknownControlFlow/1.ts
@@ -0,0 +1,15 @@
+
+// Repro from #48468
+
+export function deepEquals<T>(a: T, b: T): boolean {
+    if (typeof a !== 'object' || typeof b !== 'object' || !a || !b) {
+        return false;
+    }
+    if (Array.isArray(a) || Array.isArray(b)) {
+        return false;
+    }
+    if (Object.keys(a).length !== Object.keys(b).length) { // Error here
+        return false;
+    }
+    return true;
+}
