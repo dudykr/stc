8f48440f9ef132d517a5cd357cfd8f7698f99dbe
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-24 10:04:26 +0900

    Reduce

--- crates/stc_ts_file_analyzer/tests/visualize/controlFlow/controlFlowOptionalChain/1.ts
+++ crates/stc_ts_file_analyzer/tests/visualize/controlFlow/controlFlowOptionalChain/1.ts
@@ -4,10 +4,8 @@
 export function f40(o: Thing | undefined) {
     switch (o?.foo) {
         case "abc":
-            o.foo;
             break;
         case 42:
-            o.foo;
             break;
         case undefined:
             o.foo;  // Error
