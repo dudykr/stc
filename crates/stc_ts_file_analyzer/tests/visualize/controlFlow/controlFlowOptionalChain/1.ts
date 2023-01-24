7e28b3336ec63f2e060f56ef947df812f44cdbd0
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-24 10:13:09 +0900

    fix test input

--- crates/stc_ts_file_analyzer/tests/visualize/controlFlow/controlFlowOptionalChain/1.ts
+++ crates/stc_ts_file_analyzer/tests/visualize/controlFlow/controlFlowOptionalChain/1.ts
@@ -1,6 +1,9 @@
 //@strict: true
 //@allowUnreachableCode: false
 
+
+type Thing = { foo: string | number, bar(): number, baz: object };
+
 export function f40(o: Thing | undefined) {
     switch (o?.foo) {
         case "abc":
