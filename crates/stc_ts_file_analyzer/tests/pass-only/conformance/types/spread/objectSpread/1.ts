091f98c6d0d2ec6fc2f4ec589144bea1e03e8c2f
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-24 16:22:15 +0900

    Split tests

--- crates/stc_ts_file_analyzer/tests/pass-only/conformance/types/spread/objectSpread/1.ts
+++ crates/stc_ts_file_analyzer/tests/pass-only/conformance/types/spread/objectSpread/1.ts
@@ -6,11 +6,5 @@ function f<T, U>(t: T, u: U) {
 
 let exclusive: { id: string, a: number, b: string, c: string, d: boolean } =
     f({ a: 1, b: 'yes' }, { c: 'no', d: false })
-let overlap: { id: string, a: number, b: string } =
-    f({ a: 1 }, { a: 2, b: 'extra' })
-let overlapConflict: { id: string, a: string } =
-    f({ a: 1 }, { a: 'mismatch' })
-let overwriteId: { id: string, a: number, c: number, d: string } =
-    f({ a: 1, id: true }, { c: 1, d: 'no' })
 
 export { }
