c09835343884064bd3b2543a780f150a4c480b6a
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-27 21:41:41 +0900

    Add a test

---
+++ crates/stc_ts_file_analyzer/tests/pass-only/conformance/types/mapped/isomorphicMappedTypeInference/1.ts
@@ -0,0 +1,15 @@
+//@strict: true
+
+// Repro from #29765
+
+function getProps<T, K extends keyof T>(obj: T, list: K[]): Pick<T, K> {
+    return {} as any;
+}
+
+const myAny: any = {};
+
+const o1 = getProps(myAny, ['foo', 'bar']);
+
+const o2: { foo: any; bar: any } = getProps(myAny, ['foo', 'bar']);
+
+export { }
