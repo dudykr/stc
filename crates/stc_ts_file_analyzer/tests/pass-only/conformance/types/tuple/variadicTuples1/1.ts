46f3435a408b9dea4ba9e9deab2be89f91aaf5f2
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-27 22:01:44 +0900

    Add a test

--- crates/stc_ts_file_analyzer/tests/pass-only/conformance/types/tuple/variadicTuples1/1.ts
+++ crates/stc_ts_file_analyzer/tests/pass-only/conformance/types/tuple/variadicTuples1/1.ts
@@ -7,3 +7,17 @@ function f23<U extends string[]>(args: [...U, number]) {
 }
 
 export { }
+
+// Mapped types applied to variadic tuple types
+
+type Arrayify<T> = { [P in keyof T]: T[P][] };
+
+
+
+// Reverse mapping through mapped type applied to variadic tuple type
+
+declare function fm1<T extends unknown[]>(t: Arrayify<[string, number, ...T]>): T;
+
+var tm1 = fm1([['abc'], [42], [true], ['def']]);  // [boolean, string]
+var tm1: [boolean, string] = [true, 'def']
+export { tm1 }
