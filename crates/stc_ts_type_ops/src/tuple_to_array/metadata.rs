7bbb0c78a937ff909e842bdc5ff29f3c6d0937ed
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-28 22:51:52 +0900

    perf: Migrate simple operations to `replace_type` (#570)
    **Description:**
    
    Patched operations:
     - `rename_inferred`
     - `prevent_tuple_to_array`
     - `prevent_generalize`
     - `generic::SingleTypeParamReplacer`
     - `expr::ConstraintReducer`

--- crates/stc_ts_type_ops/src/tuple_to_array/metadata.rs
+++ crates/stc_ts_type_ops/src/tuple_to_array/metadata.rs
@@ -1,27 +1,17 @@
-use rnode::{VisitMut, VisitMutWith};
-use stc_ts_ast_rnode::RIdent;
-use stc_ts_types::Type;
+use stc_ts_types::{replace::replace_type, Type};
+use stc_ts_utils::MapWithMut;
 use tracing::instrument;
 
 /// TODO(kdy1): Optimize by visiting only tuple types.
 #[instrument(skip(ty))]
 pub fn prevent_tuple_to_array(ty: &mut Type) {
-    ty.visit_mut_with(&mut PreventTupleToArray);
-}
-
-struct PreventTupleToArray;
-
-impl VisitMut<Type> for PreventTupleToArray {
-    fn visit_mut(&mut self, ty: &mut Type) {
-        // TODO(kdy1): PERF
-        ty.normalize_mut();
-        ty.metadata_mut().prevent_tuple_to_array = true;
-
-        ty.visit_mut_children_with(self);
-    }
-}
-
-/// Prevent interop with hygiene.
-impl VisitMut<RIdent> for PreventTupleToArray {
-    fn visit_mut(&mut self, _: &mut RIdent) {}
+    replace_type(
+        ty,
+        |ty| ty.is_tuple(),
+        |ty| {
+            let mut ty = ty.take();
+            ty.metadata_mut().prevent_tuple_to_array = true;
+            Some(ty)
+        },
+    )
 }
