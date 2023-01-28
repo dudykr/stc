106bdb5436ca80057dc6f0b286fb500b8068a8f5
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-29 00:28:32 +0900

    Wrong scope

--- crates/stc_ts_file_analyzer/tests/pass-only/types/tuple/variadicTuples1/1.ts
+++ crates/stc_ts_file_analyzer/tests/pass-only/types/tuple/variadicTuples1/1.ts
@@ -1,6 +1,5 @@
 //@strict: true
 
-declare function f22<T extends unknown[] = []>(args: [...T, number]): T;
 declare function f22<T extends unknown[] = []>(args: [...T]): T;
 
 function f23<U extends string[]>(args: [...U, number]) {
