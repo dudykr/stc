ccffb1bf630333302a8b513d33d65f428ee1bfef
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-29 00:02:39 +0900

    REduce

--- crates/stc_ts_file_analyzer/tests/pass-only/types/tuple/variadicTuples1/1.ts
+++ crates/stc_ts_file_analyzer/tests/pass-only/types/tuple/variadicTuples1/1.ts
@@ -4,9 +4,7 @@ declare function f22<T extends unknown[] = []>(args: [...T, number]): T;
 declare function f22<T extends unknown[] = []>(args: [...T]): T;
 
 function f23<U extends string[]>(args: [...U, number]) {
-    let v1 = f22(args);  // U
     let v2 = f22(["foo", "bar"]);  // [string, string]
-    let v3 = f22(["foo", 42]);  // [string]
 }
 
 export { }
