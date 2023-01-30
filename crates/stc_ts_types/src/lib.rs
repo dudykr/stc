6ef440fb4601aea34fae88b663d4c7e0064fa70a
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-30 18:52:42 +0900

    Revert

--- crates/stc_ts_types/src/lib.rs
+++ crates/stc_ts_types/src/lib.rs
@@ -2237,31 +2237,6 @@ impl Type {
             idx: 0,
         }
     }
-
-    // /// Note: This iterator is infinite if the type is [Type::Rest].
-    // pub fn iter_rest(&self) -> impl '_ + Iterator<Item = &Type> {
-    //     let mut done = false;
-    //     let mut index = 0;
-    //     iter::from_fn(move || {
-    //         if done {
-    //             return None;
-    //         }
-    //         match self.normalize_instance() {
-    //             Type::Rest(rest) => match rest.ty.normalize() {
-    //                 Type::Tuple(tuple) => {
-    //                     let ty = tuple.elems.get(index).map(|el| &*el.ty);
-    //                     index += 1;
-    //                     ty
-    //                 }
-    //                 _ => Some(&*rest.ty),
-    //             },
-    //             _ => {
-    //                 done = true;
-    //                 Some(self)
-    //             }
-    //         }
-    //     })
-    // }
 }
 
 #[derive(Debug)]
