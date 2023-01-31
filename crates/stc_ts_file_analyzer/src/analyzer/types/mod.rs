401798d4e77218e44cf836f0ce400b8f1e987c16
오병진 <64676070+sunrabbit123@users.noreply.github.com>
2023-01-31 11:22:44 +0900

    feat: Improve intersection reduction (#576)

--- crates/stc_ts_file_analyzer/src/analyzer/types/mod.rs
+++ crates/stc_ts_file_analyzer/src/analyzer/types/mod.rs
@@ -977,6 +977,9 @@ impl Analyzer<'_, '_> {
         let enum_variant_len = enum_variant_iter.len();
 
         if enum_variant_len > 0 {
+            if normalized_types.iter().any(|ty| matches!(ty, Type::Lit(..))) {
+                return never!();
+            }
             if let Some(first_enum) = enum_variant_iter.first() {
                 let mut enum_temp = first_enum.normalize();
                 for elem in enum_variant_iter.into_iter() {
