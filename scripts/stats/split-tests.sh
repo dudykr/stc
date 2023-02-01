37257ae1c2bac8c6efad7d9e030e8fc56e9f1795
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-01 22:06:19 +0900

    script

--- scripts/stats/split-tests.sh
+++ scripts/stats/split-tests.sh
@@ -3,3 +3,6 @@ set -eu
 
 # Clone https://gist.github.com/kdy1/8198130f16e42514b22656c57690b124 to your CDPATH
 (cd 8198130f16e42514b22656c57690b124 && git pull || true)
+
+
+find crates/* -name "*.stats.rust-debug" | xargs grep 'extra_error: [1-9][0-9]*' | sort -n -k 3 -t ":" -r > .stc/all-tests-to-split.txt
