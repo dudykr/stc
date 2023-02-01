cdfcd3720af7f237bc6b40f4b89e7aa384f6099d
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-01 22:34:11 +0900

    More

--- scripts/stats/split-tests.sh
+++ scripts/stats/split-tests.sh
@@ -19,9 +19,9 @@ echo "Sorting done.txt"
 find crates/* -name "*.stats.rust-debug" > "$GIST_DIR/.stc/all.txt"
 
 echo "comm"
-comm -23 "$GIST_DIR/.stc/all.txt" "$GIST_DIR/done.txt" > .stc/dedup.txt
+comm -23 "$GIST_DIR/.stc/all.txt" "$GIST_DIR/done.txt" > "$GIST_DIR/.stc/dedup.txt"
 
-cat .stc/dedup.txt \
+cat "$GIST_DIR/.stc/dedup.txt" \
     | xargs grep 'extra_error: [1-9][0-9]*' \
     | sort -n -k 3 -t ":" -r \
     > "$GIST_DIR/list.txt"
