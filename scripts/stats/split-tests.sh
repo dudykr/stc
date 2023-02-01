efbd9b8469c96e0addd2628587c0dc666f742d60
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-01 23:05:00 +0900

    chore: Use gist for splitting tests into issues (#590)
    **Description:**
    
    As splitting tests into GitHub issues is a high-priority task, but it's hard to track status, I created https://gist.github.com/kdy1/8198130f16e42514b22656c57690b124 and I'm going to manage it using the script.
    
    
    **Related issue:**
    
     - https://github.com/dudykr/stc/issues/331.

---
+++ scripts/stats/split-tests.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -eu
+
+function sortFile {
+    mkdir -p .stc
+    cat $1 | awk NF | sort | uniq | awk '{$1=$1};1' | uniq | sort > .stc/tmp.txt
+    mv .stc/tmp.txt $1
+}
+
+git pull || true
+
+# Clone git@github.com:dudykr/stc-issue-split.git to ~/projects
+
+GIST_DIR="$HOME/projects/8198130f16e42514b22656c57690b124"
+echo "Gist dir: $GIST_DIR"
+(cd $GIST_DIR && git pull || true)
+
+echo "Sorting done.txt"
+(cd $GIST_DIR && sortFile done.txt)
+
+find crates/* -name "*.stats.rust-debug" > "$GIST_DIR/.stc/all.txt"
+
+echo "comm"
+sortFile "$GIST_DIR/.stc/all.txt"
+comm -23 "$GIST_DIR/.stc/all.txt" "$GIST_DIR/done.txt" > "$GIST_DIR/.stc/dedup.txt"
+
+cat "$GIST_DIR/.stc/dedup.txt" \
+    | xargs grep 'extra_error: [1-9][0-9]*' \
+    | sort -n -k 3 -t ":" -r \
+    > "$GIST_DIR/list.txt"
+
+(cd $GIST_DIR && git commit -am "Update" && git push)
