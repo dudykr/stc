c362a6bdad0c70e65e6571d5f81fe4718f450841
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-01 22:09:56 +0900

    sort

--- scripts/stats/split-tests.sh
+++ scripts/stats/split-tests.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 set -eu
 
+function sortFile {
+    mkdir -p .stc
+    cat $1 | awk NF | sort | uniq | awk '{$1=$1};1' | uniq | sort > .stc/tmp.txt
+    mv .stc/tmp.txt $1
+}
+
 # Clone https://gist.github.com/kdy1/8198130f16e42514b22656c57690b124 to your CDPATH
 (cd 8198130f16e42514b22656c57690b124 && git pull || true)
 
+(cd 8198130f16e42514b22656c57690b124 && sortFile done.txt)
 
-find crates/* -name "*.stats.rust-debug" | xargs grep 'extra_error: [1-9][0-9]*' | sort -n -k 3 -t ":" -r > .stc/all-tests-to-split.txt
+
+find crates/* -name "*.stats.rust-debug" \
+    | xargs grep 'extra_error: [1-9][0-9]*' \
+    | sort -n -k 3 -t ":" -r \
+    > .stc/all-tests-to-split.txt
