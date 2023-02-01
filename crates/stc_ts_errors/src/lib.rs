273356f921c6152b5a45c41527923a86361ebe13
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-01 23:39:15 +0900

    fix: Use correct error code for assignments to non-variable (#593)

--- crates/stc_ts_errors/src/lib.rs
+++ crates/stc_ts_errors/src/lib.rs
@@ -1702,7 +1702,14 @@ impl ErrorKind {
             ErrorKind::TypeUsedAsVar { .. } => 2693,
             ErrorKind::TypeNotFound { .. } => 2304,
 
-            ErrorKind::NotVariable { .. } => 2539,
+            ErrorKind::NotVariable { ty, .. } => match ty.as_deref().map(Type::normalize) {
+                Some(Type::Enum(..)) => 2628,
+                Some(Type::ClassDef(..)) => 2629,
+                Some(Type::Function(..)) => 2630,
+                Some(Type::Module(..)) => 2631,
+                Some(Type::Import(..)) => 2632,
+                _ => 2539,
+            },
 
             ErrorKind::NoInitAndNoDefault { .. } => 2525,
 
