02fbf26bf9a3f852cdcbe067e8a8542f648fdef0
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-02 18:52:33 +0900

    Enable

--- crates/stc_ts_type_checker/tests/types_pkg.rs
+++ crates/stc_ts_type_checker/tests/types_pkg.rs
@@ -12,7 +12,6 @@ use swc_ecma_loader::resolve::Resolve;
 use swc_ecma_parser::TsConfig;
 
 #[test]
-#[ignore = "Cross-file namespace is not implemented yet"]
 fn test_node() {
     run_tests_for_types_pkg("@types/node/index.d.ts");
 }
