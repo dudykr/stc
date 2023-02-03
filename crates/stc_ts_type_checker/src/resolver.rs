e166e1575fbd035577fce47df180a91fa9ebc131
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-03 11:02:47 +0900

    TaResolver

--- crates/stc_ts_type_checker/src/resolver.rs
+++ crates/stc_ts_type_checker/src/resolver.rs
@@ -1,1 +1,14 @@
+use swc_ecma_loader::resolve::Resolve;
 
+pub struct TsResolver<R: Resolve> {
+    inner: R,
+}
+
+impl<R> TsResolver<R>
+where
+    R: Resolve,
+{
+    pub fn new(inner: R) -> Self {
+        Self { inner }
+    }
+}
