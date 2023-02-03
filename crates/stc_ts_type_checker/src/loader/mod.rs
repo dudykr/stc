0f64004407b12c535657d12d446fadf09082dcc6
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-03 11:12:43 +0900

    Arc

--- crates/stc_ts_type_checker/src/loader/mod.rs
+++ crates/stc_ts_type_checker/src/loader/mod.rs
@@ -1,5 +1,19 @@
+use std::sync::Arc;
+
+use anyhow::Result;
+use swc_common::{FileName, Mark};
+use swc_ecma_ast::Module;
+
 pub mod resolver;
 pub mod store;
 
+#[derive(Debug)]
+pub struct ModuleRecord {
+    pub top_level_mark: Mark,
+    pub data: Module,
+}
+
 /// A module loader.
-pub trait LoadModule: 'static + Send + Sync {}
+pub trait LoadModule: 'static + Send + Sync {
+    fn load(&self, filename: &Arc<FileName>) -> Result<Arc<ModuleRecord>>;
+}
