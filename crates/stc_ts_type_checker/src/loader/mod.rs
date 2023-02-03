f55242fc098eb92241cd0f3ed3d610fe5a0f2d93
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-03 11:21:26 +0900

    ModuleLoader

--- crates/stc_ts_type_checker/src/loader/mod.rs
+++ crates/stc_ts_type_checker/src/loader/mod.rs
@@ -4,6 +4,7 @@ use anyhow::Result;
 use auto_impl::auto_impl;
 use swc_common::{FileName, Mark};
 use swc_ecma_ast::Module;
+use swc_ecma_loader::resolve::Resolve;
 
 pub mod resolver;
 pub mod store;
@@ -19,4 +20,21 @@ pub struct ModuleRecord {
 pub trait LoadModule: 'static + Send + Sync {
     /// This method should return **all modules in a cycle**.
     fn load_module(&self, filename: &Arc<FileName>) -> Result<Vec<Arc<ModuleRecord>>>;
+}
+
+/// A simple implementation of [LoadModule].
+pub struct ModuleLoader<R>
+where
+    R: Resolve,
+{
+    resolver: R,
+}
+
+impl<R> ModuleLoader<R>
+where
+    R: Resolve,
+{
+    pub fn new(resolver: R) -> Self {
+        Self { resolver }
+    }
 }
