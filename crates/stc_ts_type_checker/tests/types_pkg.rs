131462fc9729fbeceef4e845b018c3724fcda542
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-03 14:12:41 +0900

    ignore

--- crates/stc_ts_type_checker/tests/types_pkg.rs
+++ crates/stc_ts_type_checker/tests/types_pkg.rs
@@ -11,6 +11,7 @@ use swc_ecma_ast::EsVersion;
 use swc_ecma_loader::resolve::Resolve;
 
 #[test]
+#[ignore = "Cross-file namespace is not supported"]
 fn test_node() {
     run_tests_for_types_pkg("@types/node/index.d.ts");
 }
