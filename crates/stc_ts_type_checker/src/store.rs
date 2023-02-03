5892701faaf0e82140eae8c054e9305379f41b7c
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-03 10:54:05 +0900

    Module store

--- crates/stc_ts_type_checker/src/store.rs
+++ crates/stc_ts_type_checker/src/store.rs
@@ -1,2 +1,16 @@
+use std::sync::{Arc, RwLock};
+
+use fxhash::FxHashMap;
+use stc_ts_types::ModuleId;
+use swc_common::FileName;
+
 #[derive(Default)]
-pub struct ModuleStore {}
+pub struct ModuleStore {
+    data: RwLock<Data>,
+}
+
+#[derive(Default)]
+struct Data {
+    filename_to_id: FxHashMap<Arc<FileName>, ModuleId>,
+    id_to_filename: FxHashMap<ModuleId, Arc<FileName>>,
+}
