2f1ad687c40e51e24925837715fdbf27148f76af
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-04 17:52:58 +0900

    auto-unignore.sh

---
+++ crates/stc_ts_file_analyzer/scripts/auto-unignore.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# This script un-ignores passing tests
+set -eu
+set -o pipefail
+
+export CARGO_TERM_COLOR=always
+export RUST_BACKTRACE=1
+export RUST_MIN_STACK=$((16 * 1024 * 1024))
+
+# We prevent regression using faster checks
+touch ../stc_ts_file_analyzer/tests/base.rs
+UPDATE=1 cargo test -p stc_ts_file_analyzer --features tracing/max_level_off --color always --test base -- -Zunstable-options --report-time --ignored
