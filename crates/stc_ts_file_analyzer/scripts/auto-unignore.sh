dbcfdfeb47090aef6aebea1120f641589adb2681
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-05 15:01:31 +0900

    auto-unignore.sh

--- crates/stc_ts_file_analyzer/scripts/auto-unignore.sh
+++ crates/stc_ts_file_analyzer/scripts/auto-unignore.sh
@@ -7,6 +7,15 @@ export CARGO_TERM_COLOR=always
 export RUST_BACKTRACE=1
 export RUST_MIN_STACK=$((16 * 1024 * 1024))
 
+function deleteCache {
+    find tests/tsc -name '\.*.tsc-errors.json' - -delete
+}
+
+deleteCache
+
 # We prevent regression using faster checks
 touch ../stc_ts_file_analyzer/tests/base.rs
-cargo test -p stc_ts_file_analyzer --features tracing/max_level_off --color always --test base -- -Zunstable-options --report-time --ignored
+cargo test -p stc_ts_file_analyzer --features tracing/max_level_off --color always --test base -- -Zunstable-options --report-time --ignored || true
+
+
+deleteCache
