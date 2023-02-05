e140ba000d6eb0944a6fbfebbbec4c930639b7ad
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-05 20:36:02 +0900

    fixup

--- crates/stc_ts_file_analyzer/scripts/fast.sh
+++ crates/stc_ts_file_analyzer/scripts/fast.sh
@@ -1,22 +1,13 @@
 #!/usr/bin/env bash
 #
-# This scripts invokes all unignored tests, update pass list (append-only)
-# and print the list of failing tests.
-# 
+# This scripts invokes all base tests but without logging, to make test faster.
 #
 
 set -eu
-
-err_handler () {
-   ./scripts/_/notify.sh 'Check failed!'
-   exit 1
-}
-
-trap err_handler ERR
 
 export CARGO_TERM_COLOR=always
 export RUST_BACKTRACE=1
 export RUST_MIN_STACK=$((16 * 1024 * 1024))
 
 # We prevent regression using faster checks
-RUST_LOG=off ./scripts/base.sh --features tracing/max_level_error
+RUST_LOG=off ./scripts/base.sh --features tracing/max_level_error $@
