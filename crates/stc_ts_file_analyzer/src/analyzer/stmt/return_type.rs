8ff534497263342d9a0d15f0afd2a4923d156bb7
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-07 16:42:06 +0900

    Allow void

--- crates/stc_ts_file_analyzer/src/analyzer/stmt/return_type.rs
+++ crates/stc_ts_file_analyzer/src/analyzer/stmt/return_type.rs
@@ -368,6 +368,7 @@ impl Analyzer<'_, '_> {
                         AssignOpts {
                             span: node.span,
                             allow_unknown_rhs: Some(true),
+                            allow_assignment_of_void: Some(!self.rule().strict_null_checks),
                             may_unwrap_promise: true,
                             ..Default::default()
                         },
@@ -385,6 +386,7 @@ impl Analyzer<'_, '_> {
                         AssignOpts {
                             span: node.span,
                             allow_unknown_rhs: Some(true),
+                            allow_assignment_of_void: Some(!self.rule().strict_null_checks),
                             may_unwrap_promise: true,
                             ..Default::default()
                         },
@@ -419,6 +421,7 @@ impl Analyzer<'_, '_> {
                         AssignOpts {
                             span: node.span,
                             allow_unknown_rhs: Some(true),
+                            allow_assignment_of_void: Some(!self.rule().strict_null_checks),
                             ..Default::default()
                         },
                     )
@@ -434,6 +437,7 @@ impl Analyzer<'_, '_> {
                         AssignOpts {
                             span: node.span,
                             allow_unknown_rhs: Some(true),
+                            allow_assignment_of_void: Some(!self.rule().strict_null_checks),
 
                             ..Default::default()
                         },
