69969fbd5446f60542f0da142a2389ce97d2efa5
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-08 22:31:49 +0900

    Adjust option

--- crates/stc_ts_file_analyzer/src/analyzer/expr/call_new.rs
+++ crates/stc_ts_file_analyzer/src/analyzer/expr/call_new.rs
@@ -2592,7 +2592,7 @@ impl Analyzer<'_, '_> {
                 spread_arg_types,
                 None,
                 InferTypeOpts {
-                    is_type_ann: type_ann.is_some(),
+                    // is_type_ann: type_ann.is_some(),
                     ..Default::default()
                 },
             )?;
