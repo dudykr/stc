355da83ce7016188d3e522159384c166b3db9d99
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-09 18:47:11 +0900

    rebase

--- crates/stc_ts_file_analyzer/src/analyzer/generic/mod.rs
+++ crates/stc_ts_file_analyzer/src/analyzer/generic/mod.rs
@@ -491,11 +491,6 @@ impl Analyzer<'_, '_> {
         debug_assert!(!span.is_dummy(), "infer_type: `span` should not be dummy");
 
         if param.is_keyword() || param.type_eq(arg) {
-        if param.type_eq(arg) {
-            return Ok(());
-        }
-
-        if param.is_keyword() {
             return Ok(());
         }
 
