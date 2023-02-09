6d3d1801ecc02a38bb2bf6b77762af4490a4cab5
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-09 18:49:06 +0900

    Split

--- crates/stc_ts_file_analyzer/tests/pass-only/conformance/types/tuple/variadicTuples1/1.ts
+++ crates/stc_ts_file_analyzer/tests/pass-only/conformance/types/tuple/variadicTuples1/1.ts
@@ -2,22 +2,7 @@
 
 declare function f22<T extends unknown[] = []>(args: [...T]): T;
 
-function f23<U extends string[]>(args: [...U, number]) {
+export function f23<U extends string[]>(args: [...U, number]) {
     let v2 = f22(["foo", "bar"]);  // [string, string]
 }
 
-export { }
-
-// Mapped types applied to variadic tuple types
-
-type Arrayify<T> = { [P in keyof T]: T[P][] };
-
-
-
-// Reverse mapping through mapped type applied to variadic tuple type
-
-declare function fm1<T extends unknown[]>(t: Arrayify<[string, number, ...T]>): T;
-
-var tm1 = fm1([['abc'], [42], [true], ['def']]);  // [boolean, string]
-var tm1: [boolean, string] = [true, 'def']
-export { tm1 }
