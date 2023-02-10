7c5d68ce3a40dd45f4e5b9825f810cd178b1fe7a
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-10 17:47:59 +0900

    Rename

--- crates/stc_ts_file_analyzer/tests/pass-only/conformance/types/tuple/variadicTuples1/4.ts
+++ crates/stc_ts_file_analyzer/tests/pass-only/conformance/types/tuple/variadicTuples1/4.ts
@@ -6,7 +6,7 @@ type Arrayify<T> = { [P in keyof T]: T[P][] };
 
 // Reverse mapping through mapped type applied to variadic tuple type
 
-declare function fm1<T extends unknown[]>(t: Arrayify<[string, number, ...T]>): T;
+declare function fm1<N extends unknown[]>(t: Arrayify<[string, number, ...N]>): N;
 
 var tm1 = fm1([['abc'], [42], [true], ['def']]);  // [boolean, string]
 var tm1: [boolean, string] = [true, 'def']
