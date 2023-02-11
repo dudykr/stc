1c62973b6eda179804a5785bb7f0c3ff587f807f
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-11 11:50:30 +0900

    fixup

--- crates/stc_ts_file_analyzer/tests/base.rs
+++ crates/stc_ts_file_analyzer/tests/base.rs
@@ -222,64 +222,7 @@ fn errors(input: PathBuf) {
 
 #[fixture("tests/pass-only/**/*.ts")]
 fn pass_only(input: PathBuf) {
-    testing::run_test2(false, |cm, handler| {
-        cm.new_source_file(FileName::Anon, "".to_string());
-
-        let fm = cm.load_file(&input).unwrap();
-
-        let env = get_env();
-
-        let generator = module_id::ModuleIdGenerator::default();
-        let path = Arc::new(FileName::Real(input.to_path_buf()));
-
-        let (module_id, top_level_mark) = generator.generate(&path);
-
-        let mut node_id_gen = NodeIdGenerator::default();
-        let mut module = {
-            let lexer = Lexer::new(
-                Syntax::Typescript(TsConfig { ..Default::default() }),
-                EsVersion::Es2021,
-                SourceFileInput::from(&*fm),
-                None,
-            );
-            let mut parser = Parser::new_from(lexer);
-
-            parser.parse_module().unwrap()
-        };
-        module = module.fold_with(&mut resolver(env.shared().marks().unresolved_mark(), top_level_mark, true));
-        let module = RModule::from_orig(&mut node_id_gen, module);
-
-        let mut storage = Single {
-            parent: None,
-            id: module_id,
-            top_level_ctxt: SyntaxContext::empty().apply_mark(top_level_mark),
-            path,
-            is_dts: false,
-            info: Default::default(),
-        };
-
-        {
-            // Don't print logs from builtin modules.
-            let _tracing = tracing::subscriber::set_default(logger(Level::DEBUG));
-
-            let mut analyzer = Analyzer::root(env, cm, Default::default(), box &mut storage, &NoopLoader, None);
-            module.visit_with(&mut analyzer);
-        }
-
-        let errors = ::stc_ts_errors::ErrorKind::flatten(storage.info.errors.into_iter().collect());
-        let ok = errors.is_empty();
-
-        for e in &errors {
-            e.emit(&handler);
-        }
-
-        if !ok {
-            panic!()
-        }
-
-        Ok(())
-    })
-    .unwrap();
+    run_test(input.clone(), false).unwrap();
 
     testing::unignore_fixture(&input);
 }
