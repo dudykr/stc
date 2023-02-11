d2623ebaeb9dd9eb94631c59af04f6fc182db2e5
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-11 16:37:44 +0900

    Renmae

--- crates/stc_utils/src/error.rs
+++ crates/stc_utils/src/error.rs
@@ -1,7 +1,7 @@
 /// Creates an entered span for `tracing` on debug builds.
 /// On release builds, this is a no-op.
 #[macro_export]
-macro_rules! debug_ctx {
+macro_rules! debug_span {
     ($s:expr) => {{
         #[cfg(debug_assertions)]
         tracing::error_span!($s).entered();
