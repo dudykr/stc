634a31b17aa23e95297381fc98cc60a597b0685e
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-11 18:09:56 +0900

    Add a test

---
+++ crates/stc_ts_file_analyzer/tests/pass-only/conformance/types/mapped/mappedTypeOverlappingStringEnumKeys/1/1.ts
@@ -0,0 +1,34 @@
+// #37859
+
+enum TerrestrialAnimalTypes {
+  CAT = "cat",
+  DOG = "dog"
+};
+
+enum AlienAnimalTypes {
+  CAT = "cat",
+};
+
+type AnimalTypes = TerrestrialAnimalTypes | AlienAnimalTypes;
+
+interface TerrestrialCat {
+  type: TerrestrialAnimalTypes.CAT;
+  address: string;
+}
+
+interface AlienCat {
+  type: AlienAnimalTypes.CAT
+  planet: string;
+}
+
+type Cats = TerrestrialCat | AlienCat;
+
+type CatList = Extract<Cats, { type: 'cat' }>
+
+const cats: CatList[] = [
+  { type: TerrestrialAnimalTypes.CAT, address: "" },
+  { type: AlienAnimalTypes.CAT, planet: "" }
+];
+
+
+export { }
