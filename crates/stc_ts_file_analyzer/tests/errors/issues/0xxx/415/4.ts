3f6e9857f37459763965de875c2d5c3526ec1234
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-02-22 20:49:57 +0900

    Add tests

---
+++ crates/stc_ts_file_analyzer/tests/errors/issues/0xxx/415/4.ts
@@ -0,0 +1,25 @@
+// @strict: true 
+
+interface A {
+    a: string;
+}
+interface B {
+    b: string;
+}
+interface C {
+    c: string;
+}
+interface D {
+    d: string;
+}
+
+var aob: A | B;
+var cod: C | D;
+var anb: A & B;
+var cnd: C & D;
+var x: (A & B) | (C & D);
+var y: (A | B) & (C | D);
+
+aob = x;
+
+export { }
