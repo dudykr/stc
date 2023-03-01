6e666f82a361e4f1beb27730d86cdace824dd731
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-03-01 18:14:48 +0900

    Fix more

--- crates/stc_ts_generics/src/type_param/renamer.rs
+++ crates/stc_ts_generics/src/type_param/renamer.rs
@@ -74,7 +74,7 @@ impl Fold<Type> for TypeParamRenamer {
                 if self.declared.is_none() && mapped.is_type_param() {
                     return ty;
                 }
-                return mapped.clone();
+                return mapped.clone().into_type();
             }
         }
 
