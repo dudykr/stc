05e8d08072ccd68c894d34b272ec47292a5e6286
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-03-01 18:10:04 +0900

    Fix more

--- crates/stc_ts_file_analyzer/src/analyzer/scope/vars.rs
+++ crates/stc_ts_file_analyzer/src/analyzer/scope/vars.rs
@@ -1037,7 +1037,7 @@ impl Analyzer<'_, '_> {
                 metadata: Default::default(),
                 tracker: Default::default(),
             })
-            .freezed())
+            .into_freezed_cow())
         })
         .context("tried to ensure iterator")
     }
