e711bab1945d7d8d24e54c733f2474435e0406a8
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-03-15 17:49:14 +0900

    Add a test

---
+++ crates/stc_ts_file_analyzer/tests/pass-only/conformance/controlwFlow/controlFlowAliasing/1.ts
@@ -0,0 +1,13 @@
+// @strict: true
+
+
+// Mixing of aliased discriminants and conditionals
+
+function f40(obj: { kind: 'foo', foo?: string } | { kind: 'bar', bar?: number }) {
+    const { kind } = obj;
+    const isFoo = kind == 'foo';
+    if (isFoo && obj.foo) {
+        let t: string = obj.foo;
+    }
+}
+
