d465cd06ca99d6fe2165c34eb5ce734a5cf291a4
Jesse Jafa <jesse.jafa@gmail.com>
2023-03-25 15:15:59 +0200

    revert accidental pretier on ts file

--- crates/stc_ts_type_checker/tests/conformance/types/objectTypeLiteral/constructSignatures/constructSignaturesWithOverloads2.ts
+++ crates/stc_ts_type_checker/tests/conformance/types/objectTypeLiteral/constructSignatures/constructSignaturesWithOverloads2.ts
@@ -2,38 +2,38 @@
 
 // clodules
 class C {
-  constructor(x: number, y?: string);
-  constructor(x: number, y: string);
-  constructor(x: number) {}
+    constructor(x: number, y?: string);
+    constructor(x: number, y: string);
+    constructor(x: number) { }
 }
 module C {
-  export var x = 1;
+    export var x = 1;
 }
 
-var r1 = new C(1, "");
+var r1 = new C(1, '');
 
 class C2<T> {
-  constructor(x: T, y?: string);
-  constructor(x: T, y: string);
-  constructor(x: T) {}
+    constructor(x: T, y?: string);
+    constructor(x: T, y: string);
+    constructor(x: T) { }
 }
 module C2 {
-  export var x = 1;
+    export var x = 1;
 }
 
-var r2 = new C2(1, "");
+var r2 = new C2(1, '');
 
 // merged interfaces
 interface I {
-  new (x: number, y?: string): C;
-  new (x: number, y: string): C;
+    new (x: number, y?: string): C;
+    new (x: number, y: string): C;
 }
 
 interface I<T> {
-  new (x: T, y?: number): C2<T>;
-  new (x: T, y: number): C2<T>;
+    new (x: T, y?: number): C2<T>;
+    new (x: T, y: number): C2<T>;
 }
 
 var i2: I<number>;
-var r4 = new i2(1, "");
+var r4 = new i2(1, '');
 var r5 = new i2(1, 1);
