40298f78fb350d789f4150f26cb4aac664d29dd0
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-03-25 22:28:05 +0900

    List files

--- crates/stc_ts_file_analyzer/scripts/issues/sync.ts
+++ crates/stc_ts_file_analyzer/scripts/issues/sync.ts
@@ -1,2 +1,27 @@
+import fs from "fs";
+import path from "path";
 
+async function* walk(dir: string): AsyncGenerator<string> {
+    for await (const d of await fs.promises.opendir(dir)) {
+        const entry = path.join(dir, d.name);
+        if (d.isDirectory()) yield* walk(entry);
+        else if (d.isFile()) yield entry;
+    }
+}
 
+async function arrayFromGenerator<T>(gen: AsyncIterable<T>): Promise<T[]> {
+    const out: T[] = []
+    for await (const x of gen) {
+        out.push(x)
+    }
+    return out
+}
+
+async function main() {
+    const files = [...await arrayFromGenerator(walk('./tests/pass-only/')), ...await arrayFromGenerator(walk('./tests/tsc/'))
+    ];
+
+    console.log(files);
+}
+
+main();
