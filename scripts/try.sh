9134ef13b0f668fcb029aeee5123bfc07f19d163
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-04-16 22:42:17 +0900

    --release

--- scripts/try.sh
+++ scripts/try.sh
@@ -4,6 +4,6 @@ export RUST_BACKTRACE=1
 export RUST_LOG=trace
 
 export STC_DIR=$(pwd)
-cargo build
-$STC_DIR/target/debug/stc --help
-(cd $1 && $STC_DIR/target/debug/stc test $2)
+cargo build --release
+$STC_DIR/target/release/stc --help
+(cd $1 && $STC_DIR/target/release/stc test $2)
