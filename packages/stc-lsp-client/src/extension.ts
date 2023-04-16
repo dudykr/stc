8a582c7a3f23d0d0b54a8e3b58fcc511cdb41e30
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-04-16 19:57:59 +0900

    feat(lsp/client): Connect `stc-debug-lsp` to lang server client (#992)

--- packages/stc-lsp-client/src/extension.ts
+++ packages/stc-lsp-client/src/extension.ts
@@ -1,18 +1,15 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
 import * as path from 'path';
 import {
 	workspace as Workspace, window as Window, ExtensionContext, TextDocument, OutputChannel, WorkspaceFolder, Uri
 } from 'vscode';
 
 import {
-	LanguageClient, LanguageClientOptions, TransportKind
+	LanguageClient, LanguageClientOptions, ServerOptions, TransportKind
 } from 'vscode-languageclient/node';
 
 let defaultClient: LanguageClient;
 const clients: Map<string, LanguageClient> = new Map();
+
 
 let _sortedWorkspaceFolders: string[] | undefined;
 function sortedWorkspaceFolders(): string[] {
@@ -47,32 +44,58 @@ function getOuterMostWorkspaceFolder(folder: WorkspaceFolder): WorkspaceFolder {
 	return folder;
 }
 
-export function activate(context: ExtensionContext) {
+const langServerId = 'stc-lsp';
+const langServerName = 'Language server for stc';
 
-	const module = context.asAbsolutePath(path.join('server', 'out', 'server.js'));
-	const outputChannel: OutputChannel = Window.createOutputChannel('lsp-multi-server-example');
+export function activate(context: ExtensionContext) {
+	const outputChannel: OutputChannel = Window.createOutputChannel('stc language server');
+
+	const binaryFile = context.asAbsolutePath(path.join('..', '..', 'target', 'debug', 'examples', 'stc-debug-lsp'));
+	outputChannel.appendLine(`Using binary file at '${binaryFile}'`)
 
 	function didOpenTextDocument(document: TextDocument): void {
 		// We are only interested in language mode text
-		if (document.languageId !== 'plaintext' || (document.uri.scheme !== 'file' && document.uri.scheme !== 'untitled')) {
+		if (document.languageId !== 'typescript' || (document.uri.scheme !== 'file' && document.uri.scheme !== 'untitled')) {
 			return;
 		}
+
+		const serverOptions: ServerOptions = {
+			run: {
+				command: binaryFile,
+				transport: TransportKind.stdio,
+				options: {
+					env: {
+						RUST_LOG: 'TRACE'
+					}
+				}
+			},
+			debug: {
+				command: binaryFile,
+				transport: TransportKind.stdio,
+				options: {
+					env: {
+						RUST_LOG: 'TRACE'
+					}
+				}
+			},
+		};
+		const baseClientOptions: LanguageClientOptions = {
+			diagnosticCollectionName: 'stc-lsp',
+			outputChannel: outputChannel,
+		};
+
 
 		const uri = document.uri;
 		// Untitled files go to a default client.
 		if (uri.scheme === 'untitled' && !defaultClient) {
-			const serverOptions = {
-				run: { module, transport: TransportKind.ipc },
-				debug: { module, transport: TransportKind.ipc }
-			};
+
 			const clientOptions: LanguageClientOptions = {
+				...baseClientOptions,
 				documentSelector: [
-					{ scheme: 'untitled', language: 'plaintext' }
+					{ scheme: 'untitled', language: 'typescript' }
 				],
-				diagnosticCollectionName: 'lsp-multi-server-example',
-				outputChannel: outputChannel
 			};
-			defaultClient = new LanguageClient('lsp-multi-server-example', 'LSP Multi Server Example', serverOptions, clientOptions);
+			defaultClient = new LanguageClient(langServerId, langServerName, serverOptions, clientOptions);
 			defaultClient.start();
 			return;
 		}
@@ -86,19 +109,14 @@ export function activate(context: ExtensionContext) {
 		folder = getOuterMostWorkspaceFolder(folder);
 
 		if (!clients.has(folder.uri.toString())) {
-			const serverOptions = {
-				run: { module, transport: TransportKind.ipc },
-				debug: { module, transport: TransportKind.ipc }
-			};
 			const clientOptions: LanguageClientOptions = {
+				...baseClientOptions,
 				documentSelector: [
-					{ scheme: 'file', language: 'plaintext', pattern: `${folder.uri.fsPath}/**/*` }
+					{ scheme: 'file', language: 'plaintext', pattern: `${folder.uri.fsPath}/**/ * ` }
 				],
-				diagnosticCollectionName: 'lsp-multi-server-example',
 				workspaceFolder: folder,
-				outputChannel: outputChannel
 			};
-			const client = new LanguageClient('lsp-multi-server-example', 'LSP Multi Server Example', serverOptions, clientOptions);
+			const client = new LanguageClient(langServerId, langServerName, serverOptions, clientOptions);
 			client.start();
 			clients.set(folder.uri.toString(), client);
 		}
@@ -107,7 +125,7 @@ export function activate(context: ExtensionContext) {
 	Workspace.onDidOpenTextDocument(didOpenTextDocument);
 	Workspace.textDocuments.forEach(didOpenTextDocument);
 	Workspace.onDidChangeWorkspaceFolders((event) => {
-		for (const folder  of event.removed) {
+		for (const folder of event.removed) {
 			const client = clients.get(folder.uri.toString());
 			if (client) {
 				clients.delete(folder.uri.toString());
