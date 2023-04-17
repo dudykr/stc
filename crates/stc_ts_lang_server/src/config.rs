5eef314b4d348794ab567a9e473c368250280c11
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-04-17 19:02:51 +0900

    Fix compilation

--- crates/stc_ts_lang_server/src/config.rs
+++ crates/stc_ts_lang_server/src/config.rs
@@ -1,7 +1,7 @@
-use jsonc_parser::{parse_to_serde_value, ParseOptions};
 use stc_ts_env::{ModuleConfig, Rule};
 use swc_ecma_ast::EsVersion;
 use tracing::error;
+use tsconfig::{Target, TsConfig};
 
 use crate::{ir::SourceText, Db};
 
@@ -21,20 +21,43 @@ pub struct ParsedTsConfig {
 pub(crate) fn parse_ts_config(db: &dyn Db, src: SourceText) -> ParsedTsConfig {
     let s = src.content(db);
 
-    let result = parse_to_serde_value(
-        s.trim_start_matches('\u{feff}'),
-        &ParseOptions {
-            allow_comments: true,
-            allow_trailing_commas: true,
-            allow_loose_object_property_names: false,
-        },
-    );
+    // TODO: Use file path to support `extends`
 
-    let result = match result {
-        Ok(Some(v)) => v,
+    let result = TsConfig::parse_str(s);
+    let v = match result {
+        Ok(v) => v,
         _ => {
             error!("Failed to parse ts config: {:?}", result);
             return ParsedTsConfig::new(db, Default::default(), Default::default(), Default::default());
         }
     };
+
+    ParsedTsConfig::new(
+        db,
+        v.compiler_options.as_ref().map(Rule::from).unwrap_or_default(),
+        v.compiler_options
+            .as_ref()
+            .and_then(|v| v.target.clone())
+            .map(|v| match v {
+                Target::Es3 => EsVersion::Es3,
+                Target::Es5 => EsVersion::Es5,
+                Target::Es2015 | Target::Es6 => EsVersion::Es2015,
+                Target::Es2016 | Target::Es7 => EsVersion::Es2016,
+                Target::Es2017 => EsVersion::Es2017,
+                Target::Es2018 => EsVersion::Es2018,
+                Target::Es2019 => EsVersion::Es2019,
+                Target::Es2020 => EsVersion::Es2020,
+                Target::EsNext => EsVersion::EsNext,
+                Target::Other(s) => match &*s {
+                    "es2021" => EsVersion::Es2021,
+                    "es2022" => EsVersion::Es2021,
+                    _ => todo!("Unknown target: {}", s),
+                },
+            })
+            .unwrap_or_else(EsVersion::latest),
+        v.compiler_options
+            .as_ref()
+            .and_then(|v| v.module.clone())
+            .map_or_else(ModuleConfig::default, ModuleConfig::from),
+    )
 }
