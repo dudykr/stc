a6e8910b8ed10f5c336d15f6ee3ed24b99881f56
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-04-17 18:23:51 +0900

    ParsedTsConfig

--- crates/stc_ts_lang_server/src/project.rs
+++ crates/stc_ts_lang_server/src/project.rs
@@ -1,7 +1,14 @@
-use stc_ts_env::Rule;
+use stc_ts_env::{ModuleConfig, Rule};
+use swc_ecma_ast::EsVersion;
 
 #[salsa::tracked]
 pub struct ParsedTsConfig {
     #[no_eq]
     pub rule: Rule,
+
+    #[no_eq]
+    pub target: EsVersion,
+
+    #[no_eq]
+    pub module: ModuleConfig,
 }
