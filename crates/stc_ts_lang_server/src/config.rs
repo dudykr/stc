216617951cc1369a643b3fe78530cec835c1cf14
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-04-17 18:48:30 +0900

    Rename var

--- crates/stc_ts_lang_server/src/config.rs
+++ crates/stc_ts_lang_server/src/config.rs
@@ -1,5 +1,7 @@
+use jsonc_parser::{parse_to_serde_value, ParseOptions};
 use stc_ts_env::{ModuleConfig, Rule};
 use swc_ecma_ast::EsVersion;
+use tracing::error;
 
 use crate::{ir::SourceText, Db};
 
@@ -16,4 +18,23 @@ pub struct ParsedTsConfig {
 }
 
 #[salsa::tracked]
-pub(crate) fn parse_ts_config(db: &dyn Db, content: SourceText) -> ParsedTsConfig {}
+pub(crate) fn parse_ts_config(db: &dyn Db, src: SourceText) -> ParsedTsConfig {
+    let s = src.content(db);
+
+    let result = parse_to_serde_value(
+        s.trim_start_matches('\u{feff}'),
+        &ParseOptions {
+            allow_comments: true,
+            allow_trailing_commas: true,
+            allow_loose_object_property_names: false,
+        },
+    );
+
+    let result = match result {
+        Ok(Some(v)) => v,
+        _ => {
+            error!("Failed to parse ts config: {:?}", result);
+            return ParsedTsConfig::new(db, Default::default(), Default::default(), Default::default());
+        }
+    };
+}
