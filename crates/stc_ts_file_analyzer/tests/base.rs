e4cb3acb68c0c079ca4837307447d18b16c79fd1
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-04-21 16:25:12 +0900

    errors

--- crates/stc_ts_file_analyzer/tests/base.rs
+++ crates/stc_ts_file_analyzer/tests/base.rs
@@ -157,7 +157,7 @@ fn validate(input: &Path) -> Vec<StcError> {
 
 #[fixture("tests/errors/**/*.ts")]
 fn errors(input: PathBuf) {
-    let stderr = run_test(input.clone(), false, false).unwrap();
+    let stderr = run_test(input.clone(), true, false).unwrap();
 
     if stderr.is_empty() {
         panic!("Expected error, but got none");
