f63c382d2d2dd0985b599613adbc6019bad9ebad
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-04-21 16:27:22 +0900

    clippy

--- crates/stc_ts_file_analyzer/tests/base.rs
+++ crates/stc_ts_file_analyzer/tests/base.rs
@@ -225,110 +225,104 @@ fn run_test(file_name: PathBuf, want_error: bool, disable_logging: bool) -> Opti
     let filename = file_name.display().to_string();
     println!("{}", filename);
 
-    for case in parse_conformance_test(&file_name).unwrap() {
-        let result = testing::Tester::new()
-            .print_errors(|cm, handler| -> Result<(), _> {
-                let _tracing = if disable_logging {
-                    Some(tracing::subscriber::set_default(tracing::subscriber::NoSubscriber::default()))
-                } else {
-                    None
-                };
+    let case = parse_conformance_test(&file_name).unwrap().into_iter().next().unwrap();
 
-                let handler = Arc::new(handler);
-                let fm = cm.load_file(&file_name).unwrap();
-                let mut libs = vec![];
-                let ls = &[
-                    "es2020.full",
-                    "es2019.full",
-                    "es2018.full",
-                    "es2017.full",
-                    "es2016.full",
-                    "es2015.full",
-                ];
-                for s in ls {
-                    libs.extend(Lib::load(s))
-                }
-                libs.sort();
-                libs.dedup();
+    let result = testing::Tester::new()
+        .print_errors(|cm, handler| -> Result<(), _> {
+            let _tracing = if disable_logging {
+                Some(tracing::subscriber::set_default(tracing::subscriber::NoSubscriber::default()))
+            } else {
+                None
+            };
 
-                let env = Env::simple(case.rule, case.target, case.module_config, &libs);
-                let stable_env = env.shared().clone();
-                let generator = module_id::ModuleIdGenerator::default();
-                let path = Arc::new(FileName::Real(file_name.clone()));
+            let handler = Arc::new(handler);
+            let fm = cm.load_file(&file_name).unwrap();
+            let mut libs = vec![];
+            let ls = &[
+                "es2020.full",
+                "es2019.full",
+                "es2018.full",
+                "es2017.full",
+                "es2016.full",
+                "es2015.full",
+            ];
+            for s in ls {
+                libs.extend(Lib::load(s))
+            }
+            libs.sort();
+            libs.dedup();
 
-                let (module_id, top_level_mark) = generator.generate(&path);
+            let env = Env::simple(case.rule, case.target, case.module_config, &libs);
+            let stable_env = env.shared().clone();
+            let generator = module_id::ModuleIdGenerator::default();
+            let path = Arc::new(FileName::Real(file_name.clone()));
 
-                let mut storage = Single {
-                    parent: None,
-                    id: module_id,
-                    top_level_ctxt: SyntaxContext::empty().apply_mark(top_level_mark),
-                    path,
-                    is_dts: false,
-                    info: Default::default(),
-                };
+            let (module_id, top_level_mark) = generator.generate(&path);
 
-                let mut node_id_gen = NodeIdGenerator::default();
-                let comments = StcComments::default();
+            let mut storage = Single {
+                parent: None,
+                id: module_id,
+                top_level_ctxt: SyntaxContext::empty().apply_mark(top_level_mark),
+                path,
+                is_dts: false,
+                info: Default::default(),
+            };
 
-                let lexer = Lexer::new(
-                    Syntax::Typescript(TsConfig {
-                        tsx: filename.contains("tsx"),
-                        decorators: true,
-                        ..Default::default()
-                    }),
-                    EsVersion::Es2020,
-                    SourceFileInput::from(&*fm),
-                    Some(&comments),
+            let mut node_id_gen = NodeIdGenerator::default();
+            let comments = StcComments::default();
+
+            let lexer = Lexer::new(
+                Syntax::Typescript(TsConfig {
+                    tsx: filename.contains("tsx"),
+                    decorators: true,
+                    ..Default::default()
+                }),
+                EsVersion::Es2020,
+                SourceFileInput::from(&*fm),
+                Some(&comments),
+            );
+            let mut parser = Parser::new_from(lexer);
+            let module = parser.parse_module().unwrap();
+            let module = module.fold_with(&mut resolver(stable_env.marks().unresolved_mark(), top_level_mark, true));
+            let module = RModule::from_orig(&mut node_id_gen, module);
+            {
+                let mut analyzer = Analyzer::root(
+                    env,
+                    cm.clone(),
+                    Default::default(),
+                    box &mut storage,
+                    &NoopLoader,
+                    if want_error {
+                        None
+                    } else {
+                        Some(Debugger {
+                            cm,
+                            handler: handler.clone(),
+                        })
+                    },
                 );
-                let mut parser = Parser::new_from(lexer);
-                let module = parser.parse_module().unwrap();
-                let module = module.fold_with(&mut resolver(stable_env.marks().unresolved_mark(), top_level_mark, true));
-                let module = RModule::from_orig(&mut node_id_gen, module);
-                {
-                    let mut analyzer = Analyzer::root(
-                        env,
-                        cm.clone(),
-                        Default::default(),
-                        box &mut storage,
-                        &NoopLoader,
-                        if want_error {
-                            None
-                        } else {
-                            Some(Debugger {
-                                cm,
-                                handler: handler.clone(),
-                            })
-                        },
-                    );
 
-                    module.validate_with(&mut analyzer).unwrap();
-                }
-
-                if want_error {
-                    let errors = storage.take_errors();
-                    let errors = ErrorKind::flatten(errors.into());
-
-                    for err in errors {
-                        err.emit(&handler);
-                    }
-                }
-
-                Err(())
-            })
-            .unwrap_err();
-
-        if want_error {
-            if result.trim().is_empty() {
-                return None;
+                module.validate_with(&mut analyzer).unwrap();
             }
 
-            panic!("Failed to validate")
-        } else {
-            return Some(result);
-        }
+            if want_error {
+                let errors = storage.take_errors();
+                let errors = ErrorKind::flatten(errors.into());
+
+                for err in errors {
+                    err.emit(&handler);
+                }
+            }
+
+            Err(())
+        })
+        .unwrap_err();
+
+    if want_error && result.trim().is_empty() {
+        return None;
     }
 
-    None
+    Some(result)
 }
 
 #[testing::fixture("tests/visualize/**/*.ts", exclude(".*\\.\\.d.\\.ts"))]
