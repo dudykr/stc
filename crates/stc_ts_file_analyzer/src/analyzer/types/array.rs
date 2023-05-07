214e938d27792603eb841ffd33ecdf8a35ed523e
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-05-07 18:09:39 +0900

    More work

--- crates/stc_ts_file_analyzer/src/analyzer/types/array.rs
+++ crates/stc_ts_file_analyzer/src/analyzer/types/array.rs
@@ -1,14 +1,45 @@
-use stc_ts_types::Type;
+use std::borrow::Cow;
+
+use stc_ts_types::{Tuple, TupleElement, Type, TypeOrSpread};
+use stc_utils::cache::Freeze;
 use swc_common::Span;
 
+use super::NormalizeTypeOpts;
 use crate::{analyzer::Analyzer, VResult};
 
 impl Analyzer<'_, '_> {
-    pub(crate) fn expand_spread_likes<T>(&mut self, span: Span, elems: &[T]) -> VResult<Type>
+    pub(crate) fn expand_spread_likes<T>(&mut self, span: Span, elems: &[T]) -> VResult<Tuple>
     where
         T: SpreadLike,
     {
+        let mut tuple = Tuple {
+            span,
+            elems: Default::default(),
+            metadata: Default::default(),
+            tracker: Default::default(),
+        };
+
+        for elem in elems {
+            let e = elem.as_spread_and_type();
+
+            let ty = self
+                .normalize(
+                    Some(e.span),
+                    Cow::Borrowed(&e.ty),
+                    NormalizeTypeOpts {
+                        preserve_global_this: true,
+                        ..Default::default()
+                    },
+                )?
+                .freezed();
+
+            if let Some(spread) = e.spread {}
+        }
+
+        Ok(tuple)
     }
 }
 
-pub(crate) trait SpreadLike {}
+pub(crate) trait SpreadLike {
+    fn as_spread_and_type(&self) -> TypeOrSpread;
+}
