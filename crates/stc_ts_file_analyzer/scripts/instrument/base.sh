dudykr/stc

commit 7dbc707eba809c74fa4979df034b337eb306b50b

feat: Implement type param validation (#1011)
**Description:**
```ts
interface A<T extends string> {}
type a = A<number> // required error, not reported
```

**Related issue:**

 - Closes #956.

crates/stc_ts_file_analyzer/src/analyzer/generic/expander.rs
@@ -1,3 +1,5 @@
+use std::borrow::Cow;
+
 use fxhash::FxHashMap;
 use rnode::FoldWith;
 use stc_ts_errors::debug::dump_type_as_string;
@@ -6,8 +8,8 @@ use stc_ts_generics::{
     ExpandGenericOpts,
 };
 use stc_ts_type_ops::Fix;
-use stc_ts_types::{Id, Interface, KeywordType, Readonly, TypeElement, TypeParam, TypeParamDecl, TypeParamInstantiation};
-use stc_utils::{cache::Freeze, dev_span, ext::SpanExt};
+use stc_ts_types::{Id, Index, Interface, KeywordType, Readonly, TypeElement, TypeParam, TypeParamDecl, TypeParamInstantiation};
+use stc_utils::{cache::Freeze, dev_span, ext::SpanExt, stack};
 use swc_common::{Span, Spanned, TypeEq};
 use swc_ecma_ast::*;
 use tracing::debug;
@@ -132,6 +134,11 @@ impl Analyzer<'_, '_> {
             None
         };
 
+        let _stack = match stack::track(span) {
+            Ok(v) => v,
+            Err(_) => return None,
+        };
+
         let child = child.normalize();
         let parent = parent.normalize();
 
@@ -160,7 +167,7 @@ impl Analyzer<'_, '_> {
         match child {
             Type::Param(..) | Type::Infer(..) | Type::IndexedAccessType(..) | Type::Conditional(..) => return None,
             Type::Ref(..) => {
-                let child = self
+                let mut child = self
                     .expand(
                         child.span(),
                         child.clone(),
@@ -175,10 +182,34 @@ impl Analyzer<'_, '_> {
                     )
                     .unwrap()
                     .freezed();
-                if let Type::Ref(..) = child.normalize() {
-                    return None;
+                if child.is_ref_type() {
+                    if let Ok(ty) = self.normalize(Some(child.span()), Cow::Borrowed(&child), Default::default()) {
+                        child = ty.into_owned();
+                    }
                 }
 
+                if child.is_class() && parent.is_ref_type() {
+                    if let Ok(parent) = self.normalize(Some(child.span()), Cow::Borrowed(&child), Default::default()) {
+                        if let Ok(..) = self.assign_with_opts(
+                            &mut Default::default(),
+                            &parent,
+                            &child,
+                            AssignOpts {
+                                span,
+                                disallow_special_assignment_to_empty_class: true,
+                                disallow_different_classes: opts.disallow_different_classes,
+                                allow_assignment_to_param_constraint: true,
+                                allow_unknown_rhs: Some(!opts.strict),
+                                allow_unknown_rhs_if_expanded: !opts.strict,
+                                allow_missing_fields: opts.allow_missing_fields,
+                                allow_assignment_to_param: opts.allow_type_params,
+                                ..Default::default()
+                            },
+                        ) {
+                            return Some(true);
+                        }
+                    }
+                }
                 return self.extends(span, &child, parent, opts);
             }
 
@@ -240,6 +271,12 @@ impl Analyzer<'_, '_> {
                 parent.freeze();
 
                 return self.extends(span, child, &parent, opts);
+            }
+            Type::Index(Index { ty, .. }) if child.is_index() => {
+                if ty.is_any() {
+                    return Some(true);
+                } else {
+                }
             }
             _ => {}
         }

crates/stc_ts_file_analyzer/src/analyzer/generic/mod.rs
@@ -6,7 +6,7 @@ use rnode::{Fold, FoldWith, VisitMut, VisitMutWith, VisitWith};
 use stc_ts_ast_rnode::{RBindingIdent, RIdent, RNumber, RPat, RTsEntityName};
 use stc_ts_errors::{
     debug::{dump_type_as_string, force_dump_type_as_string, print_backtrace, print_type},
-    DebugExt,
+    DebugExt, ErrorKind,
 };
 use stc_ts_generics::{
     expander::InferTypeResult,
@@ -108,6 +108,7 @@ impl Analyzer<'_, '_> {
     ) -> VResult<InferTypeResult> {
         #[cfg(debug_assertions)]
         let _tracing = dev_span!("infer_arg_types");
+        let _guard = stack::track(span);
 
         warn!(
             "infer_arg_types: {:?}",
@@ -117,7 +118,6 @@ impl Analyzer<'_, '_> {
         let start = Instant::now();
 
         let mut inferred = InferData::default();
-
         for param in type_params {
             if let Some(constraint) = &param.constraint {
                 constraint.assert_clone_cheap();
@@ -128,6 +128,21 @@ impl Analyzer<'_, '_> {
         if let Some(base) = base {
             for (param, type_param) in base.params.iter().zip(type_params) {
                 info!("User provided `{:?} = {:?}`", type_param.name, param.clone());
+                if let Some(tp) = &type_param.constraint {
+                    if matches!(self.extends(span, param, tp, Default::default()), Some(false)) {
+                        if !param.span().is_dummy() && !param.is_type_param() {
+                            self.storage.report(
+                                ErrorKind::NotSatisfyConstraint {
+                                    span: param.span(),
+                                    left: tp.clone(),
+                                    right: Box::new(param.clone()),
+                                }
+                                .into(),
+                            );
+                            break;
+                        }
+                    }
+                }
                 inferred.type_params.insert(
                     type_param.name.clone(),
                     InferenceInfo {
@@ -542,8 +557,6 @@ impl Analyzer<'_, '_> {
             }
             inferred.dejavu.push((param.clone(), arg.clone()));
         }
-
-        debug_assert!(!span.is_dummy(), "infer_type: `span` should not be dummy");
 
         if param.is_keyword() || param.type_eq(arg) {
             return Ok(());
@@ -975,7 +988,7 @@ impl Analyzer<'_, '_> {
                 | Type::Module(..)
                 | Type::Function(..)
                 | Type::Constructor(..) => {
-                    if let Some(arg) = self.convert_type_to_type_lit(span, Cow::Borrowed(arg))? {
+                    if let Some(arg) = self.convert_type_to_type_lit(span, Cow::Borrowed(arg), Default::default())? {
                         return self.infer_type_using_type_lit_and_type_lit(span, inferred, param, &arg, opts);
                     }
                 }
@@ -1503,7 +1516,7 @@ impl Analyzer<'_, '_> {
             | Type::Module(..)
             | Type::Interface(..) => {
                 let arg = self
-                    .convert_type_to_type_lit(span, Cow::Borrowed(arg))
+                    .convert_type_to_type_lit(span, Cow::Borrowed(arg), Default::default())
                     .context("tried to convert a type into a type literal to infer mapped type")?
                     .map(Cow::into_owned)
                     .map(Type::TypeLit);

crates/stc_ts_file_analyzer/src/analyzer/scope/mod.rs
@@ -709,9 +709,6 @@ impl Analyzer<'_, '_> {
     pub(super) fn expand(&mut self, span: Span, ty: Type, opts: ExpandOpts) -> VResult<Type> {
         let _tracing = dev_span!("expand");
 
-        if !self.config.is_builtin {
-            debug_assert_ne!(span, DUMMY_SP, "expand: {:#?} cannot be expanded because it has empty span", ty);
-        }
         let span = span.with_ctxt(SyntaxContext::empty());
 
         ty.assert_valid();
@@ -2304,7 +2301,6 @@ impl Expander<'_, '_, '_> {
             ..
         } = r;
         let span = self.span;
-
         if !trying_primitive_expansion && (!self.full || self.opts.preserve_ref) {
             return Ok(None);
         }

commit 7e9e8869c5fe62150e01e314965408d7cc164a3c

perf: Partially revert #1011 (#1029)
**Description:**

This PR fixes a regression caused by 7dbc707eba809c74fa4979df034b337eb306b50b.

crates/stc_ts_file_analyzer/scripts/instrument/base.sh
@@ -10,4 +10,4 @@ export RUST_MIN_STACK=$((8 * 1024 * 1024))
 
 export STC_SKIP_EXEC=1
 
-cargo profile instruments -t time --features swc_common/concurrent --features tracing/max_level_off --test base $@ -- -Zunstable-options --report-time
+cargo profile instruments -t time --features swc_common/concurrent --features tracing/max_level_off --features no-threading --test base $@ -- -Zunstable-options --report-time

crates/stc_ts_file_analyzer/src/analyzer/generic/expander.rs
@@ -1,5 +1,3 @@
-use std::borrow::Cow;
-
 use fxhash::FxHashMap;
 use rnode::FoldWith;
 use stc_ts_errors::debug::dump_type_as_string;
@@ -8,8 +6,8 @@ use stc_ts_generics::{
     ExpandGenericOpts,
 };
 use stc_ts_type_ops::Fix;
-use stc_ts_types::{Id, Index, Interface, KeywordType, Readonly, TypeElement, TypeParam, TypeParamDecl, TypeParamInstantiation};
-use stc_utils::{cache::Freeze, dev_span, ext::SpanExt, stack};
+use stc_ts_types::{Id, Interface, KeywordType, Readonly, TypeElement, TypeParam, TypeParamDecl, TypeParamInstantiation};
+use stc_utils::{cache::Freeze, dev_span, ext::SpanExt};
 use swc_common::{Span, Spanned, TypeEq};
 use swc_ecma_ast::*;
 use tracing::debug;
@@ -134,11 +132,6 @@ impl Analyzer<'_, '_> {
             None
         };
 
-        let _stack = match stack::track(span) {
-            Ok(v) => v,
-            Err(_) => return None,
-        };
-
         let child = child.normalize();
         let parent = parent.normalize();
 
@@ -167,7 +160,7 @@ impl Analyzer<'_, '_> {
         match child {
             Type::Param(..) | Type::Infer(..) | Type::IndexedAccessType(..) | Type::Conditional(..) => return None,
             Type::Ref(..) => {
-                let mut child = self
+                let child = self
                     .expand(
                         child.span(),
                         child.clone(),
@@ -182,34 +175,10 @@ impl Analyzer<'_, '_> {
                     )
                     .unwrap()
                     .freezed();
-                if child.is_ref_type() {
-                    if let Ok(ty) = self.normalize(Some(child.span()), Cow::Borrowed(&child), Default::default()) {
-                        child = ty.into_owned();
-                    }
+                if let Type::Ref(..) = child.normalize() {
+                    return None;
                 }
 
-                if child.is_class() && parent.is_ref_type() {
-                    if let Ok(parent) = self.normalize(Some(child.span()), Cow::Borrowed(&child), Default::default()) {
-                        if let Ok(..) = self.assign_with_opts(
-                            &mut Default::default(),
-                            &parent,
-                            &child,
-                            AssignOpts {
-                                span,
-                                disallow_special_assignment_to_empty_class: true,
-                                disallow_different_classes: opts.disallow_different_classes,
-                                allow_assignment_to_param_constraint: true,
-                                allow_unknown_rhs: Some(!opts.strict),
-                                allow_unknown_rhs_if_expanded: !opts.strict,
-                                allow_missing_fields: opts.allow_missing_fields,
-                                allow_assignment_to_param: opts.allow_type_params,
-                                ..Default::default()
-                            },
-                        ) {
-                            return Some(true);
-                        }
-                    }
-                }
                 return self.extends(span, &child, parent, opts);
             }
 
@@ -271,12 +240,6 @@ impl Analyzer<'_, '_> {
                 parent.freeze();
 
                 return self.extends(span, child, &parent, opts);
-            }
-            Type::Index(Index { ty, .. }) if child.is_index() => {
-                if ty.is_any() {
-                    return Some(true);
-                } else {
-                }
             }
             _ => {}
         }

crates/stc_ts_file_analyzer/src/analyzer/generic/mod.rs
@@ -6,7 +6,7 @@ use rnode::{Fold, FoldWith, VisitMut, VisitMutWith, VisitWith};
 use stc_ts_ast_rnode::{RBindingIdent, RIdent, RNumber, RPat, RTsEntityName, RTsLit};
 use stc_ts_errors::{
     debug::{dump_type_as_string, force_dump_type_as_string, print_backtrace, print_type},
-    DebugExt, ErrorKind,
+    DebugExt,
 };
 use stc_ts_generics::{
     expander::InferTypeResult,
@@ -108,7 +108,6 @@ impl Analyzer<'_, '_> {
     ) -> VResult<InferTypeResult> {
         #[cfg(debug_assertions)]
         let _tracing = dev_span!("infer_arg_types");
-        let _guard = stack::track(span);
 
         warn!(
             "infer_arg_types: {:?}",
@@ -118,6 +117,7 @@ impl Analyzer<'_, '_> {
         let start = Instant::now();
 
         let mut inferred = InferData::default();
+
         for param in type_params {
             if let Some(constraint) = &param.constraint {
                 constraint.assert_clone_cheap();
@@ -128,21 +128,6 @@ impl Analyzer<'_, '_> {
         if let Some(base) = base {
             for (param, type_param) in base.params.iter().zip(type_params) {
                 info!("User provided `{:?} = {:?}`", type_param.name, param.clone());
-                if let Some(tp) = &type_param.constraint {
-                    if matches!(self.extends(span, param, tp, Default::default()), Some(false)) {
-                        if !param.span().is_dummy() && !param.is_type_param() {
-                            self.storage.report(
-                                ErrorKind::NotSatisfyConstraint {
-                                    span: param.span(),
-                                    left: tp.clone(),
-                                    right: Box::new(param.clone()),
-                                }
-                                .into(),
-                            );
-                            break;
-                        }
-                    }
-                }
                 inferred.type_params.insert(
                     type_param.name.clone(),
                     InferenceInfo {
@@ -557,6 +542,8 @@ impl Analyzer<'_, '_> {
             }
             inferred.dejavu.push((param.clone(), arg.clone()));
         }
+
+        debug_assert!(!span.is_dummy(), "infer_type: `span` should not be dummy");
 
         if param.is_keyword() || param.type_eq(arg) {
             return Ok(());

crates/stc_ts_file_analyzer/src/analyzer/scope/mod.rs
@@ -709,6 +709,9 @@ impl Analyzer<'_, '_> {
     pub(super) fn expand(&mut self, span: Span, ty: Type, opts: ExpandOpts) -> VResult<Type> {
         let _tracing = dev_span!("expand");
 
+        if !self.config.is_builtin {
+            debug_assert_ne!(span, DUMMY_SP, "expand: {:#?} cannot be expanded because it has empty span", ty);
+        }
         let span = span.with_ctxt(SyntaxContext::empty());
 
         ty.assert_valid();
@@ -2301,6 +2304,7 @@ impl Expander<'_, '_, '_> {
             ..
         } = r;
         let span = self.span;
+
         if !trying_primitive_expansion && (!self.full || self.opts.preserve_ref) {
             return Ok(None);
         }

crates/stc_ts_file_analyzer/src/analyzer/types/keyof.rs
@@ -44,10 +44,12 @@ impl Analyzer<'_, '_> {
     /// ```
     pub(crate) fn keyof(&mut self, span: Span, ty: &Type) -> VResult<Type> {
         let span = span.with_ctxt(SyntaxContext::empty());
+
         if !self.config.is_builtin {
             debug_assert!(!span.is_dummy(), "Cannot perform `keyof` operation with dummy span");
         }
         let _stack = stack::track(span)?;
+
         let ty = (|| -> VResult<_> {
             let mut ty = self
                 .normalize(
@@ -63,8 +65,8 @@ impl Analyzer<'_, '_> {
             if matches!(ty.normalize(), Type::TypeLit(..)) {
                 ty.freeze()
             }
+
             match ty.normalize() {
-                ty @ Type::Ref(..) => return self.keyof(span, ty),
                 Type::Lit(ty) => {
                     return self
                         .keyof(

crates/stc_ts_file_analyzer/src/analyzer/types/mapped.rs
@@ -5,7 +5,7 @@ use stc_ts_ast_rnode::{RBindingIdent, RIdent, RNumber, RPat, RTsEnumMemberId, RT
 use stc_ts_base_type_ops::apply_mapped_flags;
 use stc_ts_errors::{
     debug::{dump_type_as_string, force_dump_type_as_string},
-    DebugExt, ErrorKind,
+    DebugExt,
 };
 use stc_ts_generics::type_param::finder::TypeParamNameUsageFinder;
 use stc_ts_types::{
@@ -14,10 +14,9 @@ use stc_ts_types::{
 };
 use stc_utils::{
     cache::{Freeze, ALLOW_DEEP_CLONE},
-    dev_span,
-    stack::{self},
+    dev_span, stack,
 };
-use swc_common::{Span, Spanned, SyntaxContext, TypeEq, DUMMY_SP};
+use swc_common::{Span, Spanned, SyntaxContext, TypeEq};
 use swc_ecma_ast::{TruePlusMinus, TsKeywordTypeKind};
 use tracing::{debug, error};
 
@@ -127,44 +126,6 @@ impl Analyzer<'_, '_> {
                             metadata: Default::default(),
                             tracker: Default::default(),
                         })));
-                    } else {
-                        let _guard = stack::track(m.span)?;
-                        if let Ok(ty) = self.normalize(Some(m.span), Cow::Borrowed(constraint), Default::default()) {
-                            if ty.is_interface() {
-                                self.storage.report(
-                                    ErrorKind::AssignFailed {
-                                        span: constraint.span(),
-                                        left: Box::new(ty.into_owned()),
-                                        right_ident: Some(DUMMY_SP),
-                                        right: Box::new(Type::new_union(
-                                            DUMMY_SP,
-                                            vec![
-                                                Type::Keyword(KeywordType {
-                                                    span: DUMMY_SP,
-                                                    kind: TsKeywordTypeKind::TsStringKeyword,
-                                                    metadata: Default::default(),
-                                                    tracker: Default::default(),
-                                                }),
-                                                Type::Keyword(KeywordType {
-                                                    span: DUMMY_SP,
-                                                    kind: TsKeywordTypeKind::TsNumberKeyword,
-                                                    metadata: Default::default(),
-                                                    tracker: Default::default(),
-                                                }),
-                                                Type::Keyword(KeywordType {
-                                                    span: DUMMY_SP,
-                                                    kind: TsKeywordTypeKind::TsSymbolKeyword,
-                                                    metadata: Default::default(),
-                                                    tracker: Default::default(),
-                                                }),
-                                            ],
-                                        )),
-                                        cause: vec![],
-                                    }
-                                    .into(),
-                                );
-                            }
-                        }
                     }
                 }
             }
@@ -487,7 +448,8 @@ impl Analyzer<'_, '_> {
     /// Used for types like `'foo' | 'bar'` or alias of them.
     fn convert_type_to_keys_for_mapped_type(&mut self, span: Span, ty: &Type, name_type: Option<&Type>) -> VResult<Option<Vec<Key>>> {
         let _tracing = dev_span!("convert_type_to_keys_for_mapped_type");
-        let _guard = stack::track(span)?;
+
+        let _stack = stack::track(span)?;
 
         let ty = ty.normalize();
 
@@ -568,22 +530,9 @@ impl Analyzer<'_, '_> {
                 Ok(Some(keys))
             }
 
-            Type::Conditional(c) => {
-                if let Type::Conditional(Conditional { true_type, false_type, .. }) =
-                    self.expand_conditional_type(span, Type::Conditional(c.clone()))
-                {
-                    return self.convert_type_to_keys_for_mapped_type(
-                        span,
-                        &Type::new_union(span, vec![*true_type, *false_type]),
-                        name_type,
-                    );
-                }
-                error!("unimplemented: convert_type_to_keys: {}", force_dump_type_as_string(ty));
-                Ok(None)
-            }
             Type::TypeLit(..) | Type::Interface(..) | Type::Class(..) | Type::ClassDef(..) => Ok(None),
 
-            ty => {
+            _ => {
                 error!("unimplemented: convert_type_to_keys: {}", force_dump_type_as_string(ty));
                 Ok(None)
             }

crates/stc_ts_file_analyzer/src/analyzer/types/mod.rs
@@ -114,6 +114,9 @@ impl Analyzer<'_, '_> {
         ty.assert_valid();
 
         let actual_span = span.unwrap_or_else(|| ty.span());
+        if !self.config.is_builtin {
+            debug_assert!(!actual_span.is_dummy(), "Cannot normalize a type with dummy span\n{:?}", ty);
+        }
 
         match ty.normalize() {
             Type::Lit(..)
@@ -1608,7 +1611,7 @@ impl Analyzer<'_, '_> {
         &mut self,
         span: Span,
         ty: Cow<'a, Type>,
-        opt: ConvertTypeToLitOpts,
+        opts: ConvertTypeToLitOpts,
     ) -> VResult<Option<Cow<'a, TypeLit>>> {
         let span = span.with_ctxt(SyntaxContext::empty());
         debug_assert!(!span.is_dummy(), "type_to_type_lit: `span` should not be dummy");
@@ -1648,7 +1651,7 @@ impl Analyzer<'_, '_> {
                     .freezed();
                 let parent = self.instantiate_class(span, &parent)?;
 
-                let super_els = self.convert_type_to_type_lit(span, Cow::Owned(parent), Default::default())?;
+                let super_els = self.convert_type_to_type_lit(span, Cow::Owned(parent), opts)?;
 
                 members.extend(super_els.into_iter().map(Cow::into_owned).flat_map(|v| v.members))
             }
@@ -1690,7 +1693,7 @@ impl Analyzer<'_, '_> {
                             },
                             tracker: Default::default(),
                         })),
-                        Default::default(),
+                        opts,
                     )
                     .context("tried to convert a literal to type literal")?
                     .map(Cow::into_owned);
@@ -1718,7 +1721,7 @@ impl Analyzer<'_, '_> {
                             metadata: Default::default(),
                             tracker: Default::default(),
                         })),
-                        Default::default(),
+                        opts,
                     )?
                     .map(Cow::into_owned)
                     .map(Cow::Owned));
@@ -1730,7 +1733,7 @@ impl Analyzer<'_, '_> {
                 let mut members = vec![];
                 if let Some(super_class) = &c.def.super_class {
                     let super_class = self.instantiate_class(span, super_class)?;
-                    let super_els = self.convert_type_to_type_lit(span, Cow::Owned(super_class), Default::default())?;
+                    let super_els = self.convert_type_to_type_lit(span, Cow::Owned(super_class), opts)?;
                     members.extend(super_els.map(|ty| ty.into_owned().members).into_iter().flatten());
                 }
 
@@ -1751,7 +1754,7 @@ impl Analyzer<'_, '_> {
             Type::ClassDef(c) => {
                 let mut members = vec![];
                 if let Some(super_class) = &c.super_class {
-                    let super_els = self.convert_type_to_type_lit(span, Cow::Borrowed(super_class), Default::default())?;
+                    let super_els = self.convert_type_to_type_lit(span, Cow::Borrowed(super_class), opts)?;
                     members.extend(super_els.map(|ty| ty.into_owned().members).into_iter().flatten());
                 }
 
@@ -1772,7 +1775,7 @@ impl Analyzer<'_, '_> {
             Type::Intersection(t) => {
                 let mut members = vec![];
                 for ty in &t.types {
-                    let opt = self.convert_type_to_type_lit(span, Cow::Borrowed(ty), Default::default())?;
+                    let opt = self.convert_type_to_type_lit(span, Cow::Borrowed(ty), opts)?;
                     members.extend(opt.into_iter().map(Cow::into_owned).flat_map(|v| v.members));
                 }
 
@@ -1792,11 +1795,11 @@ impl Analyzer<'_, '_> {
                 let mut members = vec![];
                 {
                     let ty = self.overwrite_conditional(span, t);
-                    let opt = self.convert_type_to_type_lit(span, Cow::Borrowed(&ty), Default::default())?;
+                    let opt = self.convert_type_to_type_lit(span, Cow::Borrowed(&ty), opts)?;
                     members.extend(opt.into_iter().map(Cow::into_owned).flat_map(|v| v.members));
                 }
                 {
-                    let opt = self.convert_type_to_type_lit(span, Cow::Borrowed(&t.false_type), Default::default())?;
+                    let opt = self.convert_type_to_type_lit(span, Cow::Borrowed(&t.false_type), opts)?;
                     members.extend(opt.into_iter().map(Cow::into_owned).flat_map(|v| v.members));
                 }
                 Cow::Owned(TypeLit {
@@ -1846,7 +1849,7 @@ impl Analyzer<'_, '_> {
                     members.push(TypeElement::Property(PropertySignature {
                         span: e.span.with_ctxt(SyntaxContext::empty()),
                         accessibility: None,
-                        readonly: opt.is_readonly,
+                        readonly: opts.is_readonly,
                         key: Key::Num(RNumber {
                             span: e.span,
                             value: idx as f64,
@@ -1896,6 +1899,7 @@ impl Analyzer<'_, '_> {
                     tracker: Default::default(),
                 })
             }
+
             Type::Readonly(Readonly { span, ty, .. }) => {
                 let mut members = vec![];
                 let result = self.convert_type_to_type_lit(*span, Cow::Borrowed(ty), ConvertTypeToLitOpts { is_readonly: true })?;

crates/stc_ts_type_checker/tests/conformance/expressions/functionCalls/overloadResolution.stats.rust-debug
@@ -1,6 +1,6 @@
 Stats {
-    required_error: 1,
-    matched_error: 9,
+    required_error: 2,
+    matched_error: 8,
     extra_error: 0,
     panic: 0,
 }

crates/stc_ts_type_checker/tests/conformance/expressions/functionCalls/overloadResolutionConstructors.stats.rust-debug
@@ -1,6 +1,6 @@
 Stats {
-    required_error: 1,
-    matched_error: 9,
+    required_error: 2,
+    matched_error: 8,
     extra_error: 0,
     panic: 0,
 }

crates/stc_ts_type_checker/tests/conformance/expressions/functionCalls/typeArgumentInferenceWithConstraints.stats.rust-debug
@@ -1,6 +1,6 @@
 Stats {
-    required_error: 3,
-    matched_error: 12,
+    required_error: 8,
+    matched_error: 7,
     extra_error: 2,
     panic: 0,
 }

crates/stc_ts_type_checker/tests/conformance/types/mapped/mappedTypeErrors.stats.rust-debug
@@ -1,6 +1,6 @@
 Stats {
-    required_error: 20,
-    matched_error: 7,
+    required_error: 23,
+    matched_error: 4,
     extra_error: 16,
     panic: 0,
 }

crates/stc_ts_type_checker/tests/conformance/types/nonPrimitive/nonPrimitiveInGeneric.stats.rust-debug
@@ -1,6 +1,6 @@
 Stats {
-    required_error: 0,
-    matched_error: 8,
+    required_error: 3,
+    matched_error: 5,
     extra_error: 0,
     panic: 0,
 }

crates/stc_ts_type_checker/tests/conformance/types/nonPrimitive/nonPrimitiveStrictNull.stats.rust-debug
@@ -1,6 +1,6 @@
 Stats {
-    required_error: 9,
-    matched_error: 9,
+    required_error: 12,
+    matched_error: 6,
     extra_error: 0,
     panic: 0,
 }

crates/stc_ts_type_checker/tests/conformance/types/rest/genericRestParameters1.stats.rust-debug
@@ -1,6 +1,6 @@
 Stats {
-    required_error: 1,
-    matched_error: 1,
+    required_error: 2,
+    matched_error: 0,
     extra_error: 1,
     panic: 0,
 }

crates/stc_ts_type_checker/tests/tsc-stats.rust-debug
@@ -1,6 +1,6 @@
 Stats {
-    required_error: 3629,
-    matched_error: 6406,
+    required_error: 3646,
+    matched_error: 6389,
     extra_error: 764,
     panic: 75,
 }