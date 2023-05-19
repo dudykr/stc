ffdd54b1733c2dd9b3f19120f59d0f50b02e58a9
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-05-19 16:08:50 +0900

    Reduce

--- crates/stc_ts_file_analyzer/tests/errors/conformance/types/assignmentCompatibility/assignmentCompatWithCallSignaturesWithRestParameters/1.ts
+++ crates/stc_ts_file_analyzer/tests/errors/conformance/types/assignmentCompatibility/assignmentCompatWithCallSignaturesWithRestParameters/1.ts
@@ -1,12 +1,5 @@
 // call signatures in derived types must have the same or fewer optional parameters as the target for assignment
 
-interface Base {
-    a: (...args: number[]) => number;
-    a2: (x: number, ...z: number[]) => number;
-    a3: (x: number, y?: string, ...z: number[]) => number;
-    a4: (x?: number, y?: string, ...z: number[]) => number;
-}
-
 declare var a: (...args: number[]) => number; // ok, same number of required params
 a = (x?: string) => 1; // error, incompatible type
 
