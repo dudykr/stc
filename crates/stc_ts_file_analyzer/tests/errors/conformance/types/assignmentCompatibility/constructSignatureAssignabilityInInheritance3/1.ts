5acf3ecdfea166d69b924fb15c50e2296bf4e80c
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-05-29 02:20:03 +0900

    Reduce

--- crates/stc_ts_file_analyzer/tests/errors/conformance/types/assignmentCompatibility/constructSignatureAssignabilityInInheritance3/1.ts
+++ crates/stc_ts_file_analyzer/tests/errors/conformance/types/assignmentCompatibility/constructSignatureAssignabilityInInheritance3/1.ts
@@ -8,28 +8,7 @@ class Derived2 extends Derived { baz: string; }
 
 // base type with non-generic call signatures
 interface A {
-    a2: new (x: number) => string[];
     a7: new (x: (arg: Base) => Derived) => (r: Base) => Derived2;
-    a8: new (x: (arg: Base) => Derived, y: (arg2: Base) => Derived) => (r: Base) => Derived;
-    a10: new (...x: Base[]) => Base;
-    a11: new (x: { foo: string }, y: { foo: string; bar: string }) => Base;
-    a12: new (x: Array<Base>, y: Array<Derived2>) => Array<Derived>;
-    a14: {
-        new(x: number): number[];
-        new(x: string): string[];
-    };
-    a15: new (x: { a: string; b: number }) => number;
-    a16: {
-        // type of parameter is overload set which means we can't do inference based on this type
-        new(x: {
-            new(a: number): number;
-            new(a?: number): number;
-        }): number[];
-        new(x: {
-            new(a: boolean): boolean;
-            new(a?: boolean): boolean;
-        }): boolean[];
-    };
 }
 
 
