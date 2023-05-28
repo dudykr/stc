f6bc3d6230683ef9979f03e7924096503c081b46
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-05-28 22:44:44 +0900

    Reduce

--- crates/stc_ts_file_analyzer/tests/errors/conformance/types/assignmentCompatibility/assignmentCompatWithObjectMembersAccessibility/1.ts
+++ crates/stc_ts_file_analyzer/tests/errors/conformance/types/assignmentCompatibility/assignmentCompatWithObjectMembersAccessibility/1.ts
@@ -1,21 +1,51 @@
 // members N and M of types S and T have the same name, same accessibility, same optionality, and N is assignable M
 
+module TargetIsPublic {
+    // targets
+    class Base {
+        public foo: string;
+    }
 
-// targets
-class Base {
+    interface I {
+        foo: string;
+    }
+
+    // sources
+    class D {
+        public foo: string;
+    }
+
+    class E {
+        private foo: string;
+    }
+
 }
 
-interface I extends Base {
+module TargetIsPublic {
+    // targets
+    class Base {
+        private foo: string;
+    }
+
+    interface I extends Base {
+    }
+
+    var i: I;
+
+    // sources
+    class D {
+        public foo: string;
+    }
+
+    class E {
+        private foo: string;
+    }
+
+    var d: D;
+    var e: E;
+
+    e = i; // errror
+
+
 }
 
-var i: I;
-
-// sources
-
-class E {
-}
-
-var e: E;
-e = i; // errror
-
-export { }
