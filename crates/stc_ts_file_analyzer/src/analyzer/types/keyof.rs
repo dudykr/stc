4ad1a66b8fc26cade04da85ef9855d9a4946032e
sunrabbit123 <qudwls185@naver.com>
2023-08-13 22:52:03 +0900

    remove unused import

--- crates/stc_ts_file_analyzer/src/analyzer/types/keyof.rs
+++ crates/stc_ts_file_analyzer/src/analyzer/types/keyof.rs
@@ -6,7 +6,7 @@ use stc_ts_errors::{debug::force_dump_type_as_string, DebugExt, ErrorKind};
 use stc_ts_type_ops::{is_str_lit_or_union, Fix};
 use stc_ts_types::{
     Class, ClassMember, ClassProperty, Index, KeywordType, KeywordTypeMetadata, LitType, Method, MethodSignature, PropertySignature, Ref,
-    Type, TypeElement, Union,
+    Type, TypeElement,
 };
 use stc_utils::{cache::Freeze, ext::TypeVecExt, stack, try_cache};
 use swc_atoms::js_word;
